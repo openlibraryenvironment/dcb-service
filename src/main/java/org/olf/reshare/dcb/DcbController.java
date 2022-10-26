d46b85a466270d52327c830ecb3bd369a0755b6c
Alex <ajscottleicester@gmail.com>
2022-10-26 16:03:00 +0100

    feat: authentication

--- src/main/java/org/olf/reshare/dcb/DcbController.java
+++ src/main/java/org/olf/reshare/dcb/DcbController.java
@@ -2,11 +2,16 @@ package org.olf.reshare.dcb;
 
 import io.micronaut.http.annotation.Controller;
 import io.micronaut.http.annotation.Get;
+import io.micronaut.security.annotation.Secured;
 
+import static io.micronaut.security.rules.SecurityRule.IS_AUTHENTICATED;
+import static io.micronaut.http.MediaType.TEXT_PLAIN;
+
+@Secured(IS_AUTHENTICATED)
 @Controller("/dcb")
 public class DcbController {
 
-  @Get(uri = "/", produces = "text/plain")
+  @Get(uri = "/", produces = TEXT_PLAIN)
   public String index () {
     return "Example Response";
   }
--- src/test/java/org/olf/reshare/dcb/DcbTest.java
+++ src/test/java/org/olf/reshare/dcb/DcbTest.java
@@ -1,6 +1,9 @@
 package org.olf.reshare.dcb;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+import io.restassured.specification.RequestSpecification;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
@@ -26,12 +29,45 @@ class DcbTest {
   void testItWorks () {
     Assertions.assertTrue(application.isRunning());
   }
-
+  
   @Test
   void testDcbRootPath () {
-    final String text = client.toBlocking().retrieve(HttpRequest.GET("/dcb"),
+    final String text = client.toBlocking().retrieve(HttpRequest.GET("/dcb").basicAuth("user", "password"),
         String.class);
     assertEquals("Example Response", text);
   }
 
+  // Correct UN/PW provides a token
+  @Test
+  void testDcbCredetialsToken () {
+	  HttpRequest<?> request = HttpRequest.GET("/dcb").basicAuth("user", "password");
+	  // Null if no token
+	  assertNotNull(request.getHeaders().get("Authorization"));
+  }
+  
+  // No user name and password provides 400 Bad Request
+  @Test
+  void testDcbNoCredetials (RequestSpecification spec) {
+	  spec
+		  .when().get("/dcb")
+		  .then().assertThat().statusCode(400);
+  }
+  
+  // Correct UN/PW provides 200 OK
+  @Test
+  void testDcbCorrectCredetials (RequestSpecification spec) {
+	  spec
+		  .given().auth().preemptive().basic("user", "password")
+		  .when().get("/dcb")
+		  .then().assertThat().statusCode(200);
+  }
+  
+  // Incorrect UN/PW provides 401 Unauthorized
+  @Test
+  void testDcbIncorrectCredetials (RequestSpecification spec) {
+	  spec
+		  .given().auth().preemptive().basic("wrong", "wrong")
+		  .when().get("/dcb")
+		  .then().assertThat().statusCode(401);
+  }
 }
