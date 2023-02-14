8c730cc1377cd957c120ac6d4d0b7cc0d7080714
Marc Johnson <marc@k-int.co.uk>
2023-02-14 18:24:53 +0000

    chore: re-apply map chaining in patron request service

--- dcb/src/main/java/org/olf/reshare/dcb/processing/PatronRequestService.java
+++ dcb/src/main/java/org/olf/reshare/dcb/processing/PatronRequestService.java
@@ -1,15 +1,15 @@
 package org.olf.reshare.dcb.processing;
 
-import jakarta.inject.Inject;
-import jakarta.inject.Singleton;
-import org.olf.reshare.dcb.core.api.PatronRequestController;
+import java.util.UUID;
+
 import org.olf.reshare.dcb.core.model.PatronRequest;
 import org.olf.reshare.dcb.storage.PatronRequestRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import jakarta.inject.Inject;
+import jakarta.inject.Singleton;
 import reactor.core.publisher.Mono;
-import java.util.UUID;
-import javax.transaction.Transactional;
 
 @Singleton
 public class PatronRequestService {
@@ -24,41 +24,52 @@ public class PatronRequestService {
 	public static final Logger log = LoggerFactory.getLogger(PatronRequestService.class);
 
 
-	public Mono<PatronRequestRecord> savePatronRequest(Mono<PatronRequestRecord> patronRequestRecordMono) {
+	public Mono<PatronRequestRecord> savePatronRequest(
+		Mono<PatronRequestRecord> patronRequestRecordMono) {
 
-		log.debug(String.format("savePatronRequest(%s)",patronRequestRecordMono));
+		log.debug(String.format("savePatronRequest(%s)", patronRequestRecordMono));
 
-		// create new id
-		UUID uuid = UUID.randomUUID();
+		return patronRequestRecordMono
+			.map(PatronRequestService::mapToPatronRequest)
+			.flatMap(this::savePatronRequest)
+			.map(PatronRequestService::mapToResult);
+	}
 
-		return patronRequestRecordMono.map(pr -> {
-			if ((pr.requestor() != null) &&
-				(pr.requestor().agency() != null) &&
-				(pr.citation() != null) &&
-				(pr.pickupLocation() != null)) {
+	private static PatronRequest mapToPatronRequest(PatronRequestRecord request) {
+		final var uuid = UUID.randomUUID();
 
-				log.debug(String.format("create pr %s %s %s %s %s", uuid,
-					pr.requestor().identifier(),
-					pr.requestor().agency().code(),
-					pr.citation().bibClusterId(),
-					pr.pickupLocation().code()));
+		log.debug(String.format("create pr %s %s %s %s %s",uuid,
+			request.requestor().identifier(),
+			request.requestor().agency().code(),
+			request.citation().bibClusterId(),
+			request.pickupLocation().code()));
 
-				PatronRequest patronRequest = new PatronRequest(uuid,
-					pr.requestor().identifier(),
-					pr.requestor().agency().code(),
-					pr.citation().bibClusterId(),
-					pr.pickupLocation().code());
+		return new PatronRequest(uuid,
+			request.requestor().identifier(),
+			request.requestor().agency().code(),
+			request.citation().bibClusterId(),
+			request.pickupLocation().code());
+	}
 
-				log.debug(String.format("Execute save %s", patronRequest));
-				Mono<PatronRequest> new_pr_mono = Mono.from(patronRequestRepository.save(patronRequest));
+	private Mono<? extends PatronRequest> savePatronRequest(
+		PatronRequest patronRequest) {
 
-				// TODO: replace as block allows save to catch up
-				log.debug("Created new pr: {}", new_pr_mono.block().toString());
-			} else {
-				log.error("Invalid patron request data");
-			}
-			return new PatronRequestRecord(uuid, pr.citation(), pr.pickupLocation(), pr.requestor());
-		});
+		log.debug("call save on {}", patronRequest);
+
+		// Mono and publishers don't chain very well, so convert to mono
+		return Mono.from(patronRequestRepository.save(patronRequest));
+	}
+
+	private static PatronRequestRecord mapToResult(PatronRequest pr) {
+		final var result = new PatronRequestRecord(pr.getId(),
+			new PatronRequestRecord.Citation(pr.getBibClusterId()),
+			new PatronRequestRecord.PickupLocation(pr.getPickupLocationCode()),
+			new PatronRequestRecord.Requestor(pr.getPatronId(),
+				new PatronRequestRecord.Agency(pr.getPatronAgencyCode())));
+
+		log.debug("returning {}", result);
+
+		return result;
 	}
 
 	public Mono<PatronRequestRecord> getPatronRequestWithId(UUID id) {
@@ -66,6 +77,7 @@ public class PatronRequestService {
 			.map(pr -> new PatronRequestRecord(pr.getId(),
 				new PatronRequestRecord.Citation(pr.getBibClusterId()),
 				new PatronRequestRecord.PickupLocation(pr.getPatronAgencyCode()),
-				new PatronRequestRecord.Requestor(pr.getPatronId(), new PatronRequestRecord.Agency(pr.getPatronAgencyCode()))));
+				new PatronRequestRecord.Requestor(pr.getPatronId(),
+					new PatronRequestRecord.Agency(pr.getPatronAgencyCode()))));
 	}
 }
