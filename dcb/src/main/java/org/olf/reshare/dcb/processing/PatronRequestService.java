79a2adb4bc70f49abb8094b3d51689b0f85cdc6a
Alex <ajscottleicester@gmail.com>
2023-02-14 10:34:54 +0000

    fix: inject patron request repo

--- dcb/src/main/java/org/olf/reshare/dcb/processing/PatronRequestService.java
+++ dcb/src/main/java/org/olf/reshare/dcb/processing/PatronRequestService.java
@@ -1,18 +1,26 @@
 package org.olf.reshare.dcb.processing;
 
+import jakarta.inject.Inject;
 import jakarta.inject.Singleton;
+import org.olf.reshare.dcb.core.api.PatronRequestController;
 import org.olf.reshare.dcb.core.model.PatronRequest;
 import org.olf.reshare.dcb.storage.PatronRequestRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import reactor.core.publisher.Mono;
 import java.util.UUID;
 
 @Singleton
 public class PatronRequestService {
 
-	private final PatronRequestRepository patronRequestRepository;
+	@Inject
+	PatronRequestRepository patronRequestRepository;
+
 	public PatronRequestService(PatronRequestRepository patronRequestRepository) {
 		this.patronRequestRepository = patronRequestRepository;
 	}
+
+	public static final Logger log = LoggerFactory.getLogger(PatronRequestService.class);
 
 	public Mono<PatronRequestRecord> savePatronRequest(Mono<PatronRequestRecord> patronRequestRecordMono) {
 
@@ -23,15 +31,16 @@ public class PatronRequestService {
 		PatronRequest patronRequest = new PatronRequest();
 
 		return patronRequestRecordMono
-			.doOnNext(pr -> {
+			.map(pr -> new PatronRequestRecord(uuid, pr.citation(), pr.pickupLocation(), pr.requestor()))
+			.map(pr -> {
 				patronRequest.setId(uuid);
 				patronRequest.setPatronId(pr.requestor().identifier());
 				patronRequest.setPatronAgencyCode(pr.requestor().agency().code());
 				patronRequest.setBibClusterId(pr.citation().bibClusterId());
 				patronRequest.setPickupLocationCode(pr.pickupLocation().code());
 				patronRequestRepository.save(patronRequest);
-			})
-			.map(pr -> new PatronRequestRecord(uuid, pr.citation(), pr.pickupLocation(), pr.requestor()));
+				return pr;
+			});
 	}
 
 	public Mono<PatronRequestRecord> getPatronRequestWithId(UUID id) {
@@ -41,6 +50,4 @@ public class PatronRequestService {
 				new PatronRequestRecord.PickupLocation(pr.getPatronAgencyCode()),
 				new PatronRequestRecord.Requestor(pr.getPatronId(), new PatronRequestRecord.Agency(pr.getPatronAgencyCode()))));
 	}
-
-
 }
--- dcb/src/test/java/org/olf/reshare/dcb/api/PatronRequestTest.java
+++ dcb/src/test/java/org/olf/reshare/dcb/api/PatronRequestTest.java
@@ -2,12 +2,7 @@ package org.olf.reshare.dcb.api;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-import io.micronaut.core.annotation.NonNull;
-import io.micronaut.core.annotation.Nullable;
-import io.micronaut.data.annotation.Id;
-import jakarta.persistence.Column;
 import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 import org.olf.reshare.dcb.core.model.PatronRequest;
 import org.olf.reshare.dcb.storage.PatronRequestRepository;
@@ -26,8 +21,6 @@ import org.olf.reshare.dcb.processing.PatronRequestRecord;
 import jakarta.inject.Inject;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
-
-import javax.validation.constraints.NotNull;
 import java.util.List;
 import java.util.UUID;
 
@@ -52,8 +45,6 @@ class PatronRequestTest {
 
 		HttpResponse<?> response = e.getResponse();
 		assertEquals(HttpStatus.BAD_REQUEST, response.getStatus());
-
-
 
 		JSONObject patronRequest = new JSONObject() {{
 			// citation
@@ -86,16 +77,32 @@ class PatronRequestTest {
 	@Test
 	void testPatronRequestCreation() {
 		log.debug("1. testPatronRequestCreation");
+		JSONObject patronRequest = new JSONObject() {{
+			// citation
+			JSONObject citation = new JSONObject() {{
+				put("bibClusterId", UUID.randomUUID());
+			}};
+			put("citation", citation);
+			// requestor
+			JSONObject requestor = new JSONObject() {{
+				put("identifier", "jane-smith");
+				JSONObject agency = new JSONObject() {{
+					put("code", "RGX12");
+				}};
+				put("agency", agency);
+			}};
+			put("requestor", requestor);
+			// pickup location
+			JSONObject pickupLocation = new JSONObject() {{
+				put("code", "ABC123");
+			}};
+			put("pickupLocation", pickupLocation);
+		}};
+		log.debug("Using payload: {}", patronRequest.toString());
 
-		PatronRequest patronRequest = new PatronRequest();
-		patronRequest.setId(UUID.randomUUID());
-		patronRequest.setPatronId("test");
-		patronRequest.setPatronAgencyCode("test");
-		patronRequest.setBibClusterId(UUID.randomUUID());
-		patronRequest.setPickupLocationCode("test");
-
-		Mono<PatronRequest> patronRequestMono = Mono.from(requestRepository.save(patronRequest));
-		log.debug("Created test patron request: {}", patronRequestMono.block().toString());
+		// make a request to the api
+		client.toBlocking().exchange(HttpRequest.POST("/patrons/requests/place", patronRequest));
+		log.debug("Created test patron request: {}", patronRequest);
 
 		// Make sure that we have 1 task
 		List<PatronRequest> patronRequests = Flux.from(requestRepository.findAll()).collectList().block();
