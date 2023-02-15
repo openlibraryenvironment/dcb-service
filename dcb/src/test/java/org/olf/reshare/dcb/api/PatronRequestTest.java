f55a2dba04dc3da6d43d67edc95738907778b0a8
Marc Johnson <marc@k-int.co.uk>
2023-02-15 07:50:31 +0000

    Extract methods for creating JSON and placing request

--- dcb/src/test/java/org/olf/reshare/dcb/api/PatronRequestTest.java
+++ dcb/src/test/java/org/olf/reshare/dcb/api/PatronRequestTest.java
@@ -1,5 +1,7 @@
 package org.olf.reshare.dcb.api;
 
+import static io.micronaut.http.HttpStatus.BAD_REQUEST;
+import static io.micronaut.http.HttpStatus.OK;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertThrows;
@@ -17,7 +19,6 @@ import org.olf.reshare.dcb.test.DcbTest;
 
 import io.micronaut.http.HttpRequest;
 import io.micronaut.http.HttpResponse;
-import io.micronaut.http.HttpStatus;
 import io.micronaut.http.client.HttpClient;
 import io.micronaut.http.client.annotation.Client;
 import io.micronaut.http.client.exceptions.HttpClientResponseException;
@@ -45,37 +46,13 @@ class PatronRequestTest {
 
 		HttpResponse<?> response = e.getResponse();
 
-		assertEquals(HttpStatus.BAD_REQUEST, response.getStatus());
+		assertEquals(BAD_REQUEST, response.getStatus());
 	}
 
 	@Test
 	@Order(2)
 	void testPatronRequestCreation() {
-		final var patronRequest = new JSONObject() {{
-			// citation
-			final var citation = new JSONObject() {{
-				put("bibClusterId", UUID.randomUUID().toString());
-			}};
-			put("citation", citation);
-			// requestor
-			final var requestor = new JSONObject() {{
-				put("identifier", "jane-smith");
-				final var agency = new JSONObject() {{
-					put("code", "RGX12");
-				}};
-				put("agency", agency);
-			}};
-			put("requestor", requestor);
-			// pickup location
-			final var pickupLocation = new JSONObject() {{
-				put("code", "ABC123");
-			}};
-			put("pickupLocation", pickupLocation);
-		}};
-
-		// make a request to the api
-		client.toBlocking()
-			.exchange(HttpRequest.POST("/patrons/requests/place", patronRequest));
+		placePatronRequest(createPlacePatronRequestCommand());
 
 		final var patronRequests = Flux.from(
 			requestRepository.findAll()).collectList().block();
@@ -96,10 +73,42 @@ class PatronRequestTest {
 		PatronRequest patronRequest = patronRequests.get(0);
 
 		// get patron request by id via audit controller
-		var response = client.toBlocking().retrieve(HttpRequest.GET("/admin/patrons/requests/"+patronRequest.getId()), PatronRequestRecord.class);
+		var response = client.toBlocking()
+			.retrieve(HttpRequest.GET("/admin/patrons/requests/"+patronRequest.getId()),
+				PatronRequestRecord.class);
 
 		// check the response has same id as the requested patron request
 		assertEquals(response.id(), patronRequest.getId());
 	}
 
+	private static JSONObject createPlacePatronRequestCommand() {
+		return new JSONObject() {{
+			// citation
+			final var citation = new JSONObject() {{
+				put("bibClusterId", UUID.randomUUID().toString());
+			}};
+			put("citation", citation);
+			// requestor
+			final var requestor = new JSONObject() {{
+				put("identifier", "jane-smith");
+				final var agency = new JSONObject() {{
+					put("code", "RGX12");
+				}};
+				put("agency", agency);
+			}};
+			put("requestor", requestor);
+			// pickup location
+			final var pickupLocation = new JSONObject() {{
+				put("code", "ABC123");
+			}};
+			put("pickupLocation", pickupLocation);
+		}};
+	}
+
+	private void placePatronRequest(JSONObject json) {
+		final var response = client.toBlocking()
+			.exchange(HttpRequest.POST("/patrons/requests/place", json));
+
+		assertEquals(OK, response.getStatus());
+	}
 }
