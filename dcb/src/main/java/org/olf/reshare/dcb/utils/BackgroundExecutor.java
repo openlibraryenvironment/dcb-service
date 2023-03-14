527810d844beb8470d13fa8c98407751fa8ac54a
Marc Johnson <marc@k-int.co.uk>
2023-03-14 10:28:58 +0000

    feature: Can delay task execution DCB-123

--- dcb/src/main/java/org/olf/reshare/dcb/utils/BackgroundExecutor.java
+++ dcb/src/main/java/org/olf/reshare/dcb/utils/BackgroundExecutor.java
@@ -1,14 +1,28 @@
 package org.olf.reshare.dcb.utils;
 
+import java.time.Duration;
+
+import jakarta.inject.Inject;
 import jakarta.inject.Singleton;
 import reactor.core.publisher.Mono;
 import reactor.core.scheduler.Schedulers;
 
 @Singleton
 public class BackgroundExecutor {
+	private final Duration delay;
+
+	@Inject
+	public BackgroundExecutor() {
+		this(Duration.ZERO);
+	}
+	
+	public BackgroundExecutor(Duration delay) {
+		this.delay = delay;
+	}
+
 	public void executeAsynchronously(Mono<Void> task) {
 		// Try to ensure next transition happens asynchronously to triggering flow
-		task.subscribeOn(Schedulers.boundedElastic())
+		task.delaySubscription(delay, Schedulers.boundedElastic())
 			// Use a subscription to force publisher to be evaluated
 			.subscribe();
 	}
