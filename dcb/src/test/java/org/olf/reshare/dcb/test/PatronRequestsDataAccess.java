0942b3d26dbdaf47428039bdfbf2e47bff2fc1c4
Marc Johnson <marc.johnson@k-int.co.uk>
2023-03-28 12:05:52 +0000

    feat: Fail when host LMS cannot be found in config or database

--- dcb/src/test/java/org/olf/reshare/dcb/test/PatronRequestsDataAccess.java
+++ dcb/src/test/java/org/olf/reshare/dcb/test/PatronRequestsDataAccess.java
@@ -1,9 +1,5 @@
 package org.olf.reshare.dcb.test;
 
-import java.util.List;
-import java.util.function.Function;
-import java.util.function.Supplier;
-
 import org.olf.reshare.dcb.core.model.PatronRequest;
 import org.olf.reshare.dcb.core.model.SupplierRequest;
 import org.olf.reshare.dcb.storage.PatronRequestRepository;
@@ -11,33 +7,25 @@ import org.olf.reshare.dcb.storage.SupplierRequestRepository;
 import org.reactivestreams.Publisher;
 
 import jakarta.inject.Singleton;
-import reactor.core.publisher.Flux;
 
 @Singleton
 public class PatronRequestsDataAccess {
+	private final DataAccess dataAccess;
 	private final PatronRequestRepository patronRequestRepository;
-
 	private final SupplierRequestRepository supplierRequestRepository;
 
 	public PatronRequestsDataAccess(
 		PatronRequestRepository patronRequestRepository,
 		SupplierRequestRepository supplierRequestRepository) {
 
+		dataAccess = new DataAccess();
 		this.patronRequestRepository = patronRequestRepository;
 		this.supplierRequestRepository = supplierRequestRepository;
 	}
 
-	public List<SupplierRequest> getAllSupplierRequests() {
-		return Flux.from(supplierRequestRepository.findAll()).collectList().block();
-	}
-
-	public List<PatronRequest> getAllPatronRequests() {
-		return Flux.from(patronRequestRepository.findAll()).collectList().block();
-	}
-
 	public void deleteAllPatronRequests() {
-		deleteAll(this::getAllSupplierRequests, this::deleteSupplierRequest);
-		deleteAll(this::getAllPatronRequests, this::deletePatronRequest);
+		dataAccess.deleteAll(supplierRequestRepository.findAll(), this::deleteSupplierRequest);
+		dataAccess.deleteAll(patronRequestRepository.findAll(), this::deletePatronRequest);
 	}
 
 	private Publisher<Void> deletePatronRequest(PatronRequest patronRequest) {
@@ -46,16 +34,5 @@ public class PatronRequestsDataAccess {
 
 	private Publisher<Void> deleteSupplierRequest(SupplierRequest supplierRequest) {
 		return supplierRequestRepository.delete(supplierRequest.getId());
-	}
-
-	private <T> void deleteAll(Supplier<List<T>> allRecordsFetcher,
-		Function<T, Publisher<Void>> deleteFunction) {
-
-		final var allRecords = allRecordsFetcher.get();
-
-		Flux.fromStream(allRecords.stream())
-			.flatMap(deleteFunction)
-			.then()
-			.block();
 	}
 }
