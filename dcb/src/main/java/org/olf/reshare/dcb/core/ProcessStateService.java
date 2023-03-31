d5e3e5049f68ac7d873ade6d8bee3722f1476f05
Ian Ibbotson <ianibbo@gmail.com>
2023-03-31 15:10:08 +0000

    feature: Add process state domain class and related repository / service - in prep for storing hostLMS state

---
+++ dcb/src/main/java/org/olf/reshare/dcb/core/ProcessStateService.java
@@ -0,0 +1,53 @@
+package org.olf.reshare.dcb.core;
+
+import java.util.List;
+import java.util.Map;
+import java.util.UUID;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import org.olf.reshare.dcb.core.interaction.HostLmsClient;
+import org.olf.reshare.dcb.core.model.HostLms;
+import org.olf.reshare.dcb.ingest.IngestSource;
+import org.olf.reshare.dcb.ingest.IngestSourcesProvider;
+import org.olf.reshare.dcb.storage.ProcessStateRepository;
+import org.reactivestreams.Publisher;
+import org.olf.reshare.dcb.core.model.ProcessState;
+
+import io.micronaut.context.BeanContext;
+import jakarta.inject.Singleton;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+import  services.k_int.utils.UUIDUtils;
+
+
+@Singleton
+public class ProcessStateService {
+
+        ProcessStateRepository processStateRepository;
+
+	ProcessStateService(ProcessStateRepository processStateRepository) {
+                this.processStateRepository = processStateRepository;
+	}
+	
+        public void updateState(UUID context, String processName, Map<String, Object> state) {
+
+                UUID persistence_id = UUIDUtils.nameUUIDFromNamespaceAndString(context,processName);
+
+                ProcessState ps = new ProcessState(persistence_id, context, processName, state);
+
+                Mono.from(processStateRepository.existsById(persistence_id))
+                        .flatMap(exists -> Mono.fromDirect(exists ? processStateRepository.update(ps) : processStateRepository.save(ps)))
+                        .block();
+        }
+
+        public Map<String, Object> getState(UUID context, String processName) {
+                UUID persistence_id = UUIDUtils.nameUUIDFromNamespaceAndString(context,processName);
+
+                ProcessState ps = Mono.from(processStateRepository.findById(persistence_id))
+                        .block();
+
+                return ps.getProcessState();
+        }
+}
---
+++ dcb/src/test/java/org/olf/reshare/dcb/ProcessStateServiceTests.java
@@ -0,0 +1,60 @@
+package org.olf.reshare.dcb;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.notNullValue;
+
+import static io.micronaut.http.HttpStatus.BAD_REQUEST;
+import static io.micronaut.http.HttpStatus.OK;
+import static java.util.Objects.requireNonNull;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+
+import java.util.UUID;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.olf.reshare.dcb.core.ProcessStateService;
+import org.olf.reshare.dcb.test.DcbTest;
+
+import io.micronaut.http.HttpRequest;
+import io.micronaut.http.HttpResponse;
+import io.micronaut.http.client.HttpClient;
+import io.micronaut.http.client.annotation.Client;
+import io.micronaut.http.client.exceptions.HttpClientResponseException;
+import io.micronaut.serde.annotation.Serdeable;
+import jakarta.inject.Inject;
+import net.minidev.json.JSONObject;
+
+import jakarta.inject.Inject;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+import java.util.Map;
+import java.util.HashMap;
+
+@DcbTest
+class ProcessStateServiceTests {
+
+	@Inject
+	ProcessStateService processStateService;
+
+	@BeforeEach
+	void beforeEach() {
+	}
+
+	@Test
+	void processStateLifecycle() {
+		Map<String, Object> test_state = new HashMap<String,Object>();
+                test_state.put("key1","value1");
+
+                UUID test_context = UUID.randomUUID();
+                processStateService.updateState(test_context, "testProcess", test_state);
+
+                Map<String, Object> current_state = processStateService.getState(test_context, "testProcess");
+
+                assert(current_state.get("key1").equals("value1"));
+	}
+}
