5d0ce9d4318ef71c64d8e0a77ec87f5ff0779d91
Marc Johnson <marc.johnson@k-int.co.uk>
2023-06-01 11:14:42 +0000

    test: Reduce duplication in sierra api fixtures

--- dcb/src/test/java/org/olf/reshare/dcb/core/interaction/sierra/SierraBibsAPIFixture.java
+++ dcb/src/test/java/org/olf/reshare/dcb/core/interaction/sierra/SierraBibsAPIFixture.java
@@ -1,67 +1,46 @@
 package org.olf.reshare.dcb.core.interaction.sierra;
 
-import io.micronaut.core.io.ResourceLoader;
-import org.mockserver.client.MockServerClient;
-import org.mockserver.model.MediaType;
-import services.k_int.interaction.sierra.bibs.BibPatch;
-
-import java.io.IOException;
-
-import static org.mockserver.model.HttpRequest.request;
-import static org.mockserver.model.HttpResponse.response;
 import static org.mockserver.model.JsonBody.json;
 
+import org.mockserver.client.MockServerClient;
+
+import io.micronaut.core.io.ResourceLoader;
+import services.k_int.interaction.sierra.bibs.BibPatch;
+
 public class SierraBibsAPIFixture {
-	private static final String BIB_PATH = "/iii/sierra-api/v6/bibs";
-	private final String MOCK_ROOT = "classpath:mock-responses/sierra/bibs";
+	private final MockServerClient mockServer;
+	private final SierraMockServerRequests sierraMockServerRequests;
+	private final SierraMockServerResponses sierraMockServerResponses;
 
-	private final MockServerClient mock;
-	private final ResourceLoader loader;
+	public SierraBibsAPIFixture(MockServerClient mockServer, ResourceLoader loader) {
+		this.mockServer = mockServer;
 
-	public SierraBibsAPIFixture(MockServerClient mock, ResourceLoader loader) {
-		this.mock = mock;
-		this.loader = loader;
+		sierraMockServerRequests = new SierraMockServerRequests(
+			"/iii/sierra-api/v6/bibs");
+
+		sierraMockServerResponses = new SierraMockServerResponses(
+			"classpath:mock-responses/sierra/bibs/", loader);
 	}
 
-	public void createGetBibsMockWithQueryStringParameters() throws IOException {
-		mock.when(
-			request()
-				.withHeader("Accept", "application/json")
-				.withMethod("GET")
-				.withPath(BIB_PATH)
+	public void createGetBibsMockWithQueryStringParameters() {
+		mockServer
+			.when(sierraMockServerRequests.get()
 				.withQueryStringParameter("updatedDate", "null")
 				.withQueryStringParameter("suppressed", "false")
 				.withQueryStringParameter("offset", "1")
 				.withQueryStringParameter("locations", "a")
 				.withQueryStringParameter("limit", "3")
 				.withQueryStringParameter("deleted", "false")
-				.withQueryStringParameter("createdDate", "null")
-		).respond(
-			response()
-				.withStatusCode(200)
-				.withContentType(org.mockserver.model.MediaType.APPLICATION_JSON)
-				.withBody(
-					json(new String(loader
-						.getResourceAsStream(MOCK_ROOT + "/sierra-api-GET-bibs-success-response.json")
-						.orElseThrow()
-						.readAllBytes()))));
+				.withQueryStringParameter("createdDate", "null"))
+			.respond(sierraMockServerResponses
+				.jsonSuccess("sierra-api-GET-bibs-success-response.json"));
 	}
 
-	public void createPostBibsMock(BibPatch bibPatch) throws IOException {
-		mock.when(
-			request()
-				.withHeader("Accept", "application/json")
-				.withMethod("POST")
-				.withPath(BIB_PATH)
-				.withBody(json(bibPatch))
-		).respond(
-			response()
-				.withStatusCode(200)
-				.withContentType(MediaType.APPLICATION_JSON)
-				.withBody(
-					json(new String(loader
-						.getResourceAsStream(MOCK_ROOT + "/sierra-api-POST-bibs-success-response.json")
-						.orElseThrow()
-						.readAllBytes()))));
+	public void createPostBibsMock(BibPatch bibPatch) {
+		mockServer
+			.when(sierraMockServerRequests.post()
+				.withBody(json(bibPatch)))
+			.respond(sierraMockServerResponses
+				.jsonLink("https://sandbox.iii.com/iii/sierra-api/v6/bibs/7916922"));
 	}
 }
--- dcb/src/test/java/org/olf/reshare/dcb/core/interaction/sierra/SierraItemsAPIFixture.java
+++ dcb/src/test/java/org/olf/reshare/dcb/core/interaction/sierra/SierraItemsAPIFixture.java
@@ -1,15 +1,9 @@
 package org.olf.reshare.dcb.core.interaction.sierra;
 
-import static org.mockserver.model.HttpRequest.request;
-import static org.mockserver.model.HttpResponse.notFoundResponse;
-import static org.mockserver.model.HttpResponse.response;
 import static org.mockserver.model.JsonBody.json;
-import static org.mockserver.model.MediaType.APPLICATION_JSON;
-import static org.mockserver.model.MediaType.TEXT_PLAIN;
 
 import java.util.List;
 
-import org.mockserver.client.ForwardChainExpectation;
 import org.mockserver.client.MockServerClient;
 import org.mockserver.model.HttpRequest;
 import org.mockserver.model.HttpResponse;
@@ -18,64 +12,62 @@ import io.micronaut.core.io.ResourceLoader;
 import io.micronaut.serde.annotation.Serdeable;
 import lombok.Builder;
 import lombok.Data;
-import lombok.SneakyThrows;
 
 public class SierraItemsAPIFixture {
-	private static final String RESPONSE_RESOURCES_DIRECTORY = "classpath:mock-responses/sierra/";
-	private static final String ITEMS_PATH = "/iii/sierra-api/v6/items";
+	private final MockServerClient mockServer;
+	private final SierraMockServerRequests sierraMockServerRequests;
+	private final SierraMockServerResponses sierraMockServerResponses;
 
-	private final MockServerClient mock;
-	private final ResourceLoader loader;
+	public SierraItemsAPIFixture(MockServerClient mockServer, ResourceLoader loader) {
+		this.mockServer = mockServer;
+		this.sierraMockServerRequests = new SierraMockServerRequests(
+			"/iii/sierra-api/v6/items");
 
-	public SierraItemsAPIFixture(MockServerClient mock, ResourceLoader loader) {
-		this.mock = mock;
-		this.loader = loader;
+		this.sierraMockServerResponses = new SierraMockServerResponses(
+			"classpath:mock-responses/sierra/", loader);
 	}
 
 	public void threeItemsResponseForBibId(String bibId) {
-		mockGetItemsForBibId(bibId)
+		mockServer.when(getItemsForBib(bibId))
 			.respond(threeItemsResponse());
 	}
 
 	public void twoItemsResponseForBibId(String bibId) {
-		mockGetItemsForBibId(bibId)
+		mockServer.when(getItemsForBib(bibId))
 			.respond(twoItemsResponse());
 	}
 
 	public void zeroItemsResponseForBibId(String bibId) {
-		mockGetItemsForBibId(bibId)
+		mockServer.when(getItemsForBib(bibId))
 			.respond(zeroItemsResponse());
 	}
 
 	public HttpResponse threeItemsResponse() {
-		return withSierraResponse(response(), 200,
+		return sierraMockServerResponses.jsonSuccess(
 			"items/sierra-api-three-items.json");
 	}
 
 	private HttpResponse twoItemsResponse() {
-		return withSierraResponse(response(), 200,
+		return sierraMockServerResponses.jsonSuccess(
 			"items/sierra-api-two-items.json");
 	}
 
 	private HttpResponse zeroItemsResponse() {
-		return withSierraResponse(notFoundResponse(), 404,
+		return sierraMockServerResponses.notFound(
 			"items/sierra-api-zero-items.json");
 	}
 
 	public void jsonErrorResponseForCreateItem() {
-		mock.clear(createItemsRequest());
+		mockServer.clear(sierraMockServerRequests.post());
 
-		mock.when(createItemsRequest())
-			.respond(response()
-				.withStatusCode(500)
-				.withContentType(APPLICATION_JSON)
-				.withBody(getResourceAsString("json-error.json")));
+		mockServer.when(sierraMockServerRequests.post())
+			.respond(sierraMockServerResponses.jsonError("json-error.json"));
 	}
 
 	public void successResponseForCreateItem(Integer bibId, Integer itemType,
 		String locationCode, String barcode) {
 
-		mock.clear(createItemsRequest());
+		mockServer.clear(sierraMockServerRequests.post());
 
 		final var body = ItemPatch.builder()
 			.bibIds(List.of(bibId))
@@ -84,67 +76,25 @@ public class SierraItemsAPIFixture {
 			.barcodes(List.of(barcode))
 			.build();
 
-		mock.when(createItemsRequest()
+		mockServer
+			.when(sierraMockServerRequests.post()
 				.withBody(json(body)))
-			.respond(response()
-				.withStatusCode(200)
-				.withContentType(APPLICATION_JSON)
-				.withBody(getResourceAsString("items/sierra-api-post-item-success-response.json")));
+			.respond(sierraMockServerResponses.jsonSuccess(
+				"items/sierra-api-post-item-success-response.json"));
 	}
 
 	public void serverErrorResponseForBibId(String bibId) {
 		// This is a made up response (rather than captured from the sandbox)
 		// in order to demonstrate that general failures of the API are propagated
-		mockGetItemsForBibId(bibId)
-			.respond(response()
-				.withStatusCode(500)
-				.withContentType(TEXT_PLAIN)
-				.withBody("Broken"));
+		mockServer
+			.when(getItemsForBib(bibId))
+			.respond(sierraMockServerResponses.textError());
 	}
 
-	@SneakyThrows
-	private String getResourceAsString(String resourceName) {
-		final var resourcePath = RESPONSE_RESOURCES_DIRECTORY + resourceName;
-
-		final var optionalResource = loader.getResourceAsStream(resourcePath);
-
-		if (optionalResource.isEmpty()) {
-			throw new RuntimeException("Resource could not be found: " + resourcePath);
-		}
-
-		final var resource = optionalResource.get();
-
-		return new String(resource.readAllBytes());
-	}
-
-	private HttpResponse withSierraResponse(HttpResponse response,
-		int statusCode, String resourceName) {
-
-		return response
-			.withStatusCode(statusCode)
-			.withContentType(APPLICATION_JSON)
-			.withBody(json(getResourceAsString(resourceName)));
-	}
-
-	private static HttpRequest getItemsRequest() {
-		return request()
-			.withMethod("GET")
-			.withPath(ITEMS_PATH);
-	}
-
-	private static HttpRequest createItemsRequest() {
-		return request()
-			.withMethod("POST")
-			.withPath(ITEMS_PATH)
-			.withHeader("Accept", "application/json");
-	}
-
-	private ForwardChainExpectation mockGetItemsForBibId(String bibId) {
-		return mock.when(
-			getItemsRequest()
-				.withQueryStringParameter("bibIds", bibId)
-				.withQueryStringParameter("deleted", "false")
-				.withHeader("Accept", "application/json"));
+	private HttpRequest getItemsForBib(String bibId) {
+		return sierraMockServerRequests.get()
+			.withQueryStringParameter("bibIds", bibId)
+			.withQueryStringParameter("deleted", "false");
 	}
 
 	@Serdeable
