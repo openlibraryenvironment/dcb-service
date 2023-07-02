4ddd57c96c0eb9281d46fa9291de00527d6aa7dd
Ian Ibbotson <ianibbo@gmail.com>
2023-07-02 12:09:07 +0100

    removed blockInfo and autoBlockInfo from fields list in getPatron - Sierra sometimes 500s when requesting these fields

--- dcb/src/main/java/org/olf/reshare/dcb/core/interaction/sierra/HostLmsSierraApiClient.java
+++ dcb/src/main/java/org/olf/reshare/dcb/core/interaction/sierra/HostLmsSierraApiClient.java
@@ -450,7 +450,7 @@ public class HostLmsSierraApiClient implements SierraApiClient {
 		// See https://sandbox.iii.com/iii/sierra-api/swagger/index.html#!/patrons/Get_the_holds_data_for_a_single_patron_record_get_30
 		return getRequest("patrons/" + patronId)
                 	.map(req -> req.uri(theUri -> theUri
-                        	.queryParam("fields", "id,updatedDate,createdDate,expirationDate,names,barcodes,patronType,patronCodes,homeLibraryCode,emails,message,blockInfo,autoBlockInfo,uniqueIds,emails,fixedFields")
+                        	.queryParam("fields", "id,updatedDate,createdDate,expirationDate,names,barcodes,patronType,patronCodes,homeLibraryCode,emails,message,uniqueIds,emails,fixedFields")
         		))
 			.flatMap(this::ensureToken)
 			.flatMap(req -> doRetrieve(req, SierraPatronRecord.class) )
