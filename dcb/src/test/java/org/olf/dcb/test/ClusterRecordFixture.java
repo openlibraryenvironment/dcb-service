b4c325e4af31fe9f4189fd1a422d23fcbe0d6c36
Ian Ibbotson <ianibbo@googlemail.com>
2023-07-07 11:10:49 +0100

    Added bool to test

--- dcb/src/test/java/org/olf/dcb/test/ClusterRecordFixture.java
+++ dcb/src/test/java/org/olf/dcb/test/ClusterRecordFixture.java
@@ -22,7 +22,7 @@ public class ClusterRecordFixture {
 
 	public ClusterRecord createClusterRecord(UUID clusterRecordId) {
 		return Mono.from(clusterRecordRepository.save(new ClusterRecord(clusterRecordId,
-				now(), now(), "Brain of the Firm", new java.util.HashSet(), clusterRecordId)))
+				now(), now(), "Brain of the Firm", new java.util.HashSet(), clusterRecordId, Boolean.FALSE)))
 			.block();
 	}
 
