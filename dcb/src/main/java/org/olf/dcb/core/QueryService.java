c7a5a11922a459e4aa26788188c671aa6dbc2872
Ian Ibbotson <ianibbo@googlemail.com>
2023-07-18 12:51:48 +0100

    Use lucene query parser

--- dcb/src/main/java/org/olf/dcb/core/QueryService.java
+++ dcb/src/main/java/org/olf/dcb/core/QueryService.java
@@ -2,11 +2,31 @@ package org.olf.dcb.core;
 
 import org.olf.dcb.core.model.DataAgency;
 import io.micronaut.data.repository.jpa.criteria.PredicateSpecification;
+import io.micronaut.data.repository.jpa.JpaSpecificationExecutor;
+
+import org.apache.lucene.queryparser.flexible.standard.StandardQueryParser;
+import org.apache.lucene.queryparser.flexible.core.config.QueryConfigHandler;
+import org.apache.lucene.queryparser.flexible.core.QueryNodeException;
+import org.apache.lucene.search.Query;
 
 public class QueryService {
 
         private PredicateSpecification<DataAgency> codeLike(String code) {
                 return (root, criteriaBuilder) -> criteriaBuilder.like(root.get("code"), "%"+code+"%");
+        }
+
+        private Query parse(String q) throws QueryNodeException {
+                StandardQueryParser qpHelper = new StandardQueryParser();
+                Query query = qpHelper.parse(q, "defaultField");
+                return query;
+        }
+
+        public void evaluate(String q, Class c, JpaSpecificationExecutor repo) {
+                try {
+                        Query parsed_query = parse(q);
+                }
+                catch ( QueryNodeException qne ) {
+                }
         }
 
 }
