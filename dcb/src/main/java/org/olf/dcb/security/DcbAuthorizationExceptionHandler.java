9ed58182f0496357b63a2c96578063190dba16d0
Ian Ibbotson <ianibbo@googlemail.com>
2023-08-24 18:13:05 +0100

    Compiles but doesn't run

--- dcb/src/main/java/org/olf/dcb/security/DcbAuthorizationExceptionHandler.java
+++ dcb/src/main/java/org/olf/dcb/security/DcbAuthorizationExceptionHandler.java
@@ -10,10 +10,21 @@ import io.micronaut.http.MutableHttpResponse;
 import io.micronaut.security.authentication.AuthorizationException;
 import io.micronaut.security.authentication.DefaultAuthorizationExceptionHandler;
 import jakarta.inject.Singleton;
+import io.micronaut.security.config.*;
+import io.micronaut.security.errors.*;
+import io.micronaut.core.annotation.Nullable;
 
 @Singleton
 @Replaces(DefaultAuthorizationExceptionHandler.class)
 public class DcbAuthorizationExceptionHandler extends DefaultAuthorizationExceptionHandler {
+
+        public DcbAuthorizationExceptionHandler( 
+                io.micronaut.http.server.exceptions.response.ErrorResponseProcessor<?> errorResponseProcessor, 
+                RedirectConfiguration redirectConfiguration, 
+                RedirectService redirectService, 
+                @Nullable PriorToLoginPersistence priorToLoginPersistence) {
+                super(errorResponseProcessor,redirectConfiguration,redirectService,priorToLoginPersistence);
+        }
 
 	@Override
 	protected MutableHttpResponse<?> httpResponseWithStatus(HttpRequest<?> request, AuthorizationException e) {
