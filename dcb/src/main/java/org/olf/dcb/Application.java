b02e7ddbf6c0cd6cb65c31a3a185611dc6c25521
Steve Osguthorpe <steve.osguthorpe@k-int.com>
2023-09-04 15:31:13 +0100

    refactor: SHARED static is still a thing.

--- dcb/src/main/java/org/olf/dcb/Application.java
+++ dcb/src/main/java/org/olf/dcb/Application.java
@@ -5,7 +5,6 @@ import java.net.URL;
 import java.time.Instant;
 import java.time.ZonedDateTime;
 
-import io.micronaut.context.ApplicationContext;
 import io.micronaut.core.annotation.TypeHint;
 import io.micronaut.runtime.Micronaut;
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
@@ -14,16 +13,7 @@ import io.swagger.v3.oas.annotations.info.Info;
 @TypeHint(value = { Instant[].class, ZonedDateTime[].class, URI[].class, URL[].class })
 @OpenAPIDefinition(info = @Info(title = "DCB", description = "Direct Consortial Borrowing Service", version = "1.0.0"))
 public class Application {
-	
-	private static ApplicationContext currentContext = null;
-	
-	public static ApplicationContext getCurrentContext() {
-		if (currentContext == null) throw new IllegalStateException("No current, running application context");
-		
-		return currentContext;
-	}
-
 	public static void main(String[] args) {
-		currentContext = Micronaut.run(Application.class, args);
+		Micronaut.run(Application.class, args);
 	}
 }
