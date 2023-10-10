16f25476f34466c1114c1856182986a4a7951504
Marc Johnson <marc@k-int.co.uk>
2023-10-10 15:36:04 +0100

    test: Remove test for directly fetching holdings from FOLIO DCB-479

--- dcb/src/test/java/org/olf/dcb/core/interaction/folio/ConsortialFolioHostLmsClientItemTests.java
+++ dcb/src/test/java/org/olf/dcb/core/interaction/folio/ConsortialFolioHostLmsClientItemTests.java
@@ -3,7 +3,6 @@ package org.olf.dcb.core.interaction.folio;
 import static io.micronaut.http.MediaType.APPLICATION_JSON;
 import static org.hamcrest.CoreMatchers.allOf;
 import static org.hamcrest.CoreMatchers.is;
-import static org.hamcrest.CoreMatchers.notNullValue;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.contains;
 import static org.hamcrest.Matchers.hasProperty;
@@ -43,71 +42,7 @@ class ConsortialFolioHostLmsClientItemTests {
 
 		hostLmsFixture.createFolioHostLms(HOST_LMS_CODE, "", "", "", "");
 	}
-
-	@Test
-	void shouldBeAbleToFetchHoldings(MockServerClient mockServerClient) {
-		// Arrange
-		final var instanceId = UUID.randomUUID().toString();
-
-		mockHoldingsByInstanceId(mockServerClient, instanceId, OuterHoldings.builder()
-			.holdings(List.of(
-				OuterHolding.builder()
-					.instanceId(instanceId)
-					.holdings(List.of(
-						Holding.builder()
-							.id("ed26adb1-2e23-4aa6-a8cc-2f9892b10cf2")
-							.callNumber("QA273.A5450 1984")
-							.location("Crerar, Lower Level, Bookstacks")
-							.status("Available")
-							.permanentLoanType("stks")
-							.build(),
-						Holding.builder()
-							.id("eee7ded7-28cd-4a1d-9bbf-9e155cbe60b3")
-							.callNumber("QA273.A5450 1984")
-							.location("Social Service Administration")
-							.status("Available")
-							.permanentLoanType("stks")
-							.build()
-					))
-					.build()
-			))
-			.build());
-
-		final var client = hostLmsFixture.createFolioClient(HOST_LMS_CODE, httpClient);
-
-		// Act
-		final var response = client.getHoldings(instanceId).block();
-
-		// Assert
-		assertThat("Response should not be null", response, is(notNullValue()));
-		assertThat("Should have 1 outer holdings", response.getHoldings(), hasSize(1));
-
-		final var onlyOuterHolding = response.getHoldings().get(0);
-
-		assertThat("Should have instance ID",
-			onlyOuterHolding.getInstanceId(), is(instanceId));
-
-		assertThat("Should have 2 holdings", onlyOuterHolding.getHoldings(), hasSize(2));
-
-		assertThat("Holdings should have expected properties", onlyOuterHolding.getHoldings(),
-			contains(
-				allOf(
-					hasProperty("id", is("ed26adb1-2e23-4aa6-a8cc-2f9892b10cf2")),
-					hasProperty("callNumber", is("QA273.A5450 1984")),
-					hasProperty("location", is("Crerar, Lower Level, Bookstacks")),
-					hasProperty("status", is("Available")),
-					hasProperty("permanentLoanType", is("stks"))
-				),
-				allOf(
-					hasProperty("id", is("eee7ded7-28cd-4a1d-9bbf-9e155cbe60b3")),
-					hasProperty("callNumber", is("QA273.A5450 1984")),
-					hasProperty("location", is("Social Service Administration")),
-					hasProperty("status", is("Available")),
-					hasProperty("permanentLoanType", is("stks"))
-				)
-			));
-	}
-
+	
 	@Test
 	void shouldBeAbleToGetItems(MockServerClient mockServerClient) {
 		// Arrange
