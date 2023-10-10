e2eea971b7a55388280f2dd94738d18128be7f95
Marc Johnson <marc@k-int.co.uk>
2023-10-10 09:35:39 +0100

    chore: Remove unused code from FOLIO LMS client DCB-479

--- dcb/src/main/java/org/olf/dcb/core/interaction/folio/FolioLmsClient.java
+++ dcb/src/main/java/org/olf/dcb/core/interaction/folio/FolioLmsClient.java
@@ -1,9 +1,6 @@
 package org.olf.dcb.core.interaction.folio;
 
-import static org.olf.dcb.core.Constants.UUIDs.NAMESPACE_DCB;
-
 import java.util.List;
-import java.util.UUID;
 
 import org.olf.dcb.core.interaction.Bib;
 import org.olf.dcb.core.interaction.CreateItemCommand;
@@ -14,26 +11,16 @@ import org.olf.dcb.core.interaction.HostLmsPropertyDefinition;
 import org.olf.dcb.core.interaction.Patron;
 import org.olf.dcb.core.model.HostLms;
 import org.olf.dcb.core.model.Item;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 import io.micronaut.context.annotation.Parameter;
-import jakarta.validation.constraints.NotNull;
 import reactor.core.publisher.Mono;
 import reactor.util.function.Tuple2;
-import services.k_int.interaction.oaipmh.OaiRecord;
-import services.k_int.utils.UUIDUtils;
 
 /**
  */
 // @Prototype
 public class FolioLmsClient implements HostLmsClient {
-
-	private static final Logger log = LoggerFactory.getLogger(FolioLmsClient.class);
-
-	private static final String UUID5_PREFIX = "ingest-source:folio-lms";
 	private final HostLms lms;
-	// private final FolioApiClient client;
 
 	public FolioLmsClient(@Parameter HostLms lms) {
 		this.lms = lms;
@@ -54,15 +41,6 @@ public class FolioLmsClient implements HostLmsClient {
 		return Mono.empty();
 	}
 
-	// public Mono<String> createPatron(String uniqueId, String patronType) {
-	// return Mono.empty();
-	// }
-
-	public Mono<Patron> patronFind(String tag, String content) {
-		return Mono.empty();
-	}
-
-	// (localHoldId, localHoldStatus)
 	@Override
 	public Mono<Tuple2<String, String>> placeHoldRequest(String id, String recordType, String recordNumber,
 			String pickupLocation, String note, String patronRequestId) {
@@ -83,13 +61,7 @@ public class FolioLmsClient implements HostLmsClient {
 	public Mono<Patron> getPatronByLocalId(String localPatronId) {
 		return Mono.empty();
 	}
-
-
-	public UUID uuid5ForOAIResult(@NotNull final OaiRecord result) {
-		final String concat = UUID5_PREFIX + ":" + lms.getCode() + ":" + result.header().identifier();
-		return UUIDUtils.nameUUIDFromNamespaceAndString(NAMESPACE_DCB, concat);
-	}
-
+	
 	@Override
 	public Mono<String> createPatron(Patron patron) {
 		return Mono.empty();
@@ -125,6 +97,7 @@ public class FolioLmsClient implements HostLmsClient {
 		return Mono.empty();
 	}
 
+	@Override
 	public Mono<String> updateItemStatus(String itemId, CanonicalItemState crs) {
 		return Mono.just("Dummy");
 	}
