a816223c19c64bae8f591fb9ff644e4a41b31b9a
Marc Johnson <marc@k-int.co.uk>
2023-10-11 09:51:31 +0100

    chore: Reformat item type mapper

--- dcb/src/main/java/org/olf/dcb/core/interaction/shared/ItemTypeMapper.java
+++ dcb/src/main/java/org/olf/dcb/core/interaction/shared/ItemTypeMapper.java
@@ -1,47 +1,39 @@
 package org.olf.dcb.core.interaction.shared;
 
-import jakarta.inject.Singleton;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import reactor.core.publisher.Mono;
-
 import org.olf.dcb.storage.NumericRangeMappingRepository;
 
-/**
- *
- */
+import jakarta.inject.Singleton;
+import lombok.extern.slf4j.Slf4j;
+import reactor.core.publisher.Mono;
+
+@Slf4j
 @Singleton
 public class ItemTypeMapper {
-        private static final Logger log = LoggerFactory.getLogger(ItemTypeMapper.class);
+	private final NumericRangeMappingRepository numericRangeMappingRepository;
 
-        private NumericRangeMappingRepository numericRangeMappingRepository;
+	public ItemTypeMapper(NumericRangeMappingRepository numericRangeMappingRepository) {
+		this.numericRangeMappingRepository = numericRangeMappingRepository;
+	}
 
-        public ItemTypeMapper(NumericRangeMappingRepository numericRangeMappingRepository) {
-                this.numericRangeMappingRepository = numericRangeMappingRepository;
-        }
+	// Sierra item type comes from fixed field 61 - see https://documentation.iii.com/sierrahelp/Content/sril/sril_records_fixed_field_types_item.html
+	public Mono<String> getCanonicalItemType(String system, String localItemTypeCode) {
+		log.debug("map({},{})", system, localItemTypeCode);
 
-        // Sierra item type comes from fixed field 61 - see https://documentation.iii.com/sierrahelp/Content/sril/sril_records_fixed_field_types_item.html
-        public Mono<String> getCanonicalItemType(String system, String localItemTypeCode) {
-
-                log.debug("map({},{})", system, localItemTypeCode);
-
-                // Sierra item types are integers and they are usually mapped by a range
-                // I have a feelig that creating a static cache of system->localItemType mappings will have solid performance
-                // benefits
-                if ( localItemTypeCode != null ) {
-                        try {
-                                Long l = Long.valueOf(localItemTypeCode);
-                                log.debug("Look up item type {}",l);
-                                return Mono.from(numericRangeMappingRepository.findMappedValueFor(system, "ItemType", "DCB", l))
-                                        .doOnNext(nrm -> log.debug("nrm: {}",nrm))
-                                        .defaultIfEmpty( "UNKNOWN");
-                        }
-                        catch ( Exception e ) {
-                                log.warn("Problem trying to convert {} into  long value",localItemTypeCode);
-                        }
-                }
-                log.warn("No localItemType provided - returning UNKNOWN");
-                return Mono.just("UNKNOWN");
-        }
+		// Sierra item types are integers. They are usually mapped by a range
+		// I have a feeling that creating a static cache of system->localItemType mappings will have solid performance
+		// benefits
+		if (localItemTypeCode != null) {
+			try {
+				Long l = Long.valueOf(localItemTypeCode);
+				log.debug("Look up item type {}", l);
+				return Mono.from(numericRangeMappingRepository.findMappedValueFor(system, "ItemType", "DCB", l))
+					.doOnNext(nrm -> log.debug("nrm: {}", nrm))
+					.defaultIfEmpty("UNKNOWN");
+			} catch (Exception e) {
+				log.warn("Problem trying to convert {} into  long value", localItemTypeCode);
+			}
+		}
+		log.warn("No localItemType provided - returning UNKNOWN");
+		return Mono.just("UNKNOWN");
+	}
 }
-
