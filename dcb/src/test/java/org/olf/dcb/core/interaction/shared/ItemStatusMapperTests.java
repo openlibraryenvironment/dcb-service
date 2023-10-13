9f37c0f08e92989bb283b18f6d7d19133440a66e
Marc Johnson <marc@k-int.co.uk>
2023-10-13 07:52:57 +0100

    test: Move Sierra specific status mapping tests to nested class DCB-479

--- dcb/src/test/java/org/olf/dcb/core/interaction/shared/ItemStatusMapperTests.java
+++ dcb/src/test/java/org/olf/dcb/core/interaction/shared/ItemStatusMapperTests.java
@@ -11,6 +11,7 @@ import static org.olf.dcb.core.model.ItemStatusCode.UNAVAILABLE;
 import static org.olf.dcb.core.model.ItemStatusCode.UNKNOWN;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
@@ -36,61 +37,62 @@ class ItemStatusMapperTests {
 		referenceValueMappingFixture.deleteAll();
 	}
 
-	@Test
-	void statusIsAvailableWhenCodeIsHyphenAndDueDateIsNotPresent() {
-		final var mappedStatus = mapStatus(new Status("-", "AVAILABLE", null));
+	@Nested
+	class SierraFallbackMappingTests {
+		@Test
+		void statusIsAvailableWhenCodeIsHyphenAndDueDateIsNotPresent() {
+			final var mappedStatus = mapStatus(new Status("-", "AVAILABLE", null));
 
-		assertThat(mappedStatus, is(notNullValue()));
-		assertThat(mappedStatus.getCode(), is(AVAILABLE));
-	}
+			assertThat(mappedStatus, is(notNullValue()));
+			assertThat(mappedStatus.getCode(), is(AVAILABLE));
+		}
 
-	@Test
-	void statusIsAvailableWhenCodeIsHyphenAndDueDateIsBlank() {
-		final var mappedStatus = mapStatus(new Status("-", "AVAILABLE", ""));
+		@Test
+		void statusIsAvailableWhenCodeIsHyphenAndDueDateIsBlank() {
+			final var mappedStatus = mapStatus(new Status("-", "AVAILABLE", ""));
 
-		assertThat(mappedStatus, is(notNullValue()));
-		assertThat(mappedStatus.getCode(), is(AVAILABLE));
-	}
+			assertThat(mappedStatus, is(notNullValue()));
+			assertThat(mappedStatus.getCode(), is(AVAILABLE));
+		}
 
-	@Test
-	void statusIsCheckedOutWhenCodeIsHyphenAndDueDateIsPresent() {
-		final var mappedStatus = mapStatus(new Status("-", "AVAILABLE", "2023-04-22T15:55:13Z"));
+		@Test
+		void statusIsCheckedOutWhenCodeIsHyphenAndDueDateIsPresent() {
+			final var mappedStatus = mapStatus(new Status("-", "AVAILABLE", "2023-04-22T15:55:13Z"));
 
-		assertThat(mappedStatus, is(notNullValue()));
-		assertThat(mappedStatus.getCode(), is(CHECKED_OUT));
-	}
+			assertThat(mappedStatus, is(notNullValue()));
+			assertThat(mappedStatus.getCode(), is(CHECKED_OUT));
+		}
 
-	/**
-	 * These codes come from the
-	 * <a href="https://documentation.iii.com/sierrahelp/Content/sril/sril_records_fixed_field_types_item.html#item%20STATUS">
-	 * this documentation</a> of standard codes
-	 */
-	@ParameterizedTest
-	@CsvSource({"m,MISSING", "!,ON HOLDSHELF", "$,BILLED PAID", "n,BILLED NOTPAID",
-		"z,CL RETURNED", "o,LIB USE ONLY", "t,IN TRANSIT"})
-	void statusIsUnavailableWhenCodeAnythingOtherThanHyphen(String code,
-		String displayText) {
+		/**
+		 * These codes come from the
+		 * <a href="https://documentation.iii.com/sierrahelp/Content/sril/sril_records_fixed_field_types_item.html#item%20STATUS">
+		 * this documentation</a> of standard codes
+		 */
+		@ParameterizedTest
+		@CsvSource({"m,MISSING", "!,ON HOLDSHELF", "$,BILLED PAID", "n,BILLED NOTPAID",
+			"z,CL RETURNED", "o,LIB USE ONLY", "t,IN TRANSIT"})
+		void statusIsUnavailableWhenCodeAnythingOtherThanHyphen(String code, String displayText) {
+			final var mappedStatus = mapStatus(new Status(code, displayText, null));
 
-		final var mappedStatus = mapStatus(new Status(code, displayText, null));
+			assertThat(mappedStatus, is(notNullValue()));
+			assertThat(mappedStatus.getCode(), is(UNAVAILABLE));
+		}
 
-		assertThat(mappedStatus, is(notNullValue()));
-		assertThat(mappedStatus.getCode(), is(UNAVAILABLE));
-	}
+		@Test
+		void statusIsUnknownWhenCodeIsEmpty() {
+			final var mappedStatus = mapStatus(new Status("", "", ""));
 
-	@Test
-	void statusIsUnknownWhenCodeIsEmpty() {
-		final var mappedStatus = mapStatus(new Status("", "", ""));
+			assertThat(mappedStatus, is(notNullValue()));
+			assertThat(mappedStatus.getCode(), is(UNKNOWN));
+		}
 
-		assertThat(mappedStatus, is(notNullValue()));
-		assertThat(mappedStatus.getCode(), is(UNKNOWN));
-	}
+		@Test
+		void statusIsUnknownWhenSierraStatusIsNull() {
+			final var mappedStatus = mapStatus(null);
 
-	@Test
-	void statusIsUnknownWhenSierraStatusIsNull() {
-		final var mappedStatus = mapStatus(null);
-
-		assertThat(mappedStatus, is(notNullValue()));
-		assertThat(mappedStatus.getCode(), is(UNKNOWN));
+			assertThat(mappedStatus, is(notNullValue()));
+			assertThat(mappedStatus.getCode(), is(UNKNOWN));
+		}
 	}
 
 	@Test
