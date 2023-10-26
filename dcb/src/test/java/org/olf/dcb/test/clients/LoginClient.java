be16abf21216993090da997ed8d5d195a2100477
Marc Johnson <marc@k-int.co.uk>
2023-10-26 08:55:06 +0100

    test: Use declarative client for login DCB-519

--- dcb/src/test/java/org/olf/dcb/test/clients/LoginClient.java
+++ dcb/src/test/java/org/olf/dcb/test/clients/LoginClient.java
@@ -1,29 +1,23 @@
 package org.olf.dcb.test.clients;
 
-import io.micronaut.http.HttpRequest;
-import io.micronaut.http.client.HttpClient;
+import io.micronaut.http.annotation.Body;
+import io.micronaut.http.annotation.Post;
 import io.micronaut.http.client.annotation.Client;
 import io.micronaut.security.authentication.UsernamePasswordCredentials;
 import io.micronaut.security.token.render.BearerAccessRefreshToken;
-import jakarta.inject.Singleton;
 
-@Singleton
-public class LoginClient {
-	private final HttpClient httpClient;
-
-	public LoginClient(@Client("/") HttpClient httpClient) {
-		this.httpClient = httpClient;
-	}
-
-	public String getAccessToken() {
+@Client("/")
+public interface LoginClient {
+	default String getAccessToken() {
 		return login("admin", "password").getAccessToken();
 	}
 
-	public BearerAccessRefreshToken login(String username, String password) {
+	default BearerAccessRefreshToken login(String username, String password) {
 		final var credentials = new UsernamePasswordCredentials(username, password);
 
-		final var loginRequest = HttpRequest.POST("/login", credentials);
-
-		return httpClient.toBlocking().exchange(loginRequest, BearerAccessRefreshToken.class).body();
+		return login(credentials);
 	}
+
+	@Post("/login")
+	BearerAccessRefreshToken login(@Body UsernamePasswordCredentials credentials);
 }
