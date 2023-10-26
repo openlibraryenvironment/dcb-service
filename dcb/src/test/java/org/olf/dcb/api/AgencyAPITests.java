53ab0eed8836b49cf3c26e7628bc759f966e3d1b
Marc Johnson <marc@k-int.co.uk>
2023-10-26 08:19:59 +0100

    test: Use login client in agency API tests DCB-519

--- dcb/src/test/java/org/olf/dcb/api/AgencyAPITests.java
+++ dcb/src/test/java/org/olf/dcb/api/AgencyAPITests.java
@@ -1,27 +1,27 @@
 package org.olf.dcb.api;
 
-import io.micronaut.core.type.Argument;
-import io.micronaut.data.model.Page;
-import io.micronaut.http.HttpRequest;
-import io.micronaut.http.client.BlockingHttpClient;
-import io.micronaut.http.client.HttpClient;
-import io.micronaut.http.client.annotation.Client;
-import io.micronaut.security.authentication.UsernamePasswordCredentials;
-import io.micronaut.security.token.render.BearerAccessRefreshToken;
-import io.micronaut.test.extensions.junit5.annotation.MicronautTest;
-import jakarta.inject.Inject;
+import static io.micronaut.http.HttpStatus.OK;
+import static java.util.UUID.randomUUID;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
 import org.olf.dcb.core.api.serde.AgencyDTO;
 import org.olf.dcb.storage.postgres.PostgresAgencyRepository;
 import org.olf.dcb.test.HostLmsFixture;
+import org.olf.dcb.test.clients.LoginClient;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import static io.micronaut.http.HttpStatus.OK;
-import static java.util.UUID.randomUUID;
-import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.Matchers.is;
+
+import io.micronaut.core.type.Argument;
+import io.micronaut.data.model.Page;
+import io.micronaut.http.HttpRequest;
+import io.micronaut.http.client.HttpClient;
+import io.micronaut.http.client.annotation.Client;
+import io.micronaut.test.extensions.junit5.annotation.MicronautTest;
+import jakarta.inject.Inject;
 
 @MicronautTest(transactional = false, rebuildContext = true)
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
@@ -31,10 +31,15 @@ class AgencyAPITests {
 	@Inject
 	@Client("/")
 	private HttpClient client;
+
 	@Inject
 	private PostgresAgencyRepository agencyRepository;
+
 	@Inject
 	private HostLmsFixture hostLmsFixture;
+
+	@Inject
+	private LoginClient loginClient;
 
 	@BeforeEach
 	void beforeEach() {
@@ -45,21 +50,29 @@ class AgencyAPITests {
 	@Test
 	void shouldReturnAgenciesByUsingAPItoSaveAndList() {
 		// Arrange
-                log.info("get client");
-		final var blockingClient = client.toBlocking();
-                log.info("create hostLmsCode");
-		final var hostLms = hostLmsFixture.createHostLms(randomUUID(), "hostLmsCode");
-		final var accessToken = getAccessToken(blockingClient);
-                log.info("create agency");
+		log.info("get client");
+		log.info("create hostLmsCode");
+
+		hostLmsFixture.createHostLms(randomUUID(), "hostLmsCode");
+
+		final var accessToken = loginClient.getAccessToken();
+
+		log.info("create agency");
 		final var agencyDTO = AgencyDTO.builder().id(randomUUID()).code("ab6").name("agencyName")
 			.authProfile("authProfile").idpUrl("idpUrl").hostLMSCode("hostLmsCode").build();
-                log.info("get agencies");
+
+		log.info("get agencies");
+		final var blockingClient = client.toBlocking();
 		final var postRequest = HttpRequest.POST("/agencies", agencyDTO).bearerAuth(accessToken);
-		final var postResponse = blockingClient.exchange(postRequest, AgencyDTO.class);
-                log.info("get agencies page");
+
+		blockingClient.exchange(postRequest, AgencyDTO.class);
+
+		log.info("get agencies page");
 		final var listRequest = HttpRequest.GET("/agencies?page=0&size=10").bearerAuth(accessToken);
+
 		// Act
 		final var listResponse = blockingClient.exchange(listRequest, Argument.of(Page.class, AgencyDTO.class));
+
 		// Assert
 		assertThat(listResponse.getStatus(), is(OK));
 		assertThat(listResponse.getBody().isPresent(), is(true));
@@ -73,14 +86,5 @@ class AgencyAPITests {
 		assertThat(onlySavedAgency.getAuthProfile(), is("authProfile"));
 		assertThat(onlySavedAgency.getIdpUrl(), is("idpUrl"));
 		assertThat(onlySavedAgency.getHostLMSCode(), is("hostLmsCode")); // showing work around works
-	}
-
-	private static String getAccessToken(BlockingHttpClient blockingClient) {
-		final var creds = new UsernamePasswordCredentials("admin", "password");
-		final var loginRequest = HttpRequest.POST("/login", creds);
-		final var loginResponse = blockingClient.exchange(loginRequest, BearerAccessRefreshToken.class);
-		final var bearerAccessRefreshToken = loginResponse.body();
-		final var accessToken = bearerAccessRefreshToken.getAccessToken();
-		return accessToken;
 	}
 }
