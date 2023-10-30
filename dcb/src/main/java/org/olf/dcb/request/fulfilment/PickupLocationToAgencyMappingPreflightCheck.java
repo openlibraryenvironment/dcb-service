62e2997547d83504acc4393c2628991fe7cfb6b6
Marc Johnson <marc@k-int.co.uk>
2023-10-30 10:08:13 +0000

    refactor: Add reference value mapping service dependency to pickup location to agency mapping check DCB-519

--- dcb/src/main/java/org/olf/dcb/request/fulfilment/PickupLocationToAgencyMappingPreflightCheck.java
+++ dcb/src/main/java/org/olf/dcb/request/fulfilment/PickupLocationToAgencyMappingPreflightCheck.java
@@ -3,11 +3,19 @@ package org.olf.dcb.request.fulfilment;
 import java.util.Collections;
 import java.util.List;
 
+import org.olf.dcb.core.svc.ReferenceValueMappingService;
+
 import jakarta.inject.Singleton;
 import reactor.core.publisher.Mono;
 
 @Singleton
 public class PickupLocationToAgencyMappingPreflightCheck implements PreflightCheck {
+	private final ReferenceValueMappingService referenceValueMappingService;
+
+	public PickupLocationToAgencyMappingPreflightCheck(ReferenceValueMappingService referenceValueMappingService) {
+		this.referenceValueMappingService = referenceValueMappingService;
+	}
+
 	@Override
 	public Mono<List<CheckResult>> check(PlacePatronRequestCommand command) {
 		return Mono.just(Collections.emptyList());
