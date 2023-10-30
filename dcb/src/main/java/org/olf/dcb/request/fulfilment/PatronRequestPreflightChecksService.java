ed797cf9454f423bc36923648aff368a0f4aa57d
Marc Johnson <marc@k-int.co.uk>
2023-10-30 07:01:38 +0000

    chore: Add dependency on location repository to preflight checks service DCB-519

--- dcb/src/main/java/org/olf/dcb/request/fulfilment/PatronRequestPreflightChecksService.java
+++ dcb/src/main/java/org/olf/dcb/request/fulfilment/PatronRequestPreflightChecksService.java
@@ -3,11 +3,19 @@ package org.olf.dcb.request.fulfilment;
 import java.util.List;
 import java.util.Objects;
 
+import org.olf.dcb.storage.LocationRepository;
+
 import jakarta.inject.Singleton;
 import reactor.core.publisher.Mono;
 
 @Singleton
 public class PatronRequestPreflightChecksService {
+	private final LocationRepository locationRepository;
+
+	public PatronRequestPreflightChecksService(LocationRepository locationRepository) {
+		this.locationRepository = locationRepository;
+	}
+
 	public Mono<PlacePatronRequestCommand> check(PlacePatronRequestCommand command) {
 		final var pickupLocationCode = command.getPickupLocation().getCode();
 
