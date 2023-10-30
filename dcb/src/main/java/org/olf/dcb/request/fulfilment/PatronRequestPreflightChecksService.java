bc124c3a9a8af742bd44f8c9ca779c1a000468c9
Marc Johnson <marc@k-int.co.uk>
2023-10-30 08:44:14 +0000

    refactor: Reduce flux of preflight checks DCB-519

--- dcb/src/main/java/org/olf/dcb/request/fulfilment/PatronRequestPreflightChecksService.java
+++ dcb/src/main/java/org/olf/dcb/request/fulfilment/PatronRequestPreflightChecksService.java
@@ -1,6 +1,10 @@
 package org.olf.dcb.request.fulfilment;
 
+import java.util.List;
+
+import graphql.com.google.common.collect.Streams;
 import jakarta.inject.Singleton;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 @Singleton
@@ -12,7 +16,9 @@ public class PatronRequestPreflightChecksService {
 	}
 
 	public Mono<PlacePatronRequestCommand> check(PlacePatronRequestCommand command) {
-		return pickupLocationPreflightCheck.check(command)
+		return Flux.just(pickupLocationPreflightCheck)
+			.concatMap(check -> check.check(command))
+			.reduce(PatronRequestPreflightChecksService::concatenateChecks)
 			.flatMap(results -> {
 				if (results.stream().allMatch(CheckResult::getPassed)) {
 					return Mono.just(command);
@@ -29,5 +35,11 @@ public class PatronRequestPreflightChecksService {
 					.failedChecks(failedChecks)
 					.build());
 			});
+	}
+
+	private static List<CheckResult> concatenateChecks(List<CheckResult> firstChecks,
+		List<CheckResult> secondChecks) {
+
+		return Streams.concat(firstChecks.stream(), secondChecks.stream()).toList();
 	}
 }
