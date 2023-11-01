e4bfdee72775a1cb7e6c6860e321813837cbad83
Marc Johnson <marc@k-int.co.uk>
2023-11-01 10:10:54 +0000

    refactor: Move find by ID string overload to location service DCB-519

--- dcb/src/main/java/org/olf/dcb/core/svc/LocationService.java
+++ dcb/src/main/java/org/olf/dcb/core/svc/LocationService.java
@@ -23,4 +23,22 @@ public class LocationService {
 	public Mono<Location> findByCode(String pickupLocationCode) {
 		return Mono.from(locationRepository.findOneByCode(pickupLocationCode));
 	}
+
+	/**
+	 * Attempt to convert string ID into a UUID before finding by ID
+	 *
+	 * @param id the ID to find a location for as a string
+	 * @return empty if the ID is not a UUID, otherwise the result of finding a location by ID
+	 */
+	public Mono<Location> findById(String id) {
+		try {
+			final var parsedId = UUID.fromString(id);
+
+			return findById(parsedId);
+		}
+		// Is not a UUID
+		catch (IllegalArgumentException e) {
+			return Mono.empty();
+		}
+	}
 }
--- dcb/src/main/java/org/olf/dcb/request/fulfilment/PickupLocationPreflightCheck.java
+++ dcb/src/main/java/org/olf/dcb/request/fulfilment/PickupLocationPreflightCheck.java
@@ -1,24 +1,17 @@
 package org.olf.dcb.request.fulfilment;
 
 import java.util.List;
-import java.util.UUID;
 
-import org.olf.dcb.core.model.Location;
 import org.olf.dcb.core.svc.LocationService;
-import org.olf.dcb.storage.LocationRepository;
 
 import jakarta.inject.Singleton;
 import reactor.core.publisher.Mono;
 
 @Singleton
 public class PickupLocationPreflightCheck implements PreflightCheck {
-	private final LocationRepository locationRepository;
 	private final LocationService locationService;
 
-	public PickupLocationPreflightCheck(LocationRepository locationRepository,
-		LocationService locationService) {
-
-		this.locationRepository = locationRepository;
+	public PickupLocationPreflightCheck(LocationService locationService) {
 		this.locationService = locationService;
 	}
 
@@ -27,27 +20,9 @@ public class PickupLocationPreflightCheck implements PreflightCheck {
 		final var pickupLocationCode = command.getPickupLocationCode();
 
 		return locationService.findByCode(pickupLocationCode)
-			.switchIfEmpty(findById(pickupLocationCode))
+			.switchIfEmpty(locationService.findById(pickupLocationCode))
 			.map(location -> CheckResult.passed())
 			.defaultIfEmpty(CheckResult.failed("\"" + pickupLocationCode + "\" is not a recognised pickup location code"))
 			.map(List::of);
-	}
-
-	/**
-	 * Use the pickup location code which can sometimes be an ID to find a location record
-	 *
-	 * @param pickupLocationCode the code might actually be an ID
-	 * @return empty if the code is not a UUID, otherwise the result of finding a location by ID
-	 */
-	private Mono<Location> findById(String pickupLocationCode) {
-		try {
-			final var id = UUID.fromString(pickupLocationCode);
-
-			return locationService.findById(id);
-		}
-		// Code is not a UUID
-		catch (IllegalArgumentException e) {
-			return Mono.empty();
-		}
 	}
 }
