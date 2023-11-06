eab13a3b0522e7a3e0f00106f57d5eb7981aa722
Marc Johnson <marc@k-int.co.uk>
2023-11-06 08:02:18 +0000

    refactor: Include host LMS service dependency for resolve patron preflight check DCB-531

--- dcb/src/main/java/org/olf/dcb/request/fulfilment/ResolvePatronPreflightCheck.java
+++ dcb/src/main/java/org/olf/dcb/request/fulfilment/ResolvePatronPreflightCheck.java
@@ -2,11 +2,19 @@ package org.olf.dcb.request.fulfilment;
 
 import java.util.List;
 
+import org.olf.dcb.core.HostLmsService;
+
 import jakarta.inject.Singleton;
 import reactor.core.publisher.Mono;
 
 @Singleton
 public class ResolvePatronPreflightCheck implements PreflightCheck {
+	private final HostLmsService hostLmsService;
+
+	public ResolvePatronPreflightCheck(HostLmsService hostLmsService) {
+		this.hostLmsService = hostLmsService;
+	}
+
 	@Override
 	public Mono<List<CheckResult>> check(PlacePatronRequestCommand command) {
 		return Mono.just(List.of(CheckResult.passed()));
