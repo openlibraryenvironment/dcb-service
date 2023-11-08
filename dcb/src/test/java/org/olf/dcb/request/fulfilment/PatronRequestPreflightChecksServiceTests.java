22f4c5d4508bd62073a066d9c8515a87484cf9bb
Marc Johnson <marc@k-int.co.uk>
2023-11-08 06:48:32 +0000

    test: Delete all check related state prior to preflight checks service tests DCB-612

--- dcb/src/test/java/org/olf/dcb/request/fulfilment/PatronRequestPreflightChecksServiceTests.java
+++ dcb/src/test/java/org/olf/dcb/request/fulfilment/PatronRequestPreflightChecksServiceTests.java
@@ -4,8 +4,13 @@ import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.notNullValue;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.olf.dcb.test.AgencyFixture;
 import org.olf.dcb.test.DcbTest;
+import org.olf.dcb.test.HostLmsFixture;
+import org.olf.dcb.test.LocationFixture;
+import org.olf.dcb.test.ReferenceValueMappingFixture;
 
 import jakarta.inject.Inject;
 
@@ -13,6 +18,25 @@ import jakarta.inject.Inject;
 class PatronRequestPreflightChecksServiceTests {
 	@Inject
 	private PatronRequestPreflightChecksService preflightChecksService;
+
+	@Inject
+	private ReferenceValueMappingFixture referenceValueMappingFixture;
+	@Inject
+	private AgencyFixture agencyFixture;
+	@Inject
+	private LocationFixture locationFixture;
+	@Inject
+	private HostLmsFixture hostLmsFixture;
+
+	@BeforeEach
+	void beforeEach() {
+		// Remove all the state required by the underlying checks
+		// This is brittle WRT new checks being introduced
+		referenceValueMappingFixture.deleteAll();
+		agencyFixture.deleteAll();
+		locationFixture.deleteAll();
+		hostLmsFixture.deleteAll();
+	}
 
 	@Test
 	void shouldBeInstantiated() {
