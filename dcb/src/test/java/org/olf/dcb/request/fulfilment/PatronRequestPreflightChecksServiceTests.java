fcbb7dc26d86df179c89677c10a03e74e1ad9ac1
Marc Johnson <marc@k-int.co.uk>
2023-11-10 08:33:39 +0000

    test: Remove redundant preparation in preflight checks service tests DCB-612
    now that only the always failing check is included

--- dcb/src/test/java/org/olf/dcb/request/fulfilment/PatronRequestPreflightChecksServiceTests.java
+++ dcb/src/test/java/org/olf/dcb/request/fulfilment/PatronRequestPreflightChecksServiceTests.java
@@ -4,18 +4,12 @@ import static org.hamcrest.CoreMatchers.instanceOf;
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.containsInAnyOrder;
-import static org.hamcrest.Matchers.notNullValue;
 
 import java.util.Collection;
 import java.util.List;
 
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.olf.dcb.test.AgencyFixture;
 import org.olf.dcb.test.DcbTest;
-import org.olf.dcb.test.HostLmsFixture;
-import org.olf.dcb.test.LocationFixture;
-import org.olf.dcb.test.ReferenceValueMappingFixture;
 
 import io.micronaut.context.annotation.Property;
 import io.micronaut.context.annotation.Requires;
@@ -32,37 +26,15 @@ import reactor.core.publisher.Mono;
 class PatronRequestPreflightChecksServiceTests extends AbstractPreflightCheckTests {
 	@Inject
 	private PatronRequestPreflightChecksService preflightChecksService;
-
-	@Inject
-	private ReferenceValueMappingFixture referenceValueMappingFixture;
-	@Inject
-	private AgencyFixture agencyFixture;
-	@Inject
-	private LocationFixture locationFixture;
-	@Inject
-	private HostLmsFixture hostLmsFixture;
-
 	@Inject
 	private Collection<PreflightCheck> preflightChecks;
 
-	@BeforeEach
-	void beforeEach() {
-		// Remove all the state required by the underlying checks
-		// This is brittle WRT new checks being introduced
-		referenceValueMappingFixture.deleteAll();
-		agencyFixture.deleteAll();
-		locationFixture.deleteAll();
-		hostLmsFixture.deleteAll();
-	}
-
 	@Test
 	void noChecksShouldBeExecutedWhenDisabled() {
-		assertThat("Service instance should not be null", preflightChecksService, is(notNullValue()));
+		final var command = placeRequestCommand("any-location-code",
+			"any-context", "any-host-lms-code");
 
-		final var command = placeRequestCommand("any-location-code", "any-context", "any-host-lms-code");
-
-		// Existing checks require state to pass,
-		// if they were executed they would fail and this would throw an exception
+		// Only included check should always fail if checked
 		assertThat("No checks should be made",
 			preflightChecksService.check(command).block(), is(command));
 	}
