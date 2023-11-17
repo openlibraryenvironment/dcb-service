69cab5af8056123eb144762cf5bb688f6ccbc8c4
Marc Johnson <marc@k-int.co.uk>
2023-11-17 08:41:47 +0000

    refactor: Convert property definition to value object DCB-739

--- dcb/src/main/java/org/olf/dcb/core/interaction/HostLmsPropertyDefinition.java
+++ dcb/src/main/java/org/olf/dcb/core/interaction/HostLmsPropertyDefinition.java
@@ -1,24 +1,17 @@
 package org.olf.dcb.core.interaction;
 
-import io.micronaut.core.annotation.Creator;
 import io.micronaut.serde.annotation.Serdeable;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.Value;
 import services.k_int.tests.ExcludeFromGeneratedCoverageReport;
 
-@Builder
-@Data
-@NoArgsConstructor(onConstructor_ = @Creator())
-@AllArgsConstructor
+@Value
 @Serdeable
 @ExcludeFromGeneratedCoverageReport
 public class HostLmsPropertyDefinition {
-	private String name;
-	private String description;
-	private Boolean mandatory;
-	private String typeCode;
+	String name;
+	String description;
+	Boolean mandatory;
+	String typeCode;
 
 	public static HostLmsPropertyDefinition urlPropertyDefinition(String name,
 		String description, Boolean mandatory) {
