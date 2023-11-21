fbaf607ff9d69683c5d318ace5239744b67274b7
Steve Osguthorpe <steve.osguthorpe@k-int.com>
2023-11-21 17:33:19 +0000

    fix(Tests): Require config prefix

--- dcb/src/main/java/org/olf/dcb/indexing/SharedIndexConfiguration.java
+++ dcb/src/main/java/org/olf/dcb/indexing/SharedIndexConfiguration.java
@@ -3,7 +3,9 @@ package org.olf.dcb.indexing;
 import java.util.Optional;
 
 import io.micronaut.context.annotation.ConfigurationProperties;
+import io.micronaut.context.annotation.Requires;
 
+@Requires(property = SharedIndexConfiguration.PREFIX)
 @ConfigurationProperties(SharedIndexConfiguration.PREFIX)
 public record SharedIndexConfiguration (
 		
