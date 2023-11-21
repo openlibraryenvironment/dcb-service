846c05ca9221e43126e3f025a1e22f497cdb0c88
Marc Johnson <marc@k-int.co.uk>
2023-11-21 13:02:55 +0000

    test: Demonstrate loading host LMS from config on startup DCB-739

---
+++ dcb/src/test/java/org/olf/dcb/utils/DCBStartupEventListenerTests.java
@@ -0,0 +1,45 @@
+package org.olf.dcb.utils;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.hasEntry;
+import static org.hamcrest.Matchers.hasProperty;
+import static org.junit.jupiter.api.TestInstance.Lifecycle.PER_CLASS;
+import static org.olf.dcb.test.matchers.HostLmsMatchers.hasCode;
+import static org.olf.dcb.test.matchers.HostLmsMatchers.hasName;
+import static org.olf.dcb.test.matchers.HostLmsMatchers.hasNonNullId;
+import static org.olf.dcb.test.matchers.HostLmsMatchers.hasType;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+import org.olf.dcb.core.HostLmsService;
+import org.olf.dcb.core.interaction.sierra.SierraLmsClient;
+import org.olf.dcb.test.DcbTest;
+
+import io.micronaut.context.annotation.Property;
+import io.micronaut.test.extensions.junit5.annotation.MicronautTest;
+import jakarta.inject.Inject;
+
+@DcbTest
+@MicronautTest(transactional = false, rebuildContext = true)
+@Property(name = "r2dbc.datasources.default.options.maxSize", value = "1")
+@Property(name = "r2dbc.datasources.default.options.initialSize", value = "1")
+@TestInstance(PER_CLASS)
+class DCBStartupEventListenerTests {
+	@Inject
+	private HostLmsService hostLmsService;
+
+	@Test
+	void shouldFindHostLmsInConfigByCode() {
+		// Act
+		final var foundHost = hostLmsService.findByCode("config-host").block();
+
+		// Assert
+		assertThat(foundHost, hasNonNullId());
+		assertThat(foundHost, hasCode("config-host"));
+		// The name in the config is ignored and the code is used for the name too
+		assertThat(foundHost, hasName("config-host"));
+		assertThat(foundHost, hasType(SierraLmsClient.class));
+		assertThat(foundHost, hasProperty("clientConfig",
+			hasEntry("base-url", "https://some-sierra-system")));
+	}
+}
