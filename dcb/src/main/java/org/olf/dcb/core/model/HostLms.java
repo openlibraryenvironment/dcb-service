ffb974c567895d4bd404f98f7b625b8cd0e8854d
Marc Johnson <marc@k-int.co.uk>
2023-11-21 10:51:33 +0000

    chore: Remove redundant pubic modifiers on host LMS interface DCB-739

--- dcb/src/main/java/org/olf/dcb/core/model/HostLms.java
+++ dcb/src/main/java/org/olf/dcb/core/model/HostLms.java
@@ -3,31 +3,19 @@ package org.olf.dcb.core.model;
 import java.util.Map;
 import java.util.UUID;
 
-import org.olf.dcb.core.interaction.HostLmsClient;
-
 import io.micronaut.core.annotation.Introspected;
-import io.micronaut.core.annotation.NonNull;
-import io.micronaut.data.annotation.Id;
-import io.micronaut.data.annotation.Transient;
 import io.micronaut.serde.annotation.Serdeable;
 
 @Serdeable
 @Introspected
 public interface HostLms {
-	
-	// @NonNull
-	// @Id
-	public UUID getId();
-	
-	// @NonNull
-	public String getCode();
-	
-	// @NonNull
-	public String getName();
-	
-        // @NonNull
-	public Class<?> getType();
-	
-	// @NonNull
+	UUID getId();
+
+	String getCode();
+
+	String getName();
+
+	Class<?> getType();
+
 	Map<String, Object> getClientConfig();
 }
