f79ab9594e4cb766e765b03178a119ae5cea0b47
Marc Johnson <marc@k-int.co.uk>
2023-11-21 20:06:01 +0000

    test: Use fixture to create host LMS during patron identity repository tests DCB-739

--- dcb/src/test/java/org/olf/dcb/storage/PatronIdentityRepoTests.java
+++ dcb/src/test/java/org/olf/dcb/storage/PatronIdentityRepoTests.java
@@ -8,19 +8,16 @@ import static org.hamcrest.MatcherAssert.assertThat;
 import static org.olf.dcb.test.PublisherUtils.singleValueFrom;
 
 import java.util.List;
-import java.util.Map;
 import java.util.UUID;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.olf.dcb.core.interaction.sierra.SierraLmsClient;
 import org.olf.dcb.core.model.DataHostLms;
 import org.olf.dcb.core.model.Patron;
 import org.olf.dcb.core.model.PatronIdentity;
-import org.olf.dcb.storage.HostLmsRepository;
-import org.olf.dcb.storage.PatronIdentityRepository;
-import org.olf.dcb.storage.PatronRepository;
 import org.olf.dcb.test.DcbTest;
+import org.olf.dcb.test.HostLmsFixture;
 
 import jakarta.inject.Inject;
 
@@ -30,32 +27,36 @@ class PatronIdentityRepoTests {
 	private PatronRepository patronRepository;
 	@Inject
 	private PatronIdentityRepository patronIdentityRepository;
+
 	@Inject
-	private HostLmsRepository hostLmsRepository;
+	private HostLmsFixture hostLmsFixture;
+
+	@BeforeEach
+	void beforeEach() {
+		hostLmsFixture.deleteAll();
+	}
 
 	@Test
 	@DisplayName("Should find patron identity by local ID, host LMS, and home identity")
 	void shouldFindPatronIdentityByLocalIdAndHostLmsAndHomeIdentity() {
 		// Arrange
-		final var dataHostLmsId = randomUUID();
-		final var dataHostLms = new DataHostLms(dataHostLmsId, "database-host",
-			"Database Host", SierraLmsClient.class.getName(), Map.of());
+		final var hostLms = hostLmsFixture.createSierraHostLms("some-host-lms");
+
 		final var patronId = randomUUID();
 		final var patronIdentityId = randomUUID();
 		final var patron = new Patron(patronId, null, null, null, List.of());
-		final var patronIdentity = createPatronIdentity(patronIdentityId, patron, dataHostLms, true);
+		final var patronIdentity = createPatronIdentity(patronIdentityId, patron, hostLms, true);
 
-		singleValueFrom(hostLmsRepository.save(dataHostLms));
 		singleValueFrom(patronRepository.save(patron));
 		singleValueFrom(patronIdentityRepository.save(patronIdentity));
 
 		// Act
-		final var foundPatronIdentity = findIdentity(dataHostLms, "localId");
+		final var foundPatronIdentity = findIdentity(hostLms, "localId");
 
 		// Assert
 		assertThat("PatronIdentity should not be null.", foundPatronIdentity, is(notNullValue()));
 		assertThat("Local ID should match", foundPatronIdentity.getLocalId(), is("localId"));
-		assertThat("Host LMS ID should match", foundPatronIdentity.getHostLms().getId(), is(dataHostLmsId));
+		assertThat("Host LMS ID should match", foundPatronIdentity.getHostLms().getId(), is(hostLms.getId()));
 		assertThat("Home Identity should be true", foundPatronIdentity.getHomeIdentity(), is(true));
 	}
 
@@ -63,15 +64,13 @@ class PatronIdentityRepoTests {
 	@DisplayName("Should not find match when home identity is false")
 	void shouldNotFindMatchWhenHomeIdentityFalse() {
 		// Arrange
-		final var dataHostLmsId = randomUUID();
-		final var dataHostLms = new DataHostLms(dataHostLmsId, "database-host",
-			"Database Host", SierraLmsClient.class.getName(), Map.of());
+		final var dataHostLms = hostLmsFixture.createSierraHostLms("some-host-lms");
+
 		final var patronId = randomUUID();
 		final var patronIdentityId = randomUUID();
 		final var patron = new Patron(patronId, null, null, null, List.of());
 		final var patronIdentity = createPatronIdentity(patronIdentityId, patron, dataHostLms, false);
 
-		singleValueFrom(hostLmsRepository.save(dataHostLms));
 		singleValueFrom(patronRepository.save(patron));
 		singleValueFrom(patronIdentityRepository.save(patronIdentity));
 
@@ -87,15 +86,13 @@ class PatronIdentityRepoTests {
 	@DisplayName("Should not find match when local ID is incorrect")
 	void shouldNotFindMatchWhenLocalIdIncorrect() {
 		// Arrange
-		final var dataHostLmsId = randomUUID();
-		final var dataHostLms = new DataHostLms(dataHostLmsId, "database-host",
-			"Database Host", SierraLmsClient.class.getName(), Map.of());
+		final var dataHostLms = hostLmsFixture.createSierraHostLms("some-host-lms");
+
 		final var patronId = randomUUID();
 		final var patronIdentityId = randomUUID();
 		final var patron = new Patron(patronId, null, null, null, List.of());
 		final var patronIdentity = createPatronIdentity(patronIdentityId, patron, dataHostLms, true);
 
-		singleValueFrom(hostLmsRepository.save(dataHostLms));
 		singleValueFrom(patronRepository.save(patron));
 		singleValueFrom(patronIdentityRepository.save(patronIdentity));
 
@@ -111,24 +108,20 @@ class PatronIdentityRepoTests {
 	@DisplayName("Should not find match when host LMS is incorrect")
 	void shouldNotFindMatchWhenHostLmsIncorrect() {
 		// Arrange
-		final var dataHostLmsId1 = randomUUID();
-		final var dataHostLms1 = new DataHostLms(dataHostLmsId1, "database-host",
-			"Database Host", SierraLmsClient.class.getName(), Map.of());
-		final var dataHostLmsId2 = randomUUID();
-		final var dataHostLms2 = new DataHostLms(dataHostLmsId2, "database-host",
-			"Database Host", SierraLmsClient.class.getName(), Map.of());
+		final var firstHostLms = hostLmsFixture.createSierraHostLms("first-host-lms");
+		final var secondHostLms = hostLmsFixture.createSierraHostLms("second-host-lms");
+
 		final var patronId = randomUUID();
 		final var patronIdentityId = randomUUID();
 		final var localId = "localId";
 		final var patron = new Patron(patronId, null, null, null, List.of());
-		final var patronIdentity = createPatronIdentity(patronIdentityId, patron, dataHostLms1, true);
+		final var patronIdentity = createPatronIdentity(patronIdentityId, patron, firstHostLms, true);
 
-		singleValueFrom(hostLmsRepository.save(dataHostLms1));
 		singleValueFrom(patronRepository.save(patron));
 		singleValueFrom(patronIdentityRepository.save(patronIdentity));
 
 		// Act
-		final var foundPatronIdentity = findIdentity(dataHostLms2, localId);
+		final var foundPatronIdentity = findIdentity(secondHostLms, localId);
 
 		// Assert
 		assertThat("Expected no patron identity to be found for the given criteria",
