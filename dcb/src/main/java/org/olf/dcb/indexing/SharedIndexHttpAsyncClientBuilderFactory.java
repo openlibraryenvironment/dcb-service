1ac329e4394f67f4ebca3954c567fd7389967d78
Steve Osguthorpe <steve.osguthorpe@k-int.com>
2023-11-21 17:06:12 +0000

    chore: Shout when cert verification is disabled

--- dcb/src/main/java/org/olf/dcb/indexing/SharedIndexHttpAsyncClientBuilderFactory.java
+++ dcb/src/main/java/org/olf/dcb/indexing/SharedIndexHttpAsyncClientBuilderFactory.java
@@ -7,10 +7,11 @@ import java.security.NoSuchAlgorithmException;
 import org.apache.http.auth.AuthScope;
 import org.apache.http.auth.UsernamePasswordCredentials;
 import org.apache.http.conn.ssl.TrustAllStrategy;
-import org.apache.http.conn.ssl.TrustSelfSignedStrategy;
 import org.apache.http.impl.client.BasicCredentialsProvider;
 import org.apache.http.impl.nio.client.HttpAsyncClientBuilder;
 import org.apache.http.ssl.SSLContextBuilder;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import io.micronaut.context.annotation.Bean;
 import io.micronaut.context.annotation.Factory;
@@ -23,7 +24,8 @@ import jakarta.inject.Singleton;
 @Factory
 public class SharedIndexHttpAsyncClientBuilderFactory {
 	
-	private static final String DEVELOPMENT_FULL = "development"; 
+	private static final String TRUST_ALL_CERTS = "trust-all-certs";
+	private static final Logger log = LoggerFactory.getLogger(SharedIndexHttpAsyncClientBuilderFactory.class);
 
 	@Bean
 	@Requires(bean = SharedIndexConfiguration.class)
@@ -33,10 +35,17 @@ public class SharedIndexHttpAsyncClientBuilderFactory {
 
 		var builder = HttpAsyncClientBuilder.create();
 		
-		if (env.getActiveNames().contains(Environment.DEVELOPMENT) || env.getActiveNames().contains(DEVELOPMENT_FULL)) {
+		if (env.getActiveNames().contains(TRUST_ALL_CERTS)) {
 			// We can turn OFF cert varification
 			builder.setSSLContext(new SSLContextBuilder().loadTrustMaterial(null, TrustAllStrategy.INSTANCE).build());
+
+			final String warningBanner =
+					"********************* WARNING ********************\n"
+				+ "* SSL Certificate verification has been disabled *\n"
+				+ "*          DO NOT DO THIS IN PRODUCTION          *\n"
+				+ "**************************************************";
 			
+			System.out.println(warningBanner);
 		}
 		
 		if (config.username().isEmpty() || config.password().isEmpty()) return builder;
