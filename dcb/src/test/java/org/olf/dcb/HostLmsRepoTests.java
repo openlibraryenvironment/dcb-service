c4868b7257f8b5105e0e600035cb1b70c07b0915
Marc Johnson <marc@k-int.co.uk>
2023-11-21 19:31:08 +0000

    test: Replace spaces with tabs in host LMS repository tests DCB-739

--- dcb/src/test/java/org/olf/dcb/HostLmsRepoTests.java
+++ dcb/src/test/java/org/olf/dcb/HostLmsRepoTests.java
@@ -12,8 +12,6 @@ import org.junit.jupiter.api.Test;
 import org.olf.dcb.core.model.DataHostLms;
 import org.olf.dcb.storage.HostLmsRepository;
 import org.olf.dcb.test.DcbTest;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 import io.micronaut.http.client.HttpClient;
 import io.micronaut.http.client.annotation.Client;
@@ -23,10 +21,6 @@ import reactor.core.publisher.Mono;
 
 @DcbTest
 class HostLmsRepoTests {
-
-        private final Logger log = LoggerFactory.getLogger(HostLmsRepoTests.class);
-
-
 	@Inject
 	@Client("/")
 	HttpClient client;
@@ -40,23 +34,18 @@ class HostLmsRepoTests {
 
 	@Test
 	void createHostLmsViaRepository() {
-		Map<String, Object> cfg = new HashMap<String,Object>();
-        	DataHostLms new_host_lms = new DataHostLms(UUID.randomUUID(),
-                                                           "TCODE1",
-                                                           "Test HostLMS1",
-                                                           "org.olf.dcb.core.interaction.sierra.SierraLmsClient",
-                                                           cfg);
+		Map<String, Object> cfg = new HashMap<String, Object>();
+		DataHostLms new_host_lms = new DataHostLms(UUID.randomUUID(),
+			"TCODE1",
+			"Test HostLMS1",
+			"org.olf.dcb.core.interaction.sierra.SierraLmsClient",
+			cfg);
 
-        	Mono.from(hostLmsRepository.save(new_host_lms))
-                  .block();
+		Mono.from(hostLmsRepository.save(new_host_lms))
+			.block();
 
-        	// final var fetchedHostLMSRecords = Flux.from(hostLmsRepository.findAll())
-                //                 .collectList()
-                //                 .block();
-		// log.info("fetched host records: {}",fetchedHostLMSRecords);
+		Boolean exists = Mono.from(hostLmsRepository.existsById(new_host_lms.getId())).block();
 
-                Boolean exists = Mono.from(hostLmsRepository.existsById(new_host_lms.getId())).block();
-
-        	assertThat(exists, is(Boolean.TRUE));
+		assertThat(exists, is(Boolean.TRUE));
 	}
 }
