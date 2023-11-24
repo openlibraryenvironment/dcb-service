418e6f63c08c1f80dac324fd246aa82d1479948e
Marc Johnson <marc@k-int.co.uk>
2023-11-24 09:39:58 +0000

    chore: Remove explicit http status code from live availability controller

--- dcb/src/main/java/org/olf/dcb/core/api/LiveAvailabilityController.java
+++ dcb/src/main/java/org/olf/dcb/core/api/LiveAvailabilityController.java
@@ -1,6 +1,7 @@
 package org.olf.dcb.core.api;
 
 import static io.micronaut.http.MediaType.APPLICATION_JSON;
+import static io.micronaut.security.rules.SecurityRule.IS_ANONYMOUS;
 
 import java.util.UUID;
 
@@ -8,12 +9,10 @@ import org.olf.dcb.item.availability.AvailabilityResponseView;
 import org.olf.dcb.item.availability.LiveAvailabilityService;
 
 import io.micronaut.core.async.annotation.SingleResult;
-import io.micronaut.http.HttpResponse;
 import io.micronaut.http.annotation.Controller;
 import io.micronaut.http.annotation.Get;
 import io.micronaut.http.annotation.QueryValue;
 import io.micronaut.security.annotation.Secured;
-import io.micronaut.security.rules.SecurityRule;
 import io.micronaut.validation.Validated;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
@@ -23,7 +22,7 @@ import reactor.core.publisher.Mono;
 
 @Slf4j
 @Validated
-@Secured(SecurityRule.IS_ANONYMOUS)
+@Secured(IS_ANONYMOUS)
 @Controller
 @Tag(name = "Live Availability API")
 public class LiveAvailabilityController {
@@ -39,13 +38,12 @@ public class LiveAvailabilityController {
 	)
 	@SingleResult
 	@Get(value = "/items/availability", produces = APPLICATION_JSON)
-	public Mono<HttpResponse<AvailabilityResponseView>> getLiveAvailability(
+	public Mono<AvailabilityResponseView> getLiveAvailability(
 		@NotNull @QueryValue("clusteredBibId") final UUID clusteredBibId) {
 
 		log.info("REST, getLiveAvailability: {}", clusteredBibId);
 
 		return liveAvailabilityService.getAvailableItems(clusteredBibId)
-			.map(report -> AvailabilityResponseView.from(report, clusteredBibId))
-			.map(HttpResponse::ok);
+			.map(report -> AvailabilityResponseView.from(report, clusteredBibId));
 	}
 }
