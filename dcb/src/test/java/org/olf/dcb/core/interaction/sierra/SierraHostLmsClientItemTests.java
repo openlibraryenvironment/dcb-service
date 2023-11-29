76c658d389e0e968e7b7f663a2fac1120b15249b
Marc Johnson <marc@k-int.co.uk>
2023-11-29 10:51:18 +0000

    test: Use has property matchers for checking items from Sierra

--- dcb/src/test/java/org/olf/dcb/core/interaction/sierra/SierraHostLmsClientItemTests.java
+++ dcb/src/test/java/org/olf/dcb/core/interaction/sierra/SierraHostLmsClientItemTests.java
@@ -1,17 +1,16 @@
 package org.olf.dcb.core.interaction.sierra;
 
+import static org.hamcrest.CoreMatchers.allOf;
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.CoreMatchers.notNullValue;
-import static org.hamcrest.CoreMatchers.nullValue;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.empty;
+import static org.hamcrest.Matchers.hasProperty;
 import static org.hamcrest.Matchers.hasSize;
 import static org.junit.jupiter.api.TestInstance.Lifecycle.PER_CLASS;
 import static org.olf.dcb.core.model.ItemStatusCode.AVAILABLE;
 import static org.olf.dcb.core.model.ItemStatusCode.CHECKED_OUT;
 import static org.olf.dcb.test.PublisherUtils.singleValueFrom;
-
-import java.time.Instant;
 
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
@@ -70,48 +69,42 @@ class SierraHostLmsClientItemTests {
 
 		final var firstItem = items.get(0);
 
-		assertThat(firstItem, is(notNullValue()));
-		assertThat(firstItem.getLocalId(), is("f2010365-e1b1-4a5d-b431-a3c65b5f23fb"));
-		assertThat(firstItem.getBarcode(), is("9849123490"));
-		assertThat(firstItem.getCallNumber(), is("BL221 .C48"));
-
-		assertThat(firstItem.getStatus(), is(notNullValue()));
-		assertThat(firstItem.getStatus().getCode(), is(CHECKED_OUT));
-		assertThat(firstItem.getDueDate(), is(Instant.parse("2023-04-22T15:55:13Z")));
-
-		assertThat(firstItem.getLocation(), is(notNullValue()));
-		assertThat(firstItem.getLocation().getName(), is("King 5th Floor"));
-		assertThat(firstItem.getLocation().getCode(), is("ab5"));
+		assertThat(firstItem, allOf(
+			hasProperty("localId", is("f2010365-e1b1-4a5d-b431-a3c65b5f23fb")),
+			hasProperty("barcode", is("9849123490")),
+			hasProperty("callNumber", is("BL221 .C48")),
+			hasProperty("status", hasProperty("code", is(CHECKED_OUT))),
+			hasProperty("location", allOf(
+				hasProperty("name", is("King 5th Floor")),
+				hasProperty("code", is("ab5"))
+			))
+		));
 
 		final var secondItem = items.get(1);
 
-		assertThat(secondItem, is(notNullValue()));
-		assertThat(secondItem.getLocalId(), is("c5bc9cd0-fc23-48be-9d52-647cea8c63ca"));
-		assertThat(secondItem.getBarcode(), is("30800005315459"));
-		assertThat(secondItem.getCallNumber(), is("HX157 .H8"));
-
-		assertThat(secondItem.getStatus(), is(notNullValue()));
-		assertThat(secondItem.getStatus().getCode(), is(AVAILABLE));
-		assertThat(secondItem.getDueDate(), is(nullValue()));
-
-		assertThat(secondItem.getLocation(), is(notNullValue()));
-		assertThat(secondItem.getLocation().getName(), is("King 7th Floor"));
-		assertThat(secondItem.getLocation().getCode(), is("ab7"));
+		assertThat(secondItem, allOf(
+			hasProperty("localId", is("c5bc9cd0-fc23-48be-9d52-647cea8c63ca")),
+			hasProperty("barcode", is("30800005315459")),
+			hasProperty("callNumber", is("HX157 .H8")),
+			hasProperty("status", hasProperty("code", is(AVAILABLE))),
+			hasProperty("location", allOf(
+				hasProperty("name", is("King 7th Floor")),
+				hasProperty("code", is("ab7"))
+			))
+		));
 
 		final var thirdItem = items.get(2);
 
-		assertThat(thirdItem, is(notNullValue()));
-		assertThat(thirdItem.getLocalId(), is("69415d0a-ace5-49e4-96fd-f63855235bf0"));
-		assertThat(thirdItem.getBarcode(), is("30800005208449"));
-		assertThat(thirdItem.getCallNumber(), is("HC336.2 .S74 1969"));
-
-		assertThat(thirdItem.getStatus(), is(notNullValue()));
-		assertThat(thirdItem.getStatus().getCode(), is(AVAILABLE));
-		assertThat(thirdItem.getDueDate(), is(nullValue()));
-
-		assertThat(thirdItem.getLocation(), is(notNullValue()));
-		assertThat(thirdItem.getLocation().getName(), is("King 7th Floor"));
-		assertThat(thirdItem.getLocation().getCode(), is("ab7"));
+		assertThat(thirdItem, allOf(
+			hasProperty("localId", is("69415d0a-ace5-49e4-96fd-f63855235bf0")),
+			hasProperty("barcode", is("30800005208449")),
+			hasProperty("callNumber", is("HC336.2 .S74 1969")),
+			hasProperty("status", hasProperty("code", is(AVAILABLE))),
+			hasProperty("location", allOf(
+				hasProperty("name", is("King 7th Floor")),
+				hasProperty("code", is("ab7"))
+			))
+		));
 	}
 
 	@Test
