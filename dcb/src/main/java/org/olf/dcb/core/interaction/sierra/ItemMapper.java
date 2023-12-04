308847bbba94411d268258dbd111f05d52a39af8
Marc Johnson <marc@k-int.co.uk>
2023-12-04 09:11:25 +0000

    refactor: Add location to agency mapper dependency to Sierra specific item mapper

--- dcb/src/main/java/org/olf/dcb/core/interaction/sierra/ItemMapper.java
+++ dcb/src/main/java/org/olf/dcb/core/interaction/sierra/ItemMapper.java
@@ -2,6 +2,7 @@ package org.olf.dcb.core.interaction.sierra;
 
 import org.olf.dcb.core.interaction.shared.ItemStatusMapper;
 import org.olf.dcb.core.interaction.shared.NumericItemTypeMapper;
+import org.olf.dcb.core.svc.LocationToAgencyMappingService;
 
 import jakarta.inject.Singleton;
 
@@ -9,11 +10,13 @@ import jakarta.inject.Singleton;
 public class ItemMapper {
 	private final ItemStatusMapper itemStatusMapper;
 	private final NumericItemTypeMapper itemTypeMapper;
+	private final LocationToAgencyMappingService locationToAgencyMappingService;
 
-	public ItemMapper(ItemStatusMapper itemStatusMapper,
-		NumericItemTypeMapper itemTypeMapper) {
-		
+	public ItemMapper(ItemStatusMapper itemStatusMapper, NumericItemTypeMapper itemTypeMapper,
+		LocationToAgencyMappingService locationToAgencyMappingService) {
+
 		this.itemStatusMapper = itemStatusMapper;
 		this.itemTypeMapper = itemTypeMapper;
+		this.locationToAgencyMappingService = locationToAgencyMappingService;
 	}
 }
