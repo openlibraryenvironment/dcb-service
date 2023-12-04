8bbac32e802b67833151461ed29bd3938997ff74
Marc Johnson <marc@k-int.co.uk>
2023-12-04 09:10:21 +0000

    refactor: Add item type mapper dependency to Sierra specific item mapper

--- dcb/src/main/java/org/olf/dcb/core/interaction/sierra/ItemMapper.java
+++ dcb/src/main/java/org/olf/dcb/core/interaction/sierra/ItemMapper.java
@@ -1,14 +1,19 @@
 package org.olf.dcb.core.interaction.sierra;
 
 import org.olf.dcb.core.interaction.shared.ItemStatusMapper;
+import org.olf.dcb.core.interaction.shared.NumericItemTypeMapper;
 
 import jakarta.inject.Singleton;
 
 @Singleton
 public class ItemMapper {
 	private final ItemStatusMapper itemStatusMapper;
+	private final NumericItemTypeMapper itemTypeMapper;
 
-	public ItemMapper(ItemStatusMapper itemStatusMapper) {
+	public ItemMapper(ItemStatusMapper itemStatusMapper,
+		NumericItemTypeMapper itemTypeMapper) {
+		
 		this.itemStatusMapper = itemStatusMapper;
+		this.itemTypeMapper = itemTypeMapper;
 	}
 }
