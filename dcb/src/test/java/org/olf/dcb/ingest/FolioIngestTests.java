390cfc4fd0a2955770ab01bb171e0a2ef2ce37a7
Marc Johnson <marc@k-int.co.uk>
2023-12-20 09:59:41 +0000

    test: Delete all host LMS before each FOLIO ingest test DCB-797

---
+++ dcb/src/test/java/org/olf/dcb/ingest/FolioIngestTests.java
@@ -0,0 +1,29 @@
+package org.olf.dcb.ingest;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.olf.dcb.test.HostLmsFixture;
+
+import jakarta.inject.Inject;
+import lombok.extern.slf4j.Slf4j;
+import services.k_int.test.mockserver.MockServerMicronautTest;
+
+@Slf4j
+@MockServerMicronautTest
+class FolioIngestTests {
+	@Inject
+	private HostLmsFixture hostLmsFixture;
+
+	@BeforeEach
+	void beforeEach() {
+		hostLmsFixture.deleteAll();
+	}
+
+	@Test
+	void shouldIngestFromFolio() {
+		assertThat(true, is(true));
+	}
+}
