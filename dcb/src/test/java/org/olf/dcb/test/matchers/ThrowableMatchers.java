ede4f62b676985c9c90f0865f60c4085587655dd
Marc Johnson <marc@k-int.co.uk>
2024-01-12 16:33:48 +0000

    test: Use property matcher for Sierra invalid hold policy message DCB-490

--- dcb/src/test/java/org/olf/dcb/test/matchers/ThrowableMatchers.java
+++ dcb/src/test/java/org/olf/dcb/test/matchers/ThrowableMatchers.java
@@ -1,5 +1,6 @@
 package org.olf.dcb.test.matchers;
 
+import static org.hamcrest.CoreMatchers.containsString;
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.Matchers.hasProperty;
 
@@ -7,6 +8,14 @@ import org.hamcrest.Matcher;
 
 public class ThrowableMatchers {
 	public static Matcher<Throwable> hasMessage(String expectedMessage) {
-		return hasProperty("message", is(expectedMessage));
+		return messageProperty(is(expectedMessage));
+	}
+
+	public static Matcher<Throwable> messageContains(String expectedMessage) {
+		return messageProperty(containsString(expectedMessage));
+	}
+
+	private static Matcher<Throwable> messageProperty(Matcher<String> matcher) {
+		return hasProperty("message", matcher);
 	}
 }
