53abbc908dee31d4ba46495da7b83cb192318517
Ian Ibbotson <ian.ibbotson@k-int.com>
2024-01-22 09:24:39 +0000

    feature: Improve audit logging on circulation of home item to vpatron for DCB-838

--- dcb/src/main/java/org/olf/dcb/request/workflow/HandleBorrowerItemLoaned.java
+++ dcb/src/main/java/org/olf/dcb/request/workflow/HandleBorrowerItemLoaned.java
@@ -27,22 +27,23 @@ import java.time.Duration;
 @Named("BorrowerRequestLoaned")
 public class HandleBorrowerItemLoaned implements WorkflowAction {
 
-        private static final Logger log = LoggerFactory.getLogger(HandleBorrowerItemLoaned.class);
-        private RequestWorkflowContextHelper requestWorkflowContextHelper;
-        private PatronRequestRepository patronRequestRepository;
-        private HostLmsService hostLmsService;
+	private static final Logger log = LoggerFactory.getLogger(HandleBorrowerItemLoaned.class);
+	private RequestWorkflowContextHelper requestWorkflowContextHelper;
+	private PatronRequestRepository patronRequestRepository;
+	private HostLmsService hostLmsService;
 	private PatronRequestAuditService patronRequestAuditService;
 
-        public HandleBorrowerItemLoaned(
-                PatronRequestRepository patronRequestRepository,
-                HostLmsService hostLmsService,
-                RequestWorkflowContextHelper requestWorkflowContextHelper,
-		PatronRequestAuditService patronRequestAuditService) {
-                this.patronRequestRepository = patronRequestRepository;
-                this.hostLmsService = hostLmsService;
-                this.requestWorkflowContextHelper = requestWorkflowContextHelper;
-                this.patronRequestAuditService = patronRequestAuditService;
-        }
+	public HandleBorrowerItemLoaned(
+		PatronRequestRepository patronRequestRepository,
+		HostLmsService hostLmsService,
+		RequestWorkflowContextHelper requestWorkflowContextHelper,
+		PatronRequestAuditService patronRequestAuditService) 
+	{
+		this.patronRequestRepository = patronRequestRepository;
+		this.hostLmsService = hostLmsService;
+		this.requestWorkflowContextHelper = requestWorkflowContextHelper;
+		this.patronRequestAuditService = patronRequestAuditService;
+	}
         
         @Transactional
         public Mono<Map<String,Object>> execute(Map<String,Object> context) {
@@ -67,44 +68,47 @@ public class HandleBorrowerItemLoaned implements WorkflowAction {
         }
 
 
-        public Mono<RequestWorkflowContext> checkHomeItemOutToVirtualPatron(RequestWorkflowContext rwc) {
+	public Mono<RequestWorkflowContext> checkHomeItemOutToVirtualPatron(RequestWorkflowContext rwc) {
 
-                if ( ( rwc.getSupplierRequest() != null ) && 
-                     ( rwc.getSupplierRequest().getLocalItemId() != null ) &&
-                     ( rwc.getLenderSystemCode() != null ) &&
-                     ( rwc.getPatronVirtualIdentity() != null ) ) {
+		if ( ( rwc.getSupplierRequest() != null ) && 
+			( rwc.getSupplierRequest().getLocalItemId() != null ) &&
+			( rwc.getLenderSystemCode() != null ) &&
+			( rwc.getPatronVirtualIdentity() != null ) ) {
 
+			// In some systems a patron can have multiple barcodes. In those systems getLocalBarcode will be encoded as [value, value, value]
+			// So we trim the opening and closing [] and split on the ", " Otherwise just split on ", " just in case
+			final String[] patron_barcodes = extractPatronBarcodes(rwc.getPatronVirtualIdentity().getLocalBarcode());
 
-                        // In some systems a patron can have multiple barcodes. In those systems getLocalBarcode will be encoded as [value, value, value]
-                        // So we trim the opening and closing [] and split on the ", " Otherwise just split on ", " just in case
-                        final String[] patron_barcodes = extractPatronBarcodes(rwc.getPatronVirtualIdentity().getLocalBarcode());
+			if ( ( patron_barcodes != null ) && ( patron_barcodes.length > 0 ) ) {
 
-                        if ( ( patron_barcodes != null ) && ( patron_barcodes.length > 0 ) ) {
+				String home_item_barcode = rwc.getSupplierRequest().getLocalItemBarcode();
 
-                                log.info("Update check home item out : {} to {} at {}",
-                                        rwc.getSupplierRequest().getLocalItemBarcode(), patron_barcodes[0],rwc.getLenderSystemCode());
+				log.info("Update check home item out : {} to {} at {}",
+					home_item_barcode, patron_barcodes[0],rwc.getLenderSystemCode());
 
-                                return hostLmsService.getClientFor(rwc.getLenderSystemCode())
-                                        .flatMap(hostLmsClient -> updateThenCheckoutItem(rwc, hostLmsClient, patron_barcodes))
-					.doOnError(error -> log.error("Unable to issue item to vpatron at lender",error))
-                                        .thenReturn(rwc);
-                        }
-                        else {
-                                log.error("NO BARCODE FOR PATRON VIRTUAL IDENTITY. UNABLE TO CHECK OUT {}",rwc.getPatronVirtualIdentity().getLocalBarcode());
+				return hostLmsService.getClientFor(rwc.getLenderSystemCode())
+					.flatMap(hostLmsClient -> updateThenCheckoutItem(rwc, hostLmsClient, patron_barcodes))
+					.flatMap(srwc -> patronRequestAuditService.addAuditEntry(srwc.getPatronRequest(), "Home item (b="+home_item_barcode+"@"+rwc.getLenderSystemCode()+") checked out to virtual patron (b="+patron_barcodes[0]+")") )
+					.doOnError(error -> patronRequestAuditService.addErrorAuditEntry(rwc.getPatronRequest(), "Error attempting to check out home item to vpatron:"+error))
+					.thenReturn(rwc);
+			}
+			else {
+
+				log.error("NO BARCODE FOR PATRON VIRTUAL IDENTITY. UNABLE TO CHECK OUT {}",rwc.getPatronVirtualIdentity().getLocalBarcode());
 
 				return patronRequestAuditService.addErrorAuditEntry(rwc.getPatronRequest(),
 					"NO BARCODE FOR PATRON VIRTUAL IDENTITY. UNABLE TO CHECK OUT") 
 					.thenReturn(rwc);
-                        }
-                }
-                else { 
-                        log.error("Missing data attempting to set home item off campus {} {} {}",rwc,rwc.getSupplierRequest(), rwc.getPatronVirtualIdentity());
+			}
+		}
+		else { 
+			log.error("Missing data attempting to set home item off campus {} {} {}",rwc,rwc.getSupplierRequest(), rwc.getPatronVirtualIdentity());
 			return patronRequestAuditService.addErrorAuditEntry(rwc.getPatronRequest(),
 				String.format("Missing data attempting to set home item off campus {} {} {}",
-				rwc,rwc.getSupplierRequest(), rwc.getPatronVirtualIdentity()))
-                        	.thenReturn(rwc);
-                }       
-        }
+					rwc,rwc.getSupplierRequest(), rwc.getPatronVirtualIdentity()))
+				.thenReturn(rwc);
+		}       
+	}
 
         private Mono<RequestWorkflowContext> updateThenCheckoutItem(RequestWorkflowContext rwc, HostLmsClient hostLmsClient, String[] patronBarcode) {
 
