951e6fe00afa5c68b825b426c3fa5d84d2d6583a
Marc Johnson <marc@k-int.co.uk>
2024-01-23 13:45:20 +0000

    chore: Remove empty init method from tracking service

--- dcb/src/main/java/org/olf/dcb/tracking/TrackingService.java
+++ dcb/src/main/java/org/olf/dcb/tracking/TrackingService.java
@@ -44,10 +44,6 @@ public class TrackingService implements Runnable {
 		this.hostLmsReactions = hostLmsReactions;
 	}
 
-	@jakarta.annotation.PostConstruct
-	private void init() {
-	}
-
 	@AppTask
 	@Override
 	@Scheduled(initialDelay = "2m", fixedDelay = "${dcb.tracking.interval:5m}")
