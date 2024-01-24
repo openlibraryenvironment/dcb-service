b62f5562dbab0664d9d6f130514a7ee59201e0c4
Marc Johnson <marc@k-int.co.uk>
2024-01-24 15:07:01 +0000

    chore: Replace spaces with tabs in Sierra patron hold request object

--- dcb/src/main/java/services/k_int/interaction/sierra/holds/SierraPatronHold.java
+++ dcb/src/main/java/services/k_int/interaction/sierra/holds/SierraPatronHold.java
@@ -1,25 +1,22 @@
 package services.k_int.interaction.sierra.holds;
 
-import jakarta.validation.constraints.NotEmpty;
-
 import io.micronaut.core.annotation.Nullable;
 import io.micronaut.serde.annotation.Serdeable;
-import lombok.Data;
-import services.k_int.interaction.sierra.SierraCodeTuple;
+import jakarta.validation.constraints.NotEmpty;
 import lombok.Builder;
+import services.k_int.interaction.sierra.SierraCodeTuple;
 
 @Builder
 @Serdeable
 public record SierraPatronHold (
-	
-        @NotEmpty String id,
-        @Nullable String record,
-        @Nullable String patron,
-        @Nullable String frozen,
-        @Nullable String placed,
-        @Nullable String notWantedBeforeDate,
-        @Nullable SierraCodeTuple pickupLocation,
-        @Nullable SierraCodeTuple status,
-        @Nullable String recordType,
-        @Nullable String note
-        ) {}
+	@NotEmpty String id,
+	@Nullable String record,
+	@Nullable String patron,
+	@Nullable String frozen,
+	@Nullable String placed,
+	@Nullable String notWantedBeforeDate,
+	@Nullable SierraCodeTuple pickupLocation,
+	@Nullable SierraCodeTuple status,
+	@Nullable String recordType,
+	@Nullable String note
+) { }
