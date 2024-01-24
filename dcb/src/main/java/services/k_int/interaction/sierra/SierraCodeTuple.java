43bfbdc2021ede6cf997855e4fcba9f53192ca4f
Marc Johnson <marc@k-int.co.uk>
2024-01-24 15:21:15 +0000

    chore: Replace spaces with tabs in Sierra code tuple class

--- dcb/src/main/java/services/k_int/interaction/sierra/SierraCodeTuple.java
+++ dcb/src/main/java/services/k_int/interaction/sierra/SierraCodeTuple.java
@@ -3,9 +3,10 @@ package services.k_int.interaction.sierra;
 import io.micronaut.serde.annotation.Serdeable;
 
 import jakarta.validation.constraints.NotEmpty;
+import lombok.Builder;
 
 @Serdeable
 public record SierraCodeTuple (
-        @NotEmpty String code,
-        @NotEmpty String name
+	@NotEmpty String code,
+	@NotEmpty String name
 ) {}
