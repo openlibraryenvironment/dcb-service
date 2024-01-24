86f450dd85e3940fb40eb4f89486e4d56be7e9a3
Marc Johnson <marc@k-int.co.uk>
2024-01-24 15:24:14 +0000

    chore: Use builder for defining Sierra code tuple

--- dcb/src/main/java/services/k_int/interaction/sierra/SierraCodeTuple.java
+++ dcb/src/main/java/services/k_int/interaction/sierra/SierraCodeTuple.java
@@ -1,11 +1,11 @@
 package services.k_int.interaction.sierra;
 
 import io.micronaut.serde.annotation.Serdeable;
-
 import jakarta.validation.constraints.NotEmpty;
 import lombok.Builder;
 
 @Serdeable
+@Builder
 public record SierraCodeTuple (
 	@NotEmpty String code,
 	@NotEmpty String name
