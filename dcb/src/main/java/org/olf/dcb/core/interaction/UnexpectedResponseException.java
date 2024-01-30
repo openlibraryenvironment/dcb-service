4006a66c8e8d834f02a9a2e20d1c37e98edd8e66
Marc Johnson <marc@k-int.co.uk>
2024-01-30 18:21:30 +0000

    refactor: Introduce overload for raising unexpected response exception without a request DCB-849

--- dcb/src/main/java/org/olf/dcb/core/interaction/UnexpectedResponseException.java
+++ dcb/src/main/java/org/olf/dcb/core/interaction/UnexpectedResponseException.java
@@ -3,6 +3,7 @@ package org.olf.dcb.core.interaction;
 import static org.olf.dcb.core.interaction.HttpProtocolToLogMessageMapper.toLogOutput;
 import static org.olf.dcb.utils.PropertyAccessUtils.getValue;
 
+import io.micronaut.http.HttpMethod;
 import io.micronaut.http.HttpRequest;
 import io.micronaut.http.MutableHttpRequest;
 import io.micronaut.http.client.exceptions.HttpClientResponseException;
@@ -11,9 +12,15 @@ public class UnexpectedResponseException extends RuntimeException {
 	public UnexpectedResponseException(MutableHttpRequest<?> request,
 		HttpClientResponseException responseError) {
 
+		this(responseError, getValue(request, HttpRequest::getMethod),
+			getValue(request, HttpRequest::getPath));
+	}
+
+	public UnexpectedResponseException(HttpClientResponseException responseError,
+		HttpMethod requestMethod, String requestPath) {
+
 		super("Unexpected HTTP response from: %s %s\nResponse: %s".formatted(
-		 	getValue(request, HttpRequest::getMethodName),
-			getValue(request, HttpRequest::getPath),
+			getValue(requestMethod, HttpMethod::name), requestPath,
 			toLogOutput(getValue(responseError, HttpClientResponseException::getResponse))));
 	}
 }
