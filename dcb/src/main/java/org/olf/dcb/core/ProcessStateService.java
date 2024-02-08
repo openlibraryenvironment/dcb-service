5f1003451eb9202489de3f0732facf91bc2a539c
Ian Ibbotson <ian.ibbotson@k-int.com>
2024-02-08 00:00:19 +0000

    Add transactional annotation for processStateService

--- dcb/src/main/java/org/olf/dcb/core/ProcessStateService.java
+++ dcb/src/main/java/org/olf/dcb/core/ProcessStateService.java
@@ -6,10 +6,13 @@ import java.util.UUID;
 import org.olf.dcb.core.model.ProcessState;
 import org.olf.dcb.storage.ProcessStateRepository;
 
+import io.micronaut.transaction.TransactionDefinition.Propagation;
+import io.micronaut.transaction.annotation.Transactional;
 import jakarta.inject.Singleton;
 import reactor.core.publisher.Mono;
 import reactor.core.publisher.Flux;
 import services.k_int.utils.UUIDUtils;
+
 
 @Singleton
 public class ProcessStateService {
@@ -20,20 +23,18 @@ public class ProcessStateService {
 		this.processStateRepository = processStateRepository;
 	}
 
+	@Transactional
 	public Mono<ProcessState> updateState(UUID context, String processName, Map<String, Object> state) {
 
 		UUID persistence_id = UUIDUtils.nameUUIDFromNamespaceAndString(context, processName);
 
 		ProcessState ps = new ProcessState(persistence_id, context, processName, state);
 
-		// return Mono.from(processStateRepository.existsById(persistence_id))
-		//		.flatMap(exists -> Mono.fromDirect(exists ? processStateRepository.update(ps) : processStateRepository.save(ps)));
-
-		return Mono.from(
-			Flux.from(processStateRepository.existsById(persistence_id))
-				.concatMap(exists -> exists ? processStateRepository.update(ps) : processStateRepository.save(ps)));
+		return Mono.from(processStateRepository.existsById(persistence_id))
+			.flatMap(exists -> Mono.fromDirect(exists ? processStateRepository.update(ps) : processStateRepository.save(ps)));
 	}
 
+	@Transactional
 	public Mono<ProcessState> getState(UUID context, String processName) {
 		UUID persistence_id = UUIDUtils.nameUUIDFromNamespaceAndString(context, processName);
 
