f2fab366f200aef1e7516892993470bbcb510c9e
Marc Johnson <marc@k-int.co.uk>
2024-02-15 09:51:22 +0000

    chore: Reduce duplication in property access utility methods DCB-855

--- dcb/src/main/java/org/olf/dcb/utils/PropertyAccessUtils.java
+++ dcb/src/main/java/org/olf/dcb/utils/PropertyAccessUtils.java
@@ -1,13 +1,13 @@
 package org.olf.dcb.utils;
 
+import static java.util.function.Function.identity;
+
 import java.util.Optional;
 import java.util.function.Function;
 
 public class PropertyAccessUtils {
 	public static <T, R> R getValue(T nullableObject, Function<T, R> accessor) {
-		return Optional.ofNullable(nullableObject)
-			.map(accessor)
-			.orElse(null);
+		return getValue(nullableObject, accessor, identity());
 	}
 
 	public static <T, R, S> S getValue(T nullableObject, Function<T, R> accessor,
