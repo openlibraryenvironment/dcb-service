575d10d05d2e6eb0bcd773df5d793e4bceffebc4
Marc Johnson <marc@k-int.co.uk>
2024-02-16 10:45:48 +0000

    test: Delete all patron requests before workflow service tests

--- dcb/src/test/java/org/olf/dcb/request/workflow/PatronRequestWorkflowServiceTests.java
+++ dcb/src/test/java/org/olf/dcb/request/workflow/PatronRequestWorkflowServiceTests.java
@@ -3,8 +3,10 @@ package org.olf.dcb.request.workflow;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.notNullValue;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.olf.dcb.test.DcbTest;
+import org.olf.dcb.test.PatronRequestsFixture;
 
 import jakarta.inject.Inject;
 
@@ -12,6 +14,14 @@ import jakarta.inject.Inject;
 class PatronRequestWorkflowServiceTests {
 	@Inject
 	private PatronRequestWorkflowService workflowService;
+
+	@Inject
+	private PatronRequestsFixture patronRequestsFixture;
+
+	@BeforeEach
+	void beforeEach() {
+		patronRequestsFixture.deleteAll();
+	}
 
 	@Test
 	void shouldTransitionPatronRequestToErrorStatus() {
