2e1bc05d804132ff4f5d9d085dcfc22ac89b8a7d
Marc Johnson <marc@k-int.co.uk>
2024-02-16 08:17:06 +0000

    chore: Remove unused string utility methods

--- dcb/src/main/java/services/k_int/utils/StringUtils.java
+++ dcb/src/main/java/services/k_int/utils/StringUtils.java
@@ -4,30 +4,15 @@ import java.util.Optional;
 import java.util.function.Function;
 
 public interface StringUtils {
-	
-	public static String padRightToLength(String toPad, int length) {
-		
+	static String padRightToLength(String toPad, int length) {
 		return Optional.of(toPad != null ? toPad : "")
 			.map(Functions.formatAs("%-" + length + "s"))
 			.get();
 	}
-	
-	public static String padLeftToLength(String toPad, int length) {
-		
-		return Optional.of(toPad != null ? toPad : "")
-			.map(Functions.formatAs("%" + length + "s"))
-			.get();
-	}
-	
-	public static interface Functions {
-		public static Function<String, String> padRightToLength( int length ) {
-			return ( String subject ) -> StringUtils.padRightToLength(subject, length);
-		}
-		public static Function<String, String> padLeftToLength( int length ) {
-			return ( String subject ) -> StringUtils.padLeftToLength(subject, length);
-		}
-		public static Function<String, String> formatAs( String pattern ) {
-			return ( String subject ) -> String.format(pattern, subject);
+
+	interface Functions {
+		static Function<String, String> formatAs(String pattern) {
+			return (String subject) -> String.format(pattern, subject);
 		}
 	}
 }
