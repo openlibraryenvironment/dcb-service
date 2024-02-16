e84ff24a5351be8850486b4606ad7cb4d7a5606e
Marc Johnson <marc@k-int.co.uk>
2024-02-16 08:42:26 +0000

    chore: Remove redundant static modifier on patron request status enum

--- dcb/src/main/java/org/olf/dcb/core/model/PatronRequest.java
+++ dcb/src/main/java/org/olf/dcb/core/model/PatronRequest.java
@@ -1,36 +1,43 @@
 package org.olf.dcb.core.model;
 
-import io.micronaut.core.annotation.Creator;
-import io.micronaut.core.annotation.NonNull;
-import io.micronaut.core.annotation.Nullable;
-import io.micronaut.data.annotation.*;
-import io.micronaut.data.model.DataType;
-import io.micronaut.serde.annotation.Serdeable;
-import jakarta.persistence.Column;
-import jakarta.persistence.OneToMany;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.ToString;
-import lombok.experimental.Accessors;
-import lombok.NoArgsConstructor;
-import services.k_int.tests.ExcludeFromGeneratedCoverageReport;
-
-import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Size;
-
-import com.fasterxml.jackson.annotation.JsonIgnore;
-import com.fasterxml.jackson.annotation.JsonProperty;
+import static org.olf.dcb.core.model.PatronRequest.Status.NO_ITEMS_AVAILABLE_AT_ANY_AGENCY;
+import static org.olf.dcb.core.model.PatronRequest.Status.REQUEST_PLACED_AT_BORROWING_AGENCY;
+import static org.olf.dcb.core.model.PatronRequest.Status.REQUEST_PLACED_AT_SUPPLYING_AGENCY;
+import static org.olf.dcb.core.model.PatronRequest.Status.RESOLVED;
 
 import java.time.Instant;
 import java.util.List;
 import java.util.UUID;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import io.micronaut.core.annotation.Creator;
+import io.micronaut.core.annotation.NonNull;
+import io.micronaut.core.annotation.Nullable;
+import io.micronaut.data.annotation.DateCreated;
+import io.micronaut.data.annotation.DateUpdated;
+import io.micronaut.data.annotation.Id;
+import io.micronaut.data.annotation.MappedEntity;
+import io.micronaut.data.annotation.Relation;
+import io.micronaut.data.annotation.TypeDef;
+import io.micronaut.data.model.DataType;
+import io.micronaut.serde.annotation.Serdeable;
+import jakarta.persistence.Column;
+import jakarta.persistence.OneToMany;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+import lombok.experimental.Accessors;
+import lombok.extern.slf4j.Slf4j;
+import services.k_int.tests.ExcludeFromGeneratedCoverageReport;
 
 
-
+@Slf4j
 @Builder
 @Data
 @NoArgsConstructor(onConstructor_ = @Creator())
@@ -41,11 +48,8 @@ import org.slf4j.LoggerFactory;
 @Accessors(chain = true)
 @ToString(onlyExplicitlyIncluded = true)
 public class PatronRequest {
-
-        private static final Logger log = LoggerFactory.getLogger(PatronRequest.class);
-
 	@Serdeable
-	public static enum Status {
+	public enum Status {
 		SUBMITTED_TO_DCB, 
 		PATRON_VERIFIED, 
 		RESOLVED, 
@@ -53,19 +57,18 @@ public class PatronRequest {
 		// Added in preparation for moving to next supplier - when a supplier cancels a request we
 		// want to resubmit the request to the next possible supplier, creating a new supplier_request
 		// or if there are no more suppliers possible, move to NO_ITEMS_AVAILABLE_AT_ANY_AGENCY
-		NOT_SUPPLIED_CURRENT_SUPPLIER,	
-
-		NO_ITEMS_AVAILABLE_AT_ANY_AGENCY, 
+		NOT_SUPPLIED_CURRENT_SUPPLIER,
+		NO_ITEMS_AVAILABLE_AT_ANY_AGENCY,
 		REQUEST_PLACED_AT_SUPPLYING_AGENCY,
-		REQUEST_PLACED_AT_BORROWING_AGENCY, 
+		REQUEST_PLACED_AT_BORROWING_AGENCY,
 		RECEIVED_AT_PICKUP,
-		READY_FOR_PICKUP, // 
-		LOANED,         // Currently onloan
+		READY_FOR_PICKUP,
+		LOANED, // Currently on loan
 		PICKUP_TRANSIT, // In transit to pickup location
 		RETURN_TRANSIT, // In transit back to owning location from lender
 		CANCELLED,
-		COMPLETED,    // Everything is finished, regardless and ready to be finalised
-		FINALISED,    // We've cleaned up everything and this is the end of the line
+		COMPLETED, // Everything is finished, regardless and ready to be finalised
+		FINALISED, // We've cleaned up everything and this is the end of the line
 		ERROR
 	}
 
@@ -138,7 +141,7 @@ public class PatronRequest {
 	@Size(max = 200)
 	private String pickupItemStatus;
 
-	// In order to hand the temporary item over the the patron at the pickup
+	// In order to hand the temporary item over the patron at the pickup
 	// library, place a hold at the pickup lib and record it here
 	@Nullable
 	@Size(max = 200)
@@ -151,7 +154,7 @@ public class PatronRequest {
 
 	// Ignore at this property level. We provide explicit ignore/serializing
 	// instructions at a getter and setter level to prevent any JSON binding to this
-	// field but allow deserilization for output.
+	// field but allow deserialization for output.
 	@JsonIgnore
 	@ToString.Include
 	@Nullable
@@ -176,7 +179,6 @@ public class PatronRequest {
 	@Size(max = 32)
 	private String localItemType;
 
-
 	@Nullable
 	private String localBibId;
 
@@ -199,18 +201,18 @@ public class PatronRequest {
 	}
 	
 	@JsonIgnore
-	public PatronRequest setStatus( Status status ) {
+	public PatronRequest setStatus(Status status) {
 		this.status = status;
 		return this;
 	}
 
-        /**
-         * It is useful to have a shorthand note of the specific workflow which is in force for the patron request - initially
-         * RET- RETURNABLE ITEMS
-         * RET-LOCAL - We're placing a request in a single system - the patron, pickup and lending roles are all within a single system (1 Party)
-         * RET-STD - We're placing a request at a remote system, but the patron will pick the item up from their local library (2 parties)
-         * RET-PUA - The Borrower, Patron and Pickup systems are all different (3 parties)
-         */
+	/**
+	 * It is useful to have a shorthand note of the specific workflow which is in force for the patron request - initially
+	 * RET- RETURNABLE ITEMS
+	 * RET-LOCAL - We're placing a request in a single system - the patron, pickup and lending roles are all within a single system (1 Party)
+	 * RET-STD - We're placing a request at a remote system, but the patron will pick the item up from their local library (2 parties)
+	 * RET-PUA - The Borrower, Patron and Pickup systems are all different (3 parties)
+	 */
 	@Nullable
 	private String activeWorkflow;
 
@@ -221,20 +223,22 @@ public class PatronRequest {
 	private List<SupplierRequest> supplierRequests;
 
 	public PatronRequest resolve() {
-		return setStatus(Status.RESOLVED);
+		return setStatus(RESOLVED);
 	}
 
 	public PatronRequest resolveToNoItemsAvailable() {
 		log.debug("resolveToNoItemsAvailable()");
-		return setStatus(Status.NO_ITEMS_AVAILABLE_AT_ANY_AGENCY);
+
+		return setStatus(NO_ITEMS_AVAILABLE_AT_ANY_AGENCY);
 	}
 
 	public PatronRequest placedAtBorrowingAgency(String localId, String localStatus) {
-		return setLocalRequestId(localId).setLocalRequestStatus(localStatus)
-				.setStatus(Status.REQUEST_PLACED_AT_BORROWING_AGENCY);
+		return setLocalRequestId(localId)
+			.setLocalRequestStatus(localStatus)
+			.setStatus(REQUEST_PLACED_AT_BORROWING_AGENCY);
 	}
 
 	public PatronRequest placedAtSupplyingAgency() {
-		return setStatus(Status.REQUEST_PLACED_AT_SUPPLYING_AGENCY);
+		return setStatus(REQUEST_PLACED_AT_SUPPLYING_AGENCY);
 	}
 }
