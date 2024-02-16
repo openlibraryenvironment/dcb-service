9cec62c8ef5d22d683eccd08c546cac1ba6c2d31
Marc Johnson <marc@k-int.co.uk>
2024-02-16 11:01:35 +0000

    test: Demonstrate patron request status changes to error when error occurs in reactive chain

--- dcb/src/test/java/org/olf/dcb/request/workflow/PatronRequestWorkflowServiceTests.java
+++ dcb/src/test/java/org/olf/dcb/request/workflow/PatronRequestWorkflowServiceTests.java
@@ -1,14 +1,24 @@
 package org.olf.dcb.request.workflow;
 
+import static java.util.UUID.randomUUID;
 import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.allOf;
 import static org.hamcrest.Matchers.notNullValue;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.olf.dcb.core.model.PatronRequest.Status.ERROR;
+import static org.olf.dcb.core.model.PatronRequest.Status.RESOLVED;
+import static org.olf.dcb.test.PublisherUtils.singleValueFrom;
+import static org.olf.dcb.test.matchers.PatronRequestMatchers.hasErrorMessage;
+import static org.olf.dcb.test.matchers.PatronRequestMatchers.hasStatus;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.olf.dcb.core.model.PatronRequest;
 import org.olf.dcb.test.DcbTest;
 import org.olf.dcb.test.PatronRequestsFixture;
 
 import jakarta.inject.Inject;
+import reactor.core.publisher.Mono;
 
 @DcbTest
 class PatronRequestWorkflowServiceTests {
@@ -25,6 +35,33 @@ class PatronRequestWorkflowServiceTests {
 
 	@Test
 	void shouldTransitionPatronRequestToErrorStatus() {
-		assertThat(workflowService, notNullValue());
+		// Arrange
+		final var patronRequestId = randomUUID();
+
+		final var patronRequest = PatronRequest.builder()
+			.id(patronRequestId)
+			.status(RESOLVED)
+			.build();
+
+		patronRequestsFixture.savePatronRequest(patronRequest);
+
+		// Act
+		assertThrows(RuntimeException.class, () -> singleValueFrom(
+			Mono.just(patronRequest)
+				.flatMap(pr -> raiseError("Something went wrong"))
+				.transform(workflowService.getErrorTransformerFor(patronRequest))));
+
+		// Assert
+		final var updatedPatronRequest = patronRequestsFixture.findById(patronRequestId);
+
+		assertThat(updatedPatronRequest, allOf(
+			notNullValue(),
+			hasStatus(ERROR),
+			hasErrorMessage("Something went wrong")
+		));
+	}
+
+	private static Mono<PatronRequest> raiseError(String message) {
+		return Mono.error(new RuntimeException(message));
 	}
 }
