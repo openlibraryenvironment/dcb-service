c34ea7f587b92899a1df236256b2d30cfad84a68
Marc Johnson <marc@k-int.co.uk>
2024-02-21 16:00:13 +0000

    test: Make agency repository tests independent DCB-884

--- dcb/src/test/java/org/olf/dcb/storage/AgencyRepoTests.java
+++ dcb/src/test/java/org/olf/dcb/storage/AgencyRepoTests.java
@@ -1,37 +1,29 @@
 package org.olf.dcb.storage;
 
 import static java.util.UUID.randomUUID;
-import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.hasSize;
+import static org.olf.dcb.test.PublisherUtils.manyValuesFrom;
+import static org.olf.dcb.test.PublisherUtils.singleValueFrom;
 
 import java.util.HashMap;
-import java.util.Map;
-import java.util.List;
 import java.util.UUID;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.olf.dcb.core.model.DataAgency;
 import org.olf.dcb.core.model.DataHostLms;
-import org.olf.dcb.core.model.RecordCountSummary;
-import org.olf.dcb.storage.AgencyRepository;
-import org.olf.dcb.storage.HostLmsRepository;
-import org.olf.dcb.storage.BibRepository;
+import org.olf.dcb.test.AgencyFixture;
+import org.olf.dcb.test.BibRecordFixture;
+import org.olf.dcb.test.ClusterRecordFixture;
 import org.olf.dcb.test.DcbTest;
+import org.olf.dcb.test.HostLmsFixture;
 
-import io.micronaut.http.client.HttpClient;
-import io.micronaut.http.client.annotation.Client;
 import jakarta.inject.Inject;
-import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 @DcbTest
 class AgencyRepoTests {
-
-	@Inject
-	@Client("/")
-	HttpClient client;
-
 	@Inject
 	AgencyRepository agencyRepository;
 
@@ -41,49 +33,81 @@ class AgencyRepoTests {
 	@Inject
 	BibRepository bibRepository;
 
+	@Inject
+	AgencyFixture agencyFixture;
+	@Inject
+	HostLmsFixture hostLmsFixture;
+	@Inject
+	BibRecordFixture bibRecordFixture;
+	@Inject
+	ClusterRecordFixture clusterRecordFixture;
+
 	@BeforeEach
 	void beforeEach() {
+		bibRecordFixture.deleteAll();
+		clusterRecordFixture.deleteAll();
+		hostLmsFixture.deleteAll();
+		agencyFixture.deleteAll();
 	}
 
 	@Test
 	void createAgencyViaRepository() {
+		// Arrange
 
 		// Create a host LMS entry for our new Agency to point at
-                Map<String, Object> cfg = new HashMap<String,Object>();
-                DataHostLms new_host_lms = DataHostLms.builder()
-						.id(UUID.randomUUID())
-                                                .code("TH2")
-                                                .name("Test HostLMS2")
-                                                .lmsClientClass("org.olf.dcb.core.interaction.sierra.SierraLmsClient")
-                                                .clientConfig(cfg)
-                                                .build();
+		final var cfg = new HashMap<String, Object>();
 
-                Mono.from(hostLmsRepository.save(new_host_lms)).block();
+		final var hostLms = DataHostLms.builder()
+			.id(UUID.randomUUID())
+			.code("TH2")
+			.name("Test HostLMS2")
+			.lmsClientClass("org.olf.dcb.core.interaction.sierra.SierraLmsClient")
+			.clientConfig(cfg)
+			.build();
 
-        	DataAgency new_agency = DataAgency.builder()
-						.id(randomUUID())
-						.code("ACODE1")
-						.name("Test Agency Name1")
-						.hostLms(new_host_lms)
-						.latitude(53.383331)
-						.longitude(-1.466667)
-						.build();
+		singleValueFrom(Mono.from(hostLmsRepository.save(hostLms)));
 
-        	Mono.from(agencyRepository.save(new_agency)).block();
+		final var new_agency = DataAgency.builder()
+			.id(randomUUID())
+			.code("ACODE1")
+			.name("Test Agency Name1")
+			.hostLms(hostLms)
+			.latitude(53.383331)
+			.longitude(-1.466667)
+			.build();
 
-        	final var fetchedAgencyRecords = Flux.from(agencyRepository.queryAll())
-                                .collectList()
-                                .block();
+		singleValueFrom(Mono.from(agencyRepository.save(new_agency)));
 
-        	assertThat(fetchedAgencyRecords.size(), is(1));
+		// Act
+		final var fetchedAgencyRecords = manyValuesFrom(agencyRepository.queryAll());
+
+		// Assert
+		assertThat(fetchedAgencyRecords, hasSize(1));
 	}
 
 	@Test
 	void testBibRepoCount() {
-		final List<RecordCountSummary> summaryReport = Flux.from(bibRepository.getIngestReport())
-			.collectList()
-			.block();
+		// Arrange
+		final var firstHostLms = hostLmsFixture.createSierraHostLms("first-host-lms");
+		final var secondHostLms = hostLmsFixture.createSierraHostLms("second-host-lms");
 
-		assertThat(summaryReport.size(), is(2));
+		createBibRecord(firstHostLms, "6737415");
+		createBibRecord(secondHostLms, "4623456");
+
+		// Act
+		final var summaryReport = manyValuesFrom(bibRepository.getIngestReport());
+
+		// Assert
+		assertThat(summaryReport, hasSize(2));
+	}
+
+	private void createBibRecord(DataHostLms firstHostLms, String sourceRecordId) {
+		final var bibRecordId = randomUUID();
+
+		final var clusterRecord = clusterRecordFixture.createClusterRecord(
+			randomUUID(), bibRecordId);
+
+		bibRecordFixture.createBibRecord(bibRecordId, firstHostLms.getId(),
+			sourceRecordId, clusterRecord);
 	}
 }
