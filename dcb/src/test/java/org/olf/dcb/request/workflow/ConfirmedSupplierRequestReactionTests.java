c5781c42097f4467220ba5501536a331880dbed4
Marc Johnson <marc@k-int.co.uk>
2024-02-21 14:50:26 +0000

    test: Delete all patron requests before confirmation reaction tests DCB-884

--- dcb/src/test/java/org/olf/dcb/request/workflow/ConfirmedSupplierRequestReactionTests.java
+++ dcb/src/test/java/org/olf/dcb/request/workflow/ConfirmedSupplierRequestReactionTests.java
@@ -4,8 +4,10 @@ import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.notNullValue;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.olf.dcb.test.DcbTest;
+import org.olf.dcb.test.PatronRequestsFixture;
 
 import jakarta.inject.Inject;
 
@@ -13,6 +15,14 @@ import jakarta.inject.Inject;
 class ConfirmedSupplierRequestReactionTests {
 	@Inject
 	HostLmsReactions hostLmsReactions;
+
+	@Inject
+	PatronRequestsFixture patronRequestsFixture;
+
+	@BeforeEach
+	void beforeEach() {
+		patronRequestsFixture.deleteAll();
+	}
 
 	@Test
 	void shouldReactToLocalSupplierRequestChangingToConfirmed() {
