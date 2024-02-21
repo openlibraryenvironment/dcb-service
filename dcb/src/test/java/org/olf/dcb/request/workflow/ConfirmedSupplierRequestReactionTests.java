b0c63df47aea3b628e5e1c1adcd55b3dcf8f14e0
Marc Johnson <marc@k-int.co.uk>
2024-02-21 14:51:17 +0000

    test: Delete all supplier requests before confirmation reaction tests DCB-884

--- dcb/src/test/java/org/olf/dcb/request/workflow/ConfirmedSupplierRequestReactionTests.java
+++ dcb/src/test/java/org/olf/dcb/request/workflow/ConfirmedSupplierRequestReactionTests.java
@@ -8,6 +8,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.olf.dcb.test.DcbTest;
 import org.olf.dcb.test.PatronRequestsFixture;
+import org.olf.dcb.test.SupplierRequestsFixture;
 
 import jakarta.inject.Inject;
 
@@ -18,9 +19,12 @@ class ConfirmedSupplierRequestReactionTests {
 
 	@Inject
 	PatronRequestsFixture patronRequestsFixture;
-
+	@Inject
+	SupplierRequestsFixture supplierRequestsFixture;
+	
 	@BeforeEach
 	void beforeEach() {
+		supplierRequestsFixture.deleteAll();
 		patronRequestsFixture.deleteAll();
 	}
 
