055f3d184dcb5ece64dd2bc3d83971e30af2667e
Marc Johnson <marc@k-int.co.uk>
2024-02-21 14:56:21 +0000

    test: Delete all Host LMS before confirmation reaction tests DCB-884

--- dcb/src/test/java/org/olf/dcb/request/workflow/ConfirmedSupplierRequestReactionTests.java
+++ dcb/src/test/java/org/olf/dcb/request/workflow/ConfirmedSupplierRequestReactionTests.java
@@ -8,6 +8,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.olf.dcb.test.AgencyFixture;
 import org.olf.dcb.test.DcbTest;
+import org.olf.dcb.test.HostLmsFixture;
 import org.olf.dcb.test.PatronRequestsFixture;
 import org.olf.dcb.test.SupplierRequestsFixture;
 
@@ -24,12 +25,15 @@ class ConfirmedSupplierRequestReactionTests {
 	SupplierRequestsFixture supplierRequestsFixture;
 	@Inject
 	AgencyFixture agencyFixture;
+	@Inject
+	HostLmsFixture hostLmsFixture;
 
 	@BeforeEach
 	void beforeEach() {
 		supplierRequestsFixture.deleteAll();
 		patronRequestsFixture.deleteAll();
 		agencyFixture.deleteAll();
+		hostLmsFixture.deleteAll();
 	}
 
 	@Test
