e309d1281e9bc49b54aa5d2fdd30e5e38fea3080
Ian Ibbotson <ian.ibbotson@k-int.com>
2024-03-06 08:03:20 +0000

    Added show_stats.sh script

---
+++ scripts/show_stats.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+TARGET="http://localhost:8080"
+
+TOKEN=`./login`
+
+echo show statistics
+echo
+curl -H "Authorization: Bearer $TOKEN" -H "Content-Type: application/json"  -X GET "$TARGET/admin/statistics"
+curl -H "Authorization: Bearer $TOKEN" -H "Content-Type: application/json"  -X GET "$TARGET/admin/recordCounts"
