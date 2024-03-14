37dd9405aa11e5b3a21b39b5fa053082a4527753
Marc Johnson <marc@k-int.co.uk>
2024-03-14 18:23:44 +0000

    Demonstrate parsing MARC document with no 041 field for languages DCB-394

---
+++ dcb/src/test/java/services/k_int/integration/marc4j/MarcLanguageInterpretationTests.java
@@ -0,0 +1,30 @@
+package services.k_int.integration.marc4j;
+
+import static java.util.Collections.emptyList;
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.empty;
+
+import java.util.List;
+
+import org.junit.jupiter.api.Test;
+import org.marc4j.marc.Record;
+import org.marc4j.marc.impl.RecordImpl;
+
+class MarcLanguageInterpretationTests {
+	@Test
+	void shouldFindNoLanguagesWhenNo041FieldIsPresent() {
+		// Arrange
+		final var marcRecord = new RecordImpl();
+
+		// Act
+		final var languages = interpretLanguages(marcRecord);
+
+		// Assert
+		assertThat(languages, is(empty()));
+	}
+
+	List<String> interpretLanguages(final Record marcRecord) {
+		return emptyList();
+	}
+}
