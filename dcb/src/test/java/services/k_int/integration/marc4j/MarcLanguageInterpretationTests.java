afcd91c75ea41b9eb5be972eb200c6e90a333659
Marc Johnson <marc@k-int.co.uk>
2024-03-14 19:02:02 +0000

    Demonstrate interpreting single language code subfield in MARC DCB-394

--- dcb/src/test/java/services/k_int/integration/marc4j/MarcLanguageInterpretationTests.java
+++ dcb/src/test/java/services/k_int/integration/marc4j/MarcLanguageInterpretationTests.java
@@ -3,15 +3,20 @@ package services.k_int.integration.marc4j;
 import static java.util.Collections.emptyList;
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.containsInAnyOrder;
 import static org.hamcrest.Matchers.empty;
 
 import java.util.List;
 
 import org.junit.jupiter.api.Test;
+import org.marc4j.marc.DataField;
 import org.marc4j.marc.MarcFactory;
 import org.marc4j.marc.Record;
 import org.marc4j.marc.impl.MarcFactoryImpl;
 
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 class MarcLanguageInterpretationTests {
 	private static final MarcFactory marcFactory = new MarcFactoryImpl();
 
@@ -27,7 +32,36 @@ class MarcLanguageInterpretationTests {
 		assertThat(languages, is(empty()));
 	}
 
+	@Test
+	void shouldFindSingleLanguageForSingle041aSubfieldWithSingleLanguageCode() {
+		// Arrange
+		final var marcRecord = marcFactory.newRecord();
+
+		final var languageCodeField = marcFactory.newDataField("041", '#', '#');
+
+		final var aSubfield = marcFactory.newSubfield('a', "eng");
+
+		languageCodeField.addSubfield(aSubfield);
+
+		marcRecord.addVariableField(languageCodeField);
+
+		// Act
+		final var languages = interpretLanguages(marcRecord);
+
+		// Assert
+		assertThat(languages, containsInAnyOrder("eng"));
+	}
+
 	List<String> interpretLanguages(final Record marcRecord) {
-		return emptyList();
+		log.debug("MARC record: {}", marcRecord);
+
+		final var languageCodeField = marcRecord.getVariableField("041");
+
+		if (languageCodeField instanceof DataField languageDataField) {
+			return List.of(languageDataField.getSubfield('a').getData());
+		}
+		else {
+			return emptyList();
+		}
 	}
 }
