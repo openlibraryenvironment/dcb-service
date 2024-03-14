5c05a617658a87551daf4ad88e723ff635fc131f
Marc Johnson <marc@k-int.co.uk>
2024-03-14 19:40:04 +0000

    Demonstrate parsing langage from multiple 041 fields DCB-394

--- dcb/src/test/java/services/k_int/integration/marc4j/MarcLanguageInterpretationTests.java
+++ dcb/src/test/java/services/k_int/integration/marc4j/MarcLanguageInterpretationTests.java
@@ -1,18 +1,19 @@
 package services.k_int.integration.marc4j;
 
-import static java.util.Collections.emptyList;
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.containsInAnyOrder;
 import static org.hamcrest.Matchers.empty;
 
 import java.util.List;
+import java.util.stream.Stream;
 
 import org.junit.jupiter.api.Test;
 import org.marc4j.marc.DataField;
 import org.marc4j.marc.MarcFactory;
 import org.marc4j.marc.Record;
 import org.marc4j.marc.Subfield;
+import org.marc4j.marc.VariableField;
 import org.marc4j.marc.impl.MarcFactoryImpl;
 
 import lombok.extern.slf4j.Slf4j;
@@ -58,7 +59,7 @@ class MarcLanguageInterpretationTests {
 	}
 
 	@Test
-	void shouldFindMultipleLanguagesForMultiple041aSubfieldEachWithSingleLanguageCode() {
+	void shouldFindMultipleLanguagesForMultiple041aSubfieldsEachWithSingleLanguageCode() {
 		// Arrange
 		final var marcRecord = createMarcRecord("eng", "fre");
 
@@ -69,21 +70,41 @@ class MarcLanguageInterpretationTests {
 		assertThat(languages, containsInAnyOrder("eng", "fre"));
 	}
 
+	@Test
+	void shouldFindMultipleLanguagesForMultiple041aSubfieldsInSeparateFields() {
+		// Arrange
+		final var marcRecord = marcFactory.newRecord();
+
+		addLanguageCodeField(marcRecord, "ger");
+		addLanguageCodeField(marcRecord, "swe");
+
+		// Act
+		final var languages = interpretLanguages(marcRecord);
+
+		// Assert
+		assertThat(languages, containsInAnyOrder("ger", "swe"));
+	}
+
 	List<String> interpretLanguages(final Record marcRecord) {
 		log.debug("MARC record: {}", marcRecord);
 
 		assertThat(marcFactory.validateRecord(marcRecord), is(true));
 
-		final var languageCodeField = marcRecord.getVariableField("041");
+		final var languageCodeFields = marcRecord.getVariableFields("041");
 
+		return languageCodeFields.stream()
+			.flatMap(MarcLanguageInterpretationTests::parseSubFields)
+			.toList();
+	}
+
+	private static Stream<String> parseSubFields(final VariableField languageCodeField) {
 		if (languageCodeField instanceof DataField languageDataField) {
 			return languageDataField.getSubfields('a')
 				.stream()
-				.map(Subfield::getData)
-				.toList();
+				.map(Subfield::getData);
 		}
 		else {
-			return emptyList();
+			return Stream.of();
 		}
 	}
 
@@ -103,5 +124,15 @@ class MarcLanguageInterpretationTests {
 		marcRecord.addVariableField(languageCodeField);
 
 		return marcRecord;
+	}
+
+	private static void addLanguageCodeField(Record marcRecord, String languageCode) {
+		final var languageCodeField = marcFactory.newDataField("041", '#', '#');
+
+		final var aSubfield = marcFactory.newSubfield('a', languageCode);
+
+		languageCodeField.addSubfield(aSubfield);
+
+		marcRecord.addVariableField(languageCodeField);
 	}
 }
