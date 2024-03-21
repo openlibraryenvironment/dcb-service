21a8d2fc319aeb25fc29801f226f4391d50fe25b
Ian Ibbotson <ian.ibbotson@k-int.com>
2024-03-21 11:59:20 +0000

    added scripts/perftest

---
+++ scripts/perftest/patron_auth_test.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+TARGET="http://localhost:8080"
+
+RESHARE_ROOT_UUID=`uuidgen --sha1 -n @dns --name org.olf.dcb`
+AGENCIES_NS_UUID=`uuidgen --sha1 -n $RESHARE_ROOT_UUID --name Agency`
+HOSTLMS_NS_UUID=`uuidgen --sha1 -n $RESHARE_ROOT_UUID --name HostLms`
+LOCATION_NS_UUID=`uuidgen --sha1 -n $RESHARE_ROOT_UUID --name Location`
+
+echo Logging in
+TOKEN=`../login`
+
+sleep 1
+echo
+echo "Patron auth: "
+auth_request_data='{"agencyCode": "DA-1-1", "patronPrinciple": "1234", "secret": "1234"}'
+curl -s -X POST "$TARGET/patron/auth" -H "Accept-Language: en" -H "Content-Type: application/json" -H "Authorization: Bearer $TOKEN" -d "${auth_request_data}" | json_pp
+
