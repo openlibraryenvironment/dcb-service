6c9b30ac3a9d2de0a0b53521b54b3057de46219a
Marc Johnson <marc@k-int.co.uk>
2024-03-21 10:33:30 +0000

    Replace spaces with tabs in choose first resolution strategy

--- dcb/src/main/java/org/olf/dcb/request/resolution/FirstRequestableItemResolutionStrategy.java
+++ dcb/src/main/java/org/olf/dcb/request/resolution/FirstRequestableItemResolutionStrategy.java
@@ -1,45 +1,40 @@
 package org.olf.dcb.request.resolution;
 
+import static org.olf.dcb.utils.PropertyAccessUtils.getValue;
+
 import java.util.List;
 import java.util.UUID;
 
 import org.olf.dcb.core.model.Item;
 import org.olf.dcb.core.model.PatronRequest;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
 import jakarta.inject.Singleton;
-
+import lombok.extern.slf4j.Slf4j;
 import reactor.core.publisher.Mono;
 
+@Slf4j
 @Singleton
 public class FirstRequestableItemResolutionStrategy implements ResolutionStrategy {
-	private static final Logger log = LoggerFactory.getLogger(FirstRequestableItemResolutionStrategy.class);
-
-	public static final String FIRST_ITEM_STRATEGY = "FirstItem";
-
 	@Override
 	public String getCode() {
-		return FIRST_ITEM_STRATEGY;
+		return "FirstItem";
 	}
-
-
+	
 	@Override
 	public Mono<Item> chooseItem(List<Item> items, UUID clusterRecordId, PatronRequest patronRequest) {
-
-                UUID patronRequestId = patronRequest != null ? patronRequest.getId() : null;
+		final var patronRequestId = getValue(patronRequest, PatronRequest::getId);
 
 		log.info("PR-{} - chooseItem(array of size {})", patronRequestId, items.size());
 
 		return Mono.just(
 			items.stream()
-                       		.peek(item -> log.info("PR-{} - Consider item {} @ {} requestable:{} holds:{} ",
-                                        patronRequestId,
-                                        item.getLocalId(),
-                                        item.getLocation(),
-                                        item.getIsRequestable(),
-                                        item.hasNoHolds()))
+				.peek(item -> log.info(
+						"PR-{} - Consider item {} @ {} requestable:{} holds:{} ",
+					patronRequestId,
+					item.getLocalId(),
+					item.getLocation(),
+					item.getIsRequestable(),
+					item.hasNoHolds()))
 				.filter(Item::getIsRequestable)
 				.filter(Item::hasNoHolds)
 				.findFirst()
