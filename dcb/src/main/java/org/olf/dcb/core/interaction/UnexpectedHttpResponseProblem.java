970df60cf5714bd074eed76ee151d8f203fc03b0
Marc Johnson <marc@k-int.co.uk>
2024-04-09 11:27:28 +0100

    Introduce base class for HTTP response related problems

--- dcb/src/main/java/org/olf/dcb/core/interaction/UnexpectedHttpResponseProblem.java
+++ dcb/src/main/java/org/olf/dcb/core/interaction/UnexpectedHttpResponseProblem.java
@@ -3,21 +3,13 @@ package org.olf.dcb.core.interaction;
 import static io.micronaut.core.util.StringUtils.isEmpty;
 import static io.micronaut.core.util.StringUtils.isNotEmpty;
 import static org.olf.dcb.utils.PropertyAccessUtils.getValue;
-import static org.olf.dcb.utils.PropertyAccessUtils.getValueOrDefault;
-import static org.zalando.problem.Status.INTERNAL_SERVER_ERROR;
 
-import java.net.URI;
-import java.util.Map;
-
-import org.zalando.problem.AbstractThrowableProblem;
 import org.zalando.problem.ThrowableProblem;
 
-import io.micronaut.core.type.Argument;
 import io.micronaut.http.HttpRequest;
-import io.micronaut.http.HttpStatus;
 import io.micronaut.http.client.exceptions.HttpClientResponseException;
 
-public class UnexpectedHttpResponseProblem extends AbstractThrowableProblem {
+public class UnexpectedHttpResponseProblem extends AbstractHttpResponseProblem {
 	public static <T> ThrowableProblem unexpectedResponseProblem(
 		HttpClientResponseException responseException, HttpRequest<T> request, String hostLmsCode) {
 
@@ -27,9 +19,7 @@ public class UnexpectedHttpResponseProblem extends AbstractThrowableProblem {
 	private UnexpectedHttpResponseProblem(HttpClientResponseException responseException,
 		HttpRequest<?> request, String hostLmsCode) {
 
-		super(URI.create("https://openlibraryfoundation.atlassian.net/wiki/spaces/DCB/overview"),
-			determineTitle(hostLmsCode, request), INTERNAL_SERVER_ERROR, null, null, null,
-			determineParameters(responseException, request));
+		super(determineTitle(hostLmsCode, request), responseException, request);
 	}
 
 	private static String determineTitle(String hostLmsCode, HttpRequest<?> request) {
@@ -44,42 +34,5 @@ public class UnexpectedHttpResponseProblem extends AbstractThrowableProblem {
 				getValue(request, HttpRequest::getMethodName),
 				getValue(request, HttpRequest::getPath));
 		}
-	}
-
-	private static Map<String, Object> determineParameters(
-		HttpClientResponseException responseException, HttpRequest<?> request) {
-
-		return Map.of(
-			"responseStatusCode", getValue(responseException.getStatus(), HttpStatus::getCode),
-			"responseBody", interpretResponseBody(responseException),
-			"requestMethod", getValueOrDefault(request, HttpRequest::getMethodName, "Unknown"),
-			"requestUrl", getValue(request, HttpRequest::getUri, URI::toString, "Unknown"),
-			"requestBody", interpretRequestBody(request)
-		);
-	}
-
-	private static Object interpretResponseBody(HttpClientResponseException responseException) {
-		final var response = getValue(responseException, HttpClientResponseException::getResponse);
-
-		final var optionalJsonBody = response.getBody(Argument.of(Map.class));
-
-		if (optionalJsonBody.isPresent()) {
-			return optionalJsonBody.get();
-		}
-		else {
-			return response.getBody(Argument.of(String.class)).orElse(noBodyMessage());
-		}
-	}
-
-	private static String noBodyMessage() {
-		return "No body";
-	}
-
-	private static Object interpretRequestBody(HttpRequest<?> request) {
-		if (request == null) {
-			return noBodyMessage();
-		}
-
-		return request.getBody(Argument.of(String.class)).orElse(noBodyMessage());
 	}
 }
