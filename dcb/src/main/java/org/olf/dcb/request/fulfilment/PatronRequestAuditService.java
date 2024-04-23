a35a0f6e875f11839e981d573f9574f963a8153e
Alex Scott <alex.scott@k-int.co.uk>
2024-04-23 14:44:28 +0000

    feat: capture request workflow and state transition metrics [DCB-1039]

--- dcb/src/main/java/org/olf/dcb/request/fulfilment/PatronRequestAuditService.java
+++ dcb/src/main/java/org/olf/dcb/request/fulfilment/PatronRequestAuditService.java
@@ -4,10 +4,9 @@ import static java.util.Optional.empty;
 import static org.olf.dcb.core.model.PatronRequest.Status.ERROR;
 import static services.k_int.utils.StringUtils.truncate;
 
+import java.time.Duration;
 import java.time.Instant;
-import java.util.Map;
-import java.util.Optional;
-import java.util.UUID;
+import java.util.*;
 
 import org.olf.dcb.core.model.PatronRequest;
 import org.olf.dcb.core.model.PatronRequest.Status;
@@ -18,6 +17,7 @@ import org.olf.dcb.storage.PatronRequestRepository;
 import io.micronaut.transaction.annotation.Transactional;
 import jakarta.inject.Singleton;
 import lombok.extern.slf4j.Slf4j;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 @Slf4j
@@ -61,7 +61,91 @@ public class PatronRequestAuditService {
 
 		message.ifPresent(value -> builder.briefDescription(truncate(value, 254)));
 
-		return buildAndSaveAuditMessage(builder);
+		return stateTransitionMetrics(patronRequest, from, to, builder)
+			.flatMap(this::buildAndSaveAuditMessage);
+	}
+
+	private Mono<PatronRequestAudit.PatronRequestAuditBuilder> stateTransitionMetrics(
+		PatronRequest patronRequest, Status from, Status to,
+		PatronRequestAudit.PatronRequestAuditBuilder builder) {
+		
+		return fetchLastStateChangeAudit(patronRequest)
+			.flatMap(pr -> {
+				
+				if (pr.getPatronRequestAudits() == null) {
+					log.warn("Unable to add transition metrics because audit were null.");
+					return Mono.just(builder);
+				}
+
+				final var lastStateChangeAudit = pr.getPatronRequestAudits().get(0);
+				final var isStateChange = from.equals(to);
+
+				return Mono
+					.just(addCurrentStateTimestamp(builder, lastStateChangeAudit, isStateChange))
+					.map(b -> addNextExpectedStatus(to, b, lastStateChangeAudit, isStateChange))
+					.map(b -> addOutOfSequenceFlag(to, b, lastStateChangeAudit, isStateChange));
+			});
+	}
+
+	private static PatronRequestAudit.PatronRequestAuditBuilder addOutOfSequenceFlag(
+		Status to, PatronRequestAudit.PatronRequestAuditBuilder builder,
+		PatronRequestAudit lastStateChangeAudit, Boolean isStateChange) {
+
+		if (!isStateChange) {
+			// use last audit flag if no status change
+			builder.outOfSequenceFlag(lastStateChangeAudit.getOutOfSequenceFlag());
+
+		} else if (lastStateChangeAudit.getNextExpectedStatus() != null &&
+			!to.equals(lastStateChangeAudit.getNextExpectedStatus()) ) {
+
+			// the to status wasn't what we expected
+			builder.outOfSequenceFlag(Boolean.TRUE);
+
+		} else {
+			// everything seems to check out
+			builder.outOfSequenceFlag(Boolean.FALSE);
+
+		}
+
+		return builder;
+	}
+
+	private static PatronRequestAudit.PatronRequestAuditBuilder addNextExpectedStatus(
+		Status to, PatronRequestAudit.PatronRequestAuditBuilder builder,
+		PatronRequestAudit lastStateChangeAudit, Boolean isStateChange) {
+
+		if (isStateChange) {
+			// get next status (happy path)
+			builder.nextExpectedStatus(to.getNextExpectedStatus());
+
+		} else {
+			// we use the last expected status when it's not a state change
+			builder.nextExpectedStatus(lastStateChangeAudit.getNextExpectedStatus());
+
+		}
+
+		return builder;
+	}
+
+	private static PatronRequestAudit.PatronRequestAuditBuilder addCurrentStateTimestamp(
+		PatronRequestAudit.PatronRequestAuditBuilder builder, PatronRequestAudit lastStateChangeAudit,
+		Boolean isStateChange) {
+
+		if (isStateChange) {
+			// new to status
+			builder.currentStatusStamp(Instant.now());
+			builder.elapsedTimeInCurrentStatus(Duration.ZERO.getSeconds());
+
+		} else {
+			// get last state change timestamp
+			builder.currentStatusStamp(lastStateChangeAudit.getCurrentStatusStamp());
+
+			final var duration = Duration.between(lastStateChangeAudit.getAuditDate(), Instant.now());
+			builder.elapsedTimeInCurrentStatus(duration.getSeconds());
+
+		}
+
+		return builder;
 	}
 
 	@Transactional
@@ -83,6 +167,28 @@ public class PatronRequestAuditService {
 		return Mono.from(patronRequestRepository.findById(patronRequestId))
 			.flatMap(pr -> addAuditEntry(pr, pr.getStatus(), pr.getStatus(),
 				Optional.ofNullable(message), Optional.ofNullable(auditData)));
+	}
+
+	private Mono<PatronRequest> fetchLastStateChangeAudit(PatronRequest patronRequest) {
+		Status previousStatus = patronRequest.getPreviousStatus();
+
+		if (previousStatus == null) {
+			log.debug("previous status was null could not fetch last state change audit.");
+			return Mono.just(patronRequest);
+		}
+
+		return Flux.from(patronRequestAuditRepository.findAllByPatronRequestAndToStatusEquals(patronRequest, previousStatus))
+			.filter(patronRequestAudit -> patronRequestAudit.getFromStatus() != patronRequestAudit.getToStatus())
+			.collectSortedList(Comparator.comparing(PatronRequestAudit::getAuditDate).reversed())
+			.map(audits -> {
+				if (!audits.isEmpty()) {
+					patronRequest.setPatronRequestAudits(Collections.singletonList(audits.get(0)));
+				}
+				return patronRequest;
+			})
+			// handle cases we didn't fetch the last state change audit
+			.onErrorResume(NullPointerException.class, ex -> Mono.just(patronRequest))
+			.defaultIfEmpty(patronRequest);
 	}
 
 	public Mono<PatronRequestAudit> addAuditEntry(PatronRequest pr, String message) {
