b1120f6ceddf00407edce37c220c3e3c8691289e
Marc Johnson <marc@k-int.co.uk>
2024-06-05 14:50:43 +0100

    Map command to patron request during preflight DCB-1173

--- dcb/src/main/java/org/olf/dcb/request/fulfilment/ResolvePatronRequestPreflightCheck.java
+++ dcb/src/main/java/org/olf/dcb/request/fulfilment/ResolvePatronRequestPreflightCheck.java
@@ -3,6 +3,9 @@ package org.olf.dcb.request.fulfilment;
 import static org.olf.dcb.request.fulfilment.CheckResult.passed;
 
 import java.util.List;
+import java.util.UUID;
+
+import org.olf.dcb.core.model.PatronRequest;
 
 import io.micronaut.context.annotation.Requires;
 import jakarta.inject.Singleton;
@@ -15,6 +18,22 @@ import reactor.core.publisher.Mono;
 public class ResolvePatronRequestPreflightCheck implements PreflightCheck {
 	@Override
 	public Mono<List<CheckResult>> check(PlacePatronRequestCommand command) {
-		return Mono.just(List.of(passed()));
+		return Mono.just(command)
+			// This is a workaround due to the current resolution process
+			// being too coupled to a patron request
+			.map(this::mapToPatronRequest)
+			.map(patronRequest -> List.of(passed()));
+	}
+
+	private PatronRequest mapToPatronRequest(PlacePatronRequestCommand command) {
+		log.debug("mapToPatronRequest({})", command);
+
+		return PatronRequest.builder()
+			.id(UUID.randomUUID())
+			.bibClusterId(command.getCitation().getBibClusterId())
+			.requestedVolumeDesignation(command.getCitation().getVolumeDesignator())
+			.pickupLocationCodeContext(command.getPickupLocationContext())
+			.pickupLocationCode(command.getPickupLocationCode())
+			.build();
 	}
 }
