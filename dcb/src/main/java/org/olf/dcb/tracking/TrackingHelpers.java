da879627c9129e755978a3aa83e148cec86106d5
Marc Johnson <marc@k-int.co.uk>
2024-06-27 20:36:14 +0100

    Replace spaces with tabs in tracking helpers

--- dcb/src/main/java/org/olf/dcb/tracking/TrackingHelpers.java
+++ dcb/src/main/java/org/olf/dcb/tracking/TrackingHelpers.java
@@ -1,17 +1,11 @@
 package org.olf.dcb.tracking;
 
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.Optional;
 import java.time.Duration;
+import java.util.Optional;
 
-import org.olf.dcb.core.model.PatronRequest;
 import org.olf.dcb.core.model.PatronRequest.Status;
 
 public class TrackingHelpers {
-
-
 	// See: https://openlibraryfoundation.atlassian.net/wiki/spaces/DCB/pages/2870575137/Tracking+v3+matrix
   //
 	public static Optional<Duration> getDurationFor(Status pr_status) {
@@ -20,24 +14,24 @@ public class TrackingHelpers {
 
 	public static Optional<Duration> getDevDurationFor(Status pr_status) {
 		Duration result = switch ( pr_status ) {
-      case SUBMITTED_TO_DCB -> null;
-      case PATRON_VERIFIED -> null;
-      case RESOLVED -> null;
-      case NOT_SUPPLIED_CURRENT_SUPPLIER -> null;
-      case NO_ITEMS_AVAILABLE_AT_ANY_AGENCY -> null;
+			case SUBMITTED_TO_DCB -> null;
+			case PATRON_VERIFIED -> null;
+			case RESOLVED -> null;
+			case NOT_SUPPLIED_CURRENT_SUPPLIER -> null;
+			case NO_ITEMS_AVAILABLE_AT_ANY_AGENCY -> null;
 			// We want to poll in real time for confirmed - in part this is due to tests 1m would be OK
 			case REQUEST_PLACED_AT_SUPPLYING_AGENCY -> Duration.ofMillis(10);
-      case CONFIRMED -> null;
-      case REQUEST_PLACED_AT_BORROWING_AGENCY -> Duration.ofMinutes(10);
-      case RECEIVED_AT_PICKUP -> Duration.ofMinutes(10);
-      case READY_FOR_PICKUP -> Duration.ofMinutes(10);
-      case LOANED -> Duration.ofMinutes(10);
-      case PICKUP_TRANSIT -> Duration.ofMinutes(10);
-      case RETURN_TRANSIT -> Duration.ofMinutes(10);
-      case CANCELLED -> null;
-      case COMPLETED -> null;
-      case FINALISED -> null;
-      case ERROR -> null;
+			case CONFIRMED -> null;
+			case REQUEST_PLACED_AT_BORROWING_AGENCY -> Duration.ofMinutes(10);
+			case RECEIVED_AT_PICKUP -> Duration.ofMinutes(10);
+			case READY_FOR_PICKUP -> Duration.ofMinutes(10);
+			case LOANED -> Duration.ofMinutes(10);
+			case PICKUP_TRANSIT -> Duration.ofMinutes(10);
+			case RETURN_TRANSIT -> Duration.ofMinutes(10);
+			case CANCELLED -> null;
+			case COMPLETED -> null;
+			case FINALISED -> null;
+			case ERROR -> null;
 			default -> null;
 		};
 		return Optional.ofNullable(result);
@@ -67,5 +61,4 @@ public class TrackingHelpers {
 		};
 		return Optional.ofNullable(result);
 	}
-
 }
