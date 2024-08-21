edc494c2f088626dd82a7d5d35c792b5240a4113
Alex <ajscottleicester@gmail.com>
2024-08-21 10:27:29 +0100

    feat: manually finalise incomplete requests [DCB-1346]

--- dcb/src/main/java/org/olf/dcb/request/workflow/CleanupPatronRequestTransition.java
+++ dcb/src/main/java/org/olf/dcb/request/workflow/CleanupPatronRequestTransition.java
@@ -10,6 +10,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import reactor.core.publisher.Mono;
 
+import java.util.HashMap;
 import java.util.List;
 import java.util.Optional;
 
@@ -18,15 +19,30 @@ public class CleanupPatronRequestTransition implements PatronRequestStateTransit
 	
 	private static final Logger log = LoggerFactory.getLogger(CleanupPatronRequestTransition.class);
 
-	private static final List<Status> possibleSourceStatus = List.of(Status.ERROR);
-	
-  private final PatronRequestAuditService patronRequestAuditService;
+	private static final List<Status> possibleSourceStatus = List.of(
+		// Unexpected Termination
+		Status.ERROR,
+		// Non-terminal states
+		Status.SUBMITTED_TO_DCB,
+		Status.PATRON_VERIFIED,
+		Status.RESOLVED,
+		Status.REQUEST_PLACED_AT_SUPPLYING_AGENCY,
+		Status.CONFIRMED,
+		Status.REQUEST_PLACED_AT_BORROWING_AGENCY,
+		Status.PICKUP_TRANSIT,
+		Status.RECEIVED_AT_PICKUP,
+		Status.READY_FOR_PICKUP,
+		Status.LOANED,
+		Status.RETURN_TRANSIT
+	);
 
-  public CleanupPatronRequestTransition(
-    PatronRequestAuditService patronRequestAuditService
-    ) {
-    this.patronRequestAuditService = patronRequestAuditService;
-  }
+	private final PatronRequestAuditService patronRequestAuditService;
+
+	public CleanupPatronRequestTransition(
+		PatronRequestAuditService patronRequestAuditService
+	) {
+		this.patronRequestAuditService = patronRequestAuditService;
+	}
 
 	@Override
 	public boolean isApplicableFor(RequestWorkflowContext ctx) {
@@ -37,13 +53,23 @@ public class CleanupPatronRequestTransition implements PatronRequestStateTransit
 	public Mono<RequestWorkflowContext> attempt(RequestWorkflowContext ctx) {
 
 		PatronRequest patronRequest = ctx.getPatronRequest();
-    log.info("CleanupPatronRequestTransition firing for {}",patronRequest);
+		log.info("CleanupPatronRequestTransition firing for {}",patronRequest);
+
+		// checking cleanup hasn't been actioned incorrectly
+		if (!this.isApplicableFor(ctx)) {
+
+			final var message = "Manual cleanup failed.";
+			final var auditData = new HashMap<String, Object>();
+			auditData.put("Reason", "Patron request was not applicable for " + getName());
+			return patronRequestAuditService.addAuditEntry(patronRequest, message, auditData)
+				.thenReturn(ctx);
+		}
 
 		// Setting the status to completed should cause the cleanup routine to fire which will do all the work we need to FINALISE the request
-    Status old_state = patronRequest.getStatus();
-    patronRequest.setStatus(Status.COMPLETED);
-    return patronRequestAuditService.addAuditEntry(patronRequest, "Manual cleanup actioned.")
-      .thenReturn(ctx);
+		Status old_state = patronRequest.getStatus();
+		patronRequest.setStatus(Status.COMPLETED);
+		return patronRequestAuditService.addAuditEntry(patronRequest, "Manual cleanup actioned.")
+			.thenReturn(ctx);
 	}
 
 	@Override
