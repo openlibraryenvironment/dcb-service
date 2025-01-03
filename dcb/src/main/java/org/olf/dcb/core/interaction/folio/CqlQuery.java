f1675e83095ecb50183ae353fd1010fd225eac34
Chas <chas.woodfield@k-int.com>
2025-01-03 16:28:32 +0000

    FIX: Have added the annotation to CqlQuery to stop the exception occurring

--- dcb/src/main/java/org/olf/dcb/core/interaction/folio/CqlQuery.java
+++ dcb/src/main/java/org/olf/dcb/core/interaction/folio/CqlQuery.java
@@ -2,11 +2,13 @@ package org.olf.dcb.core.interaction.folio;
 
 import static lombok.AccessLevel.PRIVATE;
 
+import io.micronaut.serde.annotation.Serdeable;
 import lombok.AllArgsConstructor;
 import lombok.Value;
 
 @Value
 @AllArgsConstructor(access = PRIVATE)
+@Serdeable
 class CqlQuery {
 	String query;
 
