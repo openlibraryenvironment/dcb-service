082d09bf8f0851d0cd1b1096426c18044a04a1d5
Ian Ibbotson <ianibbo@gmail.com>
2025-02-26 07:50:06 +0000

    feat: Add the NO_LEND derived loan policy

--- dcb/src/main/java/org/olf/dcb/core/model/DerivedLoanPolicy.java
+++ dcb/src/main/java/org/olf/dcb/core/model/DerivedLoanPolicy.java
@@ -2,6 +2,7 @@ package org.olf.dcb.core.model;
 
 /** Host ILS adapters must infer from local rules and procedures what the canonical loan policy is with respect to consortial lending */
 public enum DerivedLoanPolicy {
+  NO_LEND,
   UNKNOWN,
   SHORT_LOAN,
   REFERENCE_ONLY,
