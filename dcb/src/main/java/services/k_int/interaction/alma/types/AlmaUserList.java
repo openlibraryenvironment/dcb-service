927560108c058ae432e84c6b3d892a52e1d10d63
Ian Ibbotson <ianibbo@gmail.com>
2025-04-21 19:51:53 +0100

    chore: rename alma types

---
+++ dcb/src/main/java/services/k_int/interaction/alma/types/AlmaUserList.java
@@ -0,0 +1,20 @@
+package services.k_int.interaction.alma.types;
+
+import io.micronaut.serde.annotation.Serdeable;
+import java.util.List;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Data
+@AllArgsConstructor
+@Builder
+@ToString(onlyExplicitlyIncluded = true)
+@Serdeable
+public class AlmaUserList {
+	private List<AlmaUser> user;
+	private Long total_record_count;
+}
