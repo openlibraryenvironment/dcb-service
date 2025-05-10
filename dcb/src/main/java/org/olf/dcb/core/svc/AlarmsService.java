cbd75b18ece89ff818875c7fec989d52a59e4980
Ian Ibbotson <ianibbo@gmail.com>
2025-05-10 18:27:43 +0100

    feat: First pass of alarm notifications to system wide slack webhook. Set an env var like "DCB_GLOBAL_NOTIFICATIONS_WEBHOOKS[0]":"https://hooks.slack.com/services/***/***/***" to have alarms broadcast to the selected slack channel

--- dcb/src/main/java/org/olf/dcb/core/svc/AlarmsService.java
+++ dcb/src/main/java/org/olf/dcb/core/svc/AlarmsService.java
@@ -11,12 +11,22 @@ import reactor.core.publisher.Flux;
 import java.time.Instant;
 import java.util.List;
 import java.util.UUID;
+import java.util.Map;
 import java.util.concurrent.atomic.AtomicInteger;
+import java.net.URL;
+
+import io.micronaut.http.HttpRequest;
+import io.micronaut.http.MediaType;
+import io.micronaut.http.client.HttpClient;
+import io.micronaut.http.client.HttpClientConfiguration;
 
 import org.olf.dcb.storage.HostLmsRepository;
 import org.olf.dcb.core.model.Alarm;
 
 import org.olf.dcb.storage.AlarmRepository;
+import reactor.util.function.Tuples;
+import io.micronaut.context.annotation.ConfigurationProperties;
+import io.micronaut.context.annotation.Value;
 
 @Slf4j
 @Singleton
@@ -24,7 +34,15 @@ public class AlarmsService {
 
 	private final AlarmRepository alarmRepository;
 
-	public AlarmsService(AlarmRepository alarmRepository) {
+	// Default to an empty list
+	// Add webhooks in environment variables as
+	// DCB_GLOBAL_NOTIFICATIONS_WEBHOOKS[0]=https://example.com/notify
+	@Value("${dcb.global.notifications.webhooks:}")
+	List<String> webhookUrls;
+
+	public AlarmsService(
+		AlarmRepository alarmRepository
+	){
 		this.alarmRepository = alarmRepository;
 	}
 
@@ -40,7 +58,14 @@ public class AlarmsService {
 				existingAlarm.incrementRepeatCount();
 				return Mono.from(alarmRepository.update(existingAlarm)).cast(Alarm.class);
 			})
-			.switchIfEmpty( createNewAlarm(alarm) );
+			.switchIfEmpty( 
+				createNewAlarm(alarm) 
+					.flatMap( dbalarm -> 
+						optionallyNotify(dbalarm.getCode(), "ACTIVATED")
+							.map(result -> Tuples.of(dbalarm, result))
+					)
+					.map( tuple -> tuple.getT1() )
+			);
 	}	
 
 	private Mono<Alarm> createNewAlarm(Alarm alarm) {
@@ -48,6 +73,30 @@ public class AlarmsService {
 		alarm.setCreated(Instant.now());
 		alarm.setRepeatCount(Long.valueOf(0));
 		return Mono.from(alarmRepository.save(alarm));
+
+	}
+
+	private Mono<String> optionallyNotify(String alarmCode, String status) {
+		// Work out the context of the alarm, and see if that context has 
+		// email or slack notifications configured, if so send
+		log.info("Optionally notify {} : {}",alarmCode,status);
+		List<String> combined = this.webhookUrls; // + any hostLms specific ones
+
+		return Flux.fromIterable(combined)
+			.map( target -> {
+				log.info("Publish {} {} to {}",alarmCode, status, target);
+				
+				return publishToWebhook(target, Map.of("blocks", List.of(
+					Map.of(
+						"type", "section",
+						"text", Map.of(
+							"type", "markdown",
+							"text", "DCB-ALARM: "+alarmCode+" "+status
+						)
+					)
+				)));
+			})
+			.then(Mono.just("OK") );
 	}
 
 	// Prune expired alarms
@@ -55,12 +104,37 @@ public class AlarmsService {
     Instant now = Instant.now();
 
     Flux.from(alarmRepository.findByExpiresBefore(now))
-        .flatMap(alarm -> alarmRepository.delete(alarm.getId()))
+				.flatMap(alarm -> 
+					optionallyNotify(alarm.getCode(), "DEACTIVATED")
+						.map(code -> alarmRepository.delete(alarm.getId()))
+				)
         .subscribe();
 	}
 
 	public Mono<String> cancel(String code) {
 		return Mono.from(alarmRepository.deleteByCode(code))
+			.then( optionallyNotify(code, "DEACTIVATED") )
 			.thenReturn( code );
 	}
+
+
+	public Mono<Void> publishToWebhook(String url, Map<String, Object> payload) {
+		// This is where you get HttpClient programmatically
+		try {
+
+			HttpClient client = HttpClient.create(new URL(url));
+
+			HttpRequest<Map<String, Object>> request = HttpRequest
+				.POST("", payload)
+				.contentType(MediaType.APPLICATION_JSON_TYPE);
+                
+			// Sending the POST request to each webhook URL
+			return Mono.from(client.exchange(request))
+				.then();
+		}
+		catch ( Exception e ) {
+			return Mono.empty();
+		}
+	}
+
 }
