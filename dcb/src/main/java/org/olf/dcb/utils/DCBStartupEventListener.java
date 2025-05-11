f6d4b0389a0c16eb0d4b818217e882bb9c975633
Ian Ibbotson <ian.ibbotson@k-int.com>
2025-05-11 09:39:43 +0100

    chore: log webhooks at startup

--- dcb/src/main/java/org/olf/dcb/utils/DCBStartupEventListener.java
+++ dcb/src/main/java/org/olf/dcb/utils/DCBStartupEventListener.java
@@ -89,6 +89,8 @@ public class DCBStartupEventListener implements ApplicationEventListener<Startup
 	public void onApplicationEvent(StartupEvent event) {
 		log.info("Bootstrapping DCB - onApplicationEvent");
 
+    log.info("Configured system webhooks: {}",webhookUrls);
+
 		if (environment.getProperty(REACTOR_DEBUG_VAR, String.class)
 				.orElse("false").equalsIgnoreCase("true")) {
 			log.info("Switching on operator debug mode");
