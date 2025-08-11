a0c44262a6f6af89b038e77a84b0ce3639872d1c
Casey Henderson <casey.henderson@k-int.co.uk>
2025-08-11 15:31:24 +0100

    feat: Implement supplying agency filter [DCB-1973]
    To eliminate a particularly unwieldy and unreliable work-around in DCB Admin and Admin for Libraries, I have implemented a special use case in our server-side filtering support for the supplying agency code. This uses a join to enable us to provide an "equals" filter for supplying agency code in our frontend applications.
    
    This is particularly important for DCB Admin for Libraries as this filter is key to providing the "library supplier requests" view - the existing frontend work-arounds fail when dealing with supplier request libraries such as UMSYS with many thousands of requests.

--- dcb/src/main/java/services/k_int/data/querying/lucene/LuceneFieldQueryNodeBuilder.java
+++ dcb/src/main/java/services/k_int/data/querying/lucene/LuceneFieldQueryNodeBuilder.java
@@ -1,5 +1,7 @@
 package services.k_int.data.querying.lucene;
 
+import jakarta.persistence.criteria.Join;
+import jakarta.persistence.criteria.JoinType;
 import org.apache.lucene.queryparser.flexible.core.nodes.FieldQueryNode;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -11,17 +13,34 @@ import services.k_int.data.querying.JpaQuerySpecificationBuilder;
 public class LuceneFieldQueryNodeBuilder<T> implements JpaQuerySpecificationBuilder<T,FieldQueryNode> {
 
 	private static final Logger log = LoggerFactory.getLogger(LuceneFieldQueryNodeBuilder.class);
-	
+
 	@Override
 	public QuerySpecification<T> build(FieldQueryNode fieldNode) throws Exception {
 		var fieldName = fieldNode.getFieldAsString();
 		var fieldText = fieldNode.getTextAsString();
-		
+
 		log.debug("Field... {}:{}", fieldName, fieldText);
+
+		// Handle special fields - carefully - to provide filtering for them in DCB Admin / Admin for Libraries
+		// This is only for fields we cannot handle normally and should not be overused.
+		if ("supplyingAgencyCode".equals(fieldName)) {
+			return buildSupplyingAgencyCodeQuery(fieldText);
+		}
+
+		// Default behavior for regular fields
 		QuerySpecification<T> cb = (root, query, criteriaBuilder) -> {
 			Path<String> path = root.get(fieldName);
 			return criteriaBuilder.equal(path, fieldText);
 		};
 		return cb;
+	}
+
+	private QuerySpecification<T> buildSupplyingAgencyCodeQuery(String agencyCode) {
+		return (root, query, criteriaBuilder) -> {
+			// Join with SupplierRequest
+			Join<Object, Object> supplierRequestJoin = root.join("supplierRequests", JoinType.LEFT);
+			// May need ordering or selection to ensure we get the correct supplier request
+			return criteriaBuilder.equal(supplierRequestJoin.get("localAgency"), agencyCode);
+		};
 	}
 }
