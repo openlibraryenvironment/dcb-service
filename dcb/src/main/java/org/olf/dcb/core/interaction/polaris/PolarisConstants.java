0bde72c850949c7efd36ad15b66c08c98d50e127
alex <alex.scott@k-int.co.uk>
2025-08-14 17:01:12 +0100

    fix: spaces in polaris bib leader fields [DCB-1969]

--- dcb/src/main/java/org/olf/dcb/core/interaction/polaris/PolarisConstants.java
+++ dcb/src/main/java/org/olf/dcb/core/interaction/polaris/PolarisConstants.java
@@ -30,6 +30,6 @@ class PolarisConstants {
 	public static final String E_CONTENT_EXTERNAL_LOAN = "EContent External Loan";
 
 	// virtual bib values
-	public static final String VIRTUAL_BIB_BOOKS_LEADER = "LDR     cam  22      a 4500";
-	public static final String VIRTUAL_BIB_AV_LEADER = "LDR     cgm  22      a 4500";
+	public static final String VIRTUAL_BIB_BOOKS_LEADER = "LDR    cam 22     a 4500";
+	public static final String VIRTUAL_BIB_AV_LEADER = "LDR    cgm 22     a 4500";
 }
