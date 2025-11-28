1d539ef0008092b53e642cbc866330538ed7ebed
Marc Johnson <marc@k-int.co.uk>
2025-11-28 13:08:46 +0000

    Introduce item with holds item filter class

--- dcb/src/main/java/org/olf/dcb/request/resolution/AllItemFilters.java
+++ dcb/src/main/java/org/olf/dcb/request/resolution/AllItemFilters.java
@@ -1,12 +1,10 @@
 package org.olf.dcb.request.resolution;
 
-import static org.olf.dcb.core.model.FunctionalSettingType.SELECT_UNAVAILABLE_ITEMS;
 import static org.olf.dcb.utils.PropertyAccessUtils.getValueOrNull;
 import static services.k_int.utils.ReactorUtils.raiseError;
 
 import java.util.List;
 
-import org.olf.dcb.core.ConsortiumService;
 import org.olf.dcb.core.HostLmsService;
 import org.olf.dcb.core.model.Item;
 import org.zalando.problem.Problem;
@@ -25,22 +23,23 @@ public class AllItemFilters {
 	// Todo: This currently includes all of the current filters
 	// they could be split into separate classes
 	// then this can act as a composite https://guides.micronaut.io/latest/micronaut-patterns-composite-maven-java.html)
-	private final ConsortiumService consortiumService;
 	private final HostLmsService hostLmsService;
 	private final AgencyExclusionItemFilter agencyExclusionItemFilter;
 	private final ExcludeFromSameAgencyItemFilter excludeFromSameAgencyItemFilter;
 	private final IsRequestableItemFilter isRequestableItemFilter;
+	private final IncludeItemWithHoldsItemFilter includeItemWithHoldsItemFilter;
 
-	AllItemFilters(ConsortiumService consortiumService, HostLmsService hostLmsService,
+	AllItemFilters(HostLmsService hostLmsService,
 		ExcludeFromSameAgencyItemFilter excludeFromSameAgencyItemFilter,
 		AgencyExclusionItemFilter agencyExclusionItemFilter,
-		IsRequestableItemFilter isRequestableItemFilter) {
+		IsRequestableItemFilter isRequestableItemFilter,
+		IncludeItemWithHoldsItemFilter includeItemWithHoldsItemFilter) {
 
-		this.consortiumService = consortiumService;
 		this.hostLmsService = hostLmsService;
 		this.excludeFromSameAgencyItemFilter = excludeFromSameAgencyItemFilter;
 		this.agencyExclusionItemFilter = agencyExclusionItemFilter;
 		this.isRequestableItemFilter = isRequestableItemFilter;
+		this.includeItemWithHoldsItemFilter = includeItemWithHoldsItemFilter;
 	}
 
 	public Mono<List<Item>> filterItems(Flux<Item> items, ItemFilterParameters parameters) {
@@ -51,30 +50,9 @@ public class AllItemFilters {
 			.filterWhen(excludeFromSameAgencyItemFilter.predicate(parameters))
 			.filterWhen(agencyExclusionItemFilter.predicate(parameters))
 			.filterWhen(isRequestableItemFilter.predicate(parameters))
-			.filterWhen(this::includeItemWithHolds)
+			.filterWhen(includeItemWithHoldsItemFilter.predicate(parameters))
 			.filterWhen(item -> fromSameServer(item, borrowingHostLmsCode))
 			.collectList();
-	}
-
-	/**
-	 * Checks if an item with holds should be included in the resolution process.
-	 * <p>
-	 * This method checks the consortium's functional setting for selecting unavailable items.
-	 * If the setting is enabled, items with holds are included. Otherwise, only items with no holds are included.
-	 *
-	 * @param item the item to check
-	 * @return true if the item should be included, false otherwise
-	 */
-	private Mono<Boolean> includeItemWithHolds(Item item) {
-		return this.consortiumService.isEnabled(SELECT_UNAVAILABLE_ITEMS)
-			.map(enabled -> {
-				final boolean includeItem = enabled || item.hasNoHolds();
-
-				log.debug("Include item with holds: enabled={}, item.hasNoHolds={}, includeItem={}",
-					enabled, item.hasNoHolds(), includeItem);
-
-				return includeItem;
-			});
 	}
 
 	/**
---
+++ dcb/src/main/java/org/olf/dcb/request/resolution/IncludeItemWithHoldsItemFilter.java
@@ -0,0 +1,42 @@
+package org.olf.dcb.request.resolution;
+
+import static org.olf.dcb.core.model.FunctionalSettingType.SELECT_UNAVAILABLE_ITEMS;
+
+import java.util.function.Function;
+
+import org.olf.dcb.core.ConsortiumService;
+import org.olf.dcb.core.model.Item;
+import org.reactivestreams.Publisher;
+
+import jakarta.inject.Singleton;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@Singleton
+public class IncludeItemWithHoldsItemFilter implements ItemFilter {
+	private final ConsortiumService consortiumService;
+
+	public IncludeItemWithHoldsItemFilter(ConsortiumService consortiumService) {
+		this.consortiumService = consortiumService;
+	}
+
+	/**
+	 * Checks if an item with holds should be included in the resolution process.
+	 * <p>
+	 * This method checks the consortium's functional setting for selecting unavailable items.
+	 * If the setting is enabled, items with holds are included. Otherwise, only items with no holds are included.
+	 */
+	public Function<Item, Publisher<Boolean>> predicate(
+		ItemFilterParameters parameters) {
+		return item -> consortiumService.isEnabled(SELECT_UNAVAILABLE_ITEMS)
+			.map(enabled -> {
+				final boolean includeItem = enabled || item.hasNoHolds();
+
+				log.debug(
+					"Include item with holds: enabled={}, item.hasNoHolds={}, includeItem={}",
+					enabled, item.hasNoHolds(), includeItem);
+
+				return includeItem;
+			});
+	}
+}
