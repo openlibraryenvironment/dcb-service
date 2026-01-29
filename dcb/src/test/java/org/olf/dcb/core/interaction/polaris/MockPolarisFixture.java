be7b15ae5e6d17c36564fe9892cd9b6f95ad5987
Marc Johnson <marc@k-int.co.uk>
2026-01-29 22:42:40 +0000

    Extract methods for common Polaris mock responses

--- dcb/src/test/java/org/olf/dcb/core/interaction/polaris/MockPolarisFixture.java
+++ dcb/src/test/java/org/olf/dcb/core/interaction/polaris/MockPolarisFixture.java
@@ -11,10 +11,10 @@ import static org.olf.dcb.core.interaction.polaris.ApplicationServicesClient.Wor
 
 import java.util.List;
 
-import org.checkerframework.checker.nullness.qual.NonNull;
 import org.mockserver.client.MockServerClient;
 import org.mockserver.model.HttpRequest;
 import org.mockserver.model.HttpResponse;
+import org.mockserver.model.JsonBody;
 import org.olf.dcb.core.interaction.polaris.ApplicationServicesClient.BibliographicRecord;
 import org.olf.dcb.core.interaction.polaris.ApplicationServicesClient.ItemRecordFull;
 import org.olf.dcb.core.interaction.polaris.ApplicationServicesClient.LibraryHold;
@@ -40,30 +40,28 @@ public class MockPolarisFixture {
 	}
 
 	public void mockPapiStaffAuthentication() {
-		mock("POST", protectedPapiServicePath("/authenticator/staff"), "test-staff-auth.json");
+		mockPost(protectedPapiServicePath("/authenticator/staff"), "test-staff-auth.json");
 	}
 
 	public void mockAppServicesStaffAuthentication() {
-		mock("POST", "/polaris.applicationservices/api/v1/eng/20/authentication/staffuser", "auth-response.json");
+		mockPost("/polaris.applicationservices/api/v1/eng/20/authentication/staffuser",
+			"auth-response.json");
 	}
 
 	public void mockPatronAuthentication() {
-		mock("POST", publicPapiServicePath("/authenticator/patron"), "test-patron-auth.json");
+		mockPost(publicPapiServicePath("/authenticator/patron"), "test-patron-auth.json");
 	}
 
 	public void mockCreatePatron() {
-		mock("POST", publicPapiServicePath("/patron"), "create-patron.json");
+		mockPost(publicPapiServicePath("/patron"), "create-patron.json");
 	}
 
 	public void mockCreatePatron(PAPIClient.PatronRegistrationCreateResult response) {
-		mock("POST", publicPapiServicePath("/patron"),
-			response()
-				.withStatusCode(200)
-				.withBody(json(response)));
+		mockPost(publicPapiServicePath("/patron"), okJson(response));
 	}
 
 	public void mockUpdatePatron(String patronBarcode) {
-		mock("PUT", patronByBarcodePath(patronBarcode), "update-patron.json");
+		mockPut(patronByBarcodePath(patronBarcode), "update-patron.json");
 	}
 
 	public void verifyUpdatePatron(String barcode, PatronRegistration expectedUpdate) {
@@ -78,7 +76,7 @@ public class MockPolarisFixture {
 
 	public void mockPatronSearch(String firstMiddleLastName) {
 		mockServerClient.when(patronSearchRequest(firstMiddleLastName))
-			.respond(okJson(resourceLoader.getResource("patron-search.json")));
+			.respond(okJson(getResource("patron-search.json")));
 	}
 
 	public void mockPatronSearchPapiError(String firstMiddleLastName,
@@ -105,65 +103,62 @@ public class MockPolarisFixture {
 	}
 
 	public void mockGetPatron(String patronId, ApplicationServicesClient.PatronData patron) {
-		mock("GET", patronByIdPath(patronId), okJson(patron));
+		mockGet(patronByIdPath(patronId), patron);
 	}
 
 	public void mockGetPatronServerErrorResponse(String patronId) {
-		mock("GET", patronByIdPath(patronId),
-			response()
-				.withStatusCode(500)
-				.withBody("Something went wrong"));
+		mockGet(patronByIdPath(patronId), HttpResponse.response()
+			.withStatusCode(500)
+			.withBody("Something went wrong"));
 	}
 
 	public void mockGetPatronByBarcode(String barcode) {
-		mock("GET", patronByBarcodePath(barcode), "patron-by-barcode.json");
+		mockGet(patronByBarcodePath(barcode), "patron-by-barcode.json");
 	}
 
 	public void mockGetPatronCirculationBlocks(String barcode,
 		PAPIClient.PatronCirculationBlocksResult response) {
 
-		mock("GET", patronByBarcodePath(barcode) + "/circulationblocks", okJson(response));
+		mockGet(patronByBarcodePath(barcode) + "/circulationblocks", okJson(response));
 	}
 
 	public void mockGetPatronBlocksSummary(String patronId) {
 		String path = "/polaris.applicationservices/api/v1/eng/20/polaris/73/1/patrons/%s/blockssummary"
 			.formatted(patronId);
-		mock("GET", path, okText("[]"));
+
+		mockGet(path, okText("[]"));
 	}
 
 	public void mockGetPatronBlocksSummaryNotFoundResponse(String patronId) {
 		String path = "/polaris.applicationservices/api/v1/eng/20/polaris/73/1/patrons/%s/blockssummary"
 			.formatted(patronId);
 
-		mock("GET", path, notFoundResponse());
+		mockGet(path, notFoundResponse());
 	}
 
 	public void mockGetPatronBlocksSummaryServerErrorResponse(String patronId) {
 		String path = "/polaris.applicationservices/api/v1/eng/20/polaris/73/1/patrons/%s/blockssummary"
 			.formatted(patronId);
 
-		mock("GET", path, response().withStatusCode(INTERNAL_SERVER_ERROR.getCode()));
+		mockGet(path, HttpResponse.response().withStatusCode(INTERNAL_SERVER_ERROR.getCode()));
 	}
 
 	public void mockGetPatronBarcode(String patronId, String barcode) {
 		String body = "\"%s\"".formatted(barcode);
-		mock("GET",
-			"/polaris.applicationservices/api/v1/eng/20/polaris/73/1/barcodes/patrons/" + patronId, okText(body));
+
+		mockGet("/polaris.applicationservices/api/v1/eng/20/polaris/73/1/barcodes/patrons/" + patronId, okText(body));
 	}
 
 	public void mockCheckoutItemToPatron(String localPatronBarcode) {
-		mock("POST", patronItemCheckOutPath(localPatronBarcode),
-			"itemcheckoutsuccess.json");
+		mockPost(patronItemCheckOutPath(localPatronBarcode), "itemcheckoutsuccess.json");
 	}
 
 	public void mockRenewalSuccess(String localPatronBarcode) {
-		mock("POST", patronItemCheckOutPath(localPatronBarcode),
-			"renewal-success.json");
+		mockPost(patronItemCheckOutPath(localPatronBarcode), "renewal-success.json");
 	}
 
 	public void mockRenewalItemBlockedError(String localPatronBarcode) {
-		mock("POST", patronItemCheckOutPath(localPatronBarcode),
-			"renewal-item-blocked.json");
+		mockPost(patronItemCheckOutPath(localPatronBarcode), "renewal-item-blocked.json");
 	}
 
 	private static String patronItemCheckOutPath(String localPatronBarcode) {
@@ -171,11 +166,11 @@ public class MockPolarisFixture {
 	}
 
 	public void mockGetItemsForBib(String bibId) {
-		mock("GET", itemsByBibIdPath(bibId), "items-get.json");
+		mockGet(itemsByBibIdPath(bibId), "items-get.json");
 	}
 
 	public void mockGetItemsForBibWithShelfLocations(String bibId) {
-		mock("GET", itemsByBibIdPath(bibId), "items-get-with-shelf-locations.json");
+		mockGet(itemsByBibIdPath(bibId), "items-get-with-shelf-locations.json");
 	}
 
 	private static String itemsByBibIdPath(String bibId) {
@@ -183,23 +178,21 @@ public class MockPolarisFixture {
 	}
 
 	public void mockGetItem(String itemId) {
-		mock("GET", "/polaris.applicationservices/api/v1/eng/20/polaris/73/1/itemrecords/" + itemId, "item-by-id.json");
+		mockGet("/polaris.applicationservices/api/v1/eng/20/polaris/73/1/itemrecords/" + itemId,
+			"item-by-id.json");
 	}
 
 	public void mockGetItem(Integer itemId, ItemRecordFull expectedItem) {
-		mock("GET", "/polaris.applicationservices/api/v1/eng/20/polaris/73/1/itemrecords/" + itemId,
-			response()
-				.withHeader("Content-Type", "application/json")
-				.withStatusCode(200)
-				.withBody(json(expectedItem)));
+		mockGet("/polaris.applicationservices/api/v1/eng/20/polaris/73/1/itemrecords/" + itemId, expectedItem);
 	}
 
 	public void mockGetItemWithNullRenewalCount(String itemId) {
-		mock("GET", "/polaris.applicationservices/api/v1/eng/20/polaris/73/1/itemrecords/" + itemId, "item-by-id-without-renewal-count.json");
+		mockGet("/polaris.applicationservices/api/v1/eng/20/polaris/73/1/itemrecords/" + itemId,
+			"item-by-id-without-renewal-count.json");
 	}
 
 	public void mockGetItemServerErrorResponse(String itemId) {
-		mock("GET", "/polaris.applicationservices/api/v1/eng/20/polaris/73/1/itemrecords/" + itemId,
+		mockGet("/polaris.applicationservices/api/v1/eng/20/polaris/73/1/itemrecords/" + itemId,
 			response()
 				.withStatusCode(500)
 				.withBody("Something went wrong"));
@@ -207,17 +200,16 @@ public class MockPolarisFixture {
 
 	public void mockGetItemBarcode(String localItemId, String barcode) {
 		String body = "\"%s\"".formatted(barcode);
-		mock("GET",
-			"/polaris.applicationservices/api/v1/eng/20/polaris/73/1/barcodes/items/" + localItemId, okText(body));
+		mockGet("/polaris.applicationservices/api/v1/eng/20/polaris/73/1/barcodes/items/" + localItemId, okText(body));
 	}
 
 	void mockPlaceHold() {
-		mock("POST", "/polaris.applicationservices/api/v1/eng/20/polaris/73/1/workflow",
+		mockPost("/polaris.applicationservices/api/v1/eng/20/polaris/73/1/workflow",
 			"successful-place-request.json");
 	}
 
 	public void mockPlaceHoldUnsuccessful() {
-		mock("POST", "/polaris.applicationservices/api/v1/eng/20/polaris/73/1/workflow",
+		mockPost("/polaris.applicationservices/api/v1/eng/20/polaris/73/1/workflow",
 			"unsuccessful-place-request.json");
 	}
 
@@ -238,92 +230,73 @@ public class MockPolarisFixture {
 	}
 
 	public void mockListPatronLocalHolds() {
-		mock("GET", "/polaris.applicationservices/api/v1/eng/20/polaris/73/1/patrons/1/requests/local",
+		mockGet("/polaris.applicationservices/api/v1/eng/20/polaris/73/1/patrons/1/requests/local",
 			"listPatronLocalHolds.json");
 	}
 
 	public void mockListPatronLocalHolds(Integer patronId, SysHoldRequest expectedHold) {
-		mock("GET", "/polaris.applicationservices/api/v1/eng/20/polaris/73/1/patrons/%s/requests/local".formatted(patronId),
-			response()
-				.withStatusCode(200)
-				.withBody(json(List.of(expectedHold))));
+		mockGet("/polaris.applicationservices/api/v1/eng/20/polaris/73/1/patrons/%s/requests/local".formatted(patronId),
+			List.of(expectedHold));
 	}
 
 	public void mockEmptyListPatronLocalHolds() {
-		mock("GET", "/polaris.applicationservices/api/v1/eng/20/polaris/73/1/patrons/1/requests/local",
+		mockGet("/polaris.applicationservices/api/v1/eng/20/polaris/73/1/patrons/1/requests/local",
 			"emptyListPatronLocalHolds.json");
 	}
 
-	public void mockGetHold(String holdId, LibraryHold response) {
-		mock("GET", "/polaris.applicationservices/api/v1/eng/20/polaris/73/1/holds/" + holdId,
-			response()
-				.withStatusCode(200)
-				.withBody(json(response)
-		));
+	public void mockGetHold(String holdId, LibraryHold responseBody) {
+		mockGet("/polaris.applicationservices/api/v1/eng/20/polaris/73/1/holds/" + holdId, responseBody);
 	}
 
 	public void mockGetHoldNotFound(String holdId, PolarisError response) {
-		mock("GET", "/polaris.applicationservices/api/v1/eng/20/polaris/73/1/holds/" + holdId,
+		mockGet("/polaris.applicationservices/api/v1/eng/20/polaris/73/1/holds/" + holdId,
 			response()
 				.withStatusCode(404)
-				.withBody(json(response)
-				));
+				.withBody(JsonBody.json(response)));
 	}
 
 	void mockCreateBib() {
-		mock("POST", "/polaris.applicationservices/api/v1/eng/20/polaris/73/1/bibliographicrecords*",
+		mockPost("/polaris.applicationservices/api/v1/eng/20/polaris/73/1/bibliographicrecords*",
 			"create-bib-resp.json");
 	}
 
 	public void mockCreateBibNotAuthorisedResponse() {
-		mock("POST", "/polaris.applicationservices/api/v1/eng/20/polaris/73/1/bibliographicrecords*",
+		mockPost("/polaris.applicationservices/api/v1/eng/20/polaris/73/1/bibliographicrecords*",
 			response().withStatusCode(401));
 	}
 
 	public void mockGetBib(String bibId) {
-		mock("GET", "/polaris.applicationservices/api/v1/eng/20/polaris/73/1/bibliographicrecords/" + bibId + "*",
+		mockGet("/polaris.applicationservices/api/v1/eng/20/polaris/73/1/bibliographicrecords/" + bibId + "*",
 			"get-bib.json");
 	}
 
 	public void mockGetBib(Integer bibId, BibliographicRecord expectedBib) {
-		mock("GET", "/polaris.applicationservices/api/v1/eng/20/polaris/73/1/bibliographicrecords/" + bibId + "*",
-			response()
-				.withStatusCode(200)
-				.withContentType(APPLICATION_JSON)
-				.withBody(json(expectedBib)));
+		mockGet("/polaris.applicationservices/api/v1/eng/20/polaris/73/1/bibliographicrecords/" + bibId + "*",
+			expectedBib);
 	}
 
 	public void mockGetPagedBibs() {
-		mock("GET", protectedPapiServicePath("/string/synch/bibs/MARCXML/paged/*"),
-			"bibs-slice-0-9.json");
+		mockGet(protectedPapiServicePath("/string/synch/bibs/MARCXML/paged/*"), "bibs-slice-0-9.json");
 	}
 
 	void mockStartWorkflow(String responsePath) {
-		mock("POST", "/polaris.applicationservices/api/v1/eng/20/polaris/73/1/workflow",
-			responsePath);
+		mockPost("/polaris.applicationservices/api/v1/eng/20/polaris/73/1/workflow", responsePath);
 	}
 
 	void mockStartWorkflow(WorkflowResponse response) {
-		mock("POST", "/polaris.applicationservices/api/v1/eng/20/polaris/73/1/workflow",
-			response()
-				.withStatusCode(200)
-				.withBody(json(response)));
-
+		mockPost("/polaris.applicationservices/api/v1/eng/20/polaris/73/1/workflow", response);
 	}
 
 	public void mockContinueWorkflow(String workflowId, String responsePath) {
-		mock("PUT", "/polaris.applicationservices/api/v1/eng/20/polaris/73/1/workflow/" + workflowId,
-			responsePath);
+		mockPut("/polaris.applicationservices/api/v1/eng/20/polaris/73/1/workflow/" + workflowId, responsePath);
 	}
 
 	void mockGetMaterialTypes() {
-		mock("GET", "/polaris.applicationservices/api/v1/eng/20/polaris/73/1/materialtypes",
-			"materialtypes.json");
+		mockGet("/polaris.applicationservices/api/v1/eng/20/polaris/73/1/materialtypes", "materialtypes.json");
 	}
 
 	void mockGetItemStatuses() {
-		mock("GET", "/polaris.applicationservices/api/v1/eng/20/polaris/73/1/itemstatuses",
-			"itemstatuses.json");
+		mockGet("/polaris.applicationservices/api/v1/eng/20/polaris/73/1/itemstatuses", "itemstatuses.json");
 	}
 
 	private static String patronByIdPath(String patronId) {
@@ -346,8 +319,36 @@ public class MockPolarisFixture {
 		return "/PAPIService/REST/%s/v1/1033/100/1%s".formatted(scope, subPath);
 	}
 
-	private void mock(String method, String path, String jsonResourcePath) {
-		mock(method, path, okJson(resourceLoader.getResource(jsonResourcePath)));
+	private void mockPost(String path, String jsonResourcePath) {
+		mockPost(path, (Object)getResource(jsonResourcePath));
+	}
+
+	private void mockPost(String path, Object responseBody) {
+		mockPost(path, okJson(responseBody));
+	}
+
+	private void mockPost(String path, HttpResponse response) {
+		mock("POST", path, response);
+	}
+
+	private void mockGet(String path, String jsonResourcePath) {
+		mockGet(path, (Object)getResource(jsonResourcePath));
+	}
+
+	private void mockGet(String path, Object responseBody) {
+		mockGet(path, okJson(responseBody));
+	}
+
+	private void mockGet(String path, HttpResponse response) {
+		mock("GET", path, response);
+	}
+
+	private void mockPut(String path, String jsonResourcePath) {
+		mock("PUT", path, okJson(getResource(jsonResourcePath)));
+	}
+
+	private String getResource(String path) {
+		return resourceLoader.getResource(path);
 	}
 
 	private void mock(String method, String path, HttpResponse response) {
