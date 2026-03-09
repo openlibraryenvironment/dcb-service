6a2366892ad6cc4e6ea4f7de755a5ddfb2c4161f
Steve Osguthorpe <steve.osguthorpe@k-int.com>
2026-03-09 13:23:01 +0000

    Actually extend DefaultConfig

--- dcb/src/main/java/org/olf/dcb/core/interaction/folio/FolioOaiClientConfig.java
+++ dcb/src/main/java/org/olf/dcb/core/interaction/folio/FolioOaiClientConfig.java
@@ -4,23 +4,11 @@ import java.time.Duration;
 import java.util.Optional;
 
 import io.micronaut.http.client.DefaultHttpClientConfiguration;
-import io.micronaut.http.client.HttpClientConfiguration;
 import jakarta.inject.Singleton;
 
 @Singleton
-public class FolioOaiClientConfig extends HttpClientConfiguration {
+public class FolioOaiClientConfig extends DefaultHttpClientConfiguration {
 	final Optional<Duration> folioTimeout = Optional.of( Duration.ofMinutes(10) );
-	
-	HttpClientConfiguration defaultConfig;
-	
-	public FolioOaiClientConfig(DefaultHttpClientConfiguration defaultConfig) {
-		this.defaultConfig = defaultConfig;
-	}
-	
-	@Override
-	public ConnectionPoolConfiguration getConnectionPoolConfiguration() {
-		return defaultConfig.getConnectionPoolConfiguration();
-	}
 	
 	@Override
 	public Optional<Duration> getReadTimeout() {
